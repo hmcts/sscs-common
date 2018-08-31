3af7f7b46951b9e21339e6ed1d79f27c8f336183
Chris Grimble <chris@cg-it-solutions.com>
2018-08-31 14:05:27 +0100

    Added create case to ccd client

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/CcdClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/CcdClientTest.java
@@ -11,7 +11,10 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
@@ -30,6 +33,8 @@ public class CcdClientTest {
     private IdamTokens idamTokens;
     private long caseDetailsId;
     private CaseDetails caseDetails;
+    private CcdClient underTest;
+    private SscsCcdConvertService sscsCcdConvertService;
 
     @Before
     public void setUp() {
@@ -52,6 +57,8 @@ public class CcdClientTest {
         caseDetails = CaseDetails.builder()
                 .id(caseDetailsId)
                 .build();
+        sscsCcdConvertService = mock(SscsCcdConvertService.class);
+        underTest = new CcdClient(coreCaseDataApi, idamService, sscsCcdConvertService);
     }
 
     @Test
@@ -61,10 +68,50 @@ public class CcdClientTest {
                 oauthToken, serviceAuthToken, userId, jurisdiction, caseTypeId, searchCriteria)
         ).thenReturn(singletonList(caseDetails));
 
-        List<SscsCaseDetails> caseDetailsList = new CcdClient(coreCaseDataApi, idamService)
-                .findCaseBy(ccdRequestDetails, searchCriteria);
+        SscsCaseDetails sscsCaseDetails = SscsCaseDetails.builder().build();
+        when(sscsCcdConvertService.getCaseDetails(caseDetails)).thenReturn(sscsCaseDetails);
+
+        List<SscsCaseDetails> caseDetailsList = underTest.findCaseBy(ccdRequestDetails, searchCriteria);
 
         assertThat(caseDetailsList.size(), is(1));
-        assertThat(caseDetailsList.get(0).getId(), is(caseDetailsId));
+        assertThat(caseDetailsList.get(0), is(sscsCaseDetails));
+    }
+
+    @Test
+    public void canCreateACase() {
+        when(idamService.getIdamTokens()).thenReturn(idamTokens);
+        SscsCaseData sscsCaseData = SscsCaseData.builder().build();
+        StartEventResponse startEventResponse = StartEventResponse.builder().build();
+        when(coreCaseDataApi.startForCaseworker(oauthToken,
+                serviceAuthToken,
+                userId,
+                jurisdiction,
+                caseTypeId,
+                "appealCreated"
+                )).thenReturn(startEventResponse);
+        CaseDataContent caseDataContent = CaseDataContent.builder().build();
+        when(sscsCcdConvertService.getCaseDataContent(
+                sscsCaseData,
+                startEventResponse,
+                "SSCS - appeal created event",
+                "Created SSCS")).thenReturn(caseDataContent);
+
+        when(coreCaseDataApi.submitForCaseworker(
+                oauthToken,
+                serviceAuthToken,
+                userId,
+                jurisdiction,
+                caseTypeId,
+                true,
+                caseDataContent
+        )).thenReturn(caseDetails);
+
+        SscsCaseDetails expectedSscsCaseDetails = SscsCaseDetails.builder().build();
+        when(sscsCcdConvertService.getCaseDetails(caseDetails)).thenReturn(expectedSscsCaseDetails);
+
+
+        SscsCaseDetails sscsCaseDetails = underTest.createCase(ccdRequestDetails, sscsCaseData);
+
+        assertThat(sscsCaseDetails, is(expectedSscsCaseDetails));
     }
 }
