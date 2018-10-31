4dfc8db7ea66f9e3979c4fb0d4a1a68152c2fd9e
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-10-31 12:14:45 +0000

    SSCS-4179 Added subscription for representative

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Subscriptions.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Subscriptions.java
@@ -12,11 +12,14 @@ import lombok.Value;
 public class Subscriptions {
     Subscription appellantSubscription;
     Subscription supporterSubscription;
+    Subscription representativeSubscription;
 
     @JsonCreator
     public Subscriptions(@JsonProperty("appellantSubscription") Subscription appellantSubscription,
-                        @JsonProperty("supporterSubscription") Subscription supporterSubscription) {
+                         @JsonProperty("supporterSubscription") Subscription supporterSubscription,
+                         @JsonProperty("representativeSubscription") Subscription representativeSubscription) {
         this.appellantSubscription = appellantSubscription;
         this.supporterSubscription = supporterSubscription;
+        this.representativeSubscription = representativeSubscription;
     }
 }
