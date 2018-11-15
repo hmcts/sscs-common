df738b7dd1b1c0670ceb855a047a5f74776a98b6
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-11-15 13:27:43 +0000

    SSCS-4390 Updated domain objects

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Appellant.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Appellant.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Value;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Value
+@Data
 @Builder
 public class Appellant {
 
