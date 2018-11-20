c6436c7ce960e7baacdcff9084c8d6c303d16160
Mark Hunter <mark.hunter@agilesphere.co.uk>
2018-11-20 14:36:29 +0000

    SSCS-4393 - Remove retry from create case

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/service/CreateCcdCaseService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/service/CreateCcdCaseService.java
@@ -2,10 +2,6 @@ package uk.gov.hmcts.reform.sscs.ccd.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.retry.RecoveryCallback;
-import org.springframework.retry.RetryCallback;
-import org.springframework.retry.RetryContext;
-import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
@@ -22,22 +18,19 @@ import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 @Slf4j
 @Service
 public class CreateCcdCaseService {
-
     private final IdamService idamService;
     private final SscsCcdConvertService sscsCcdConvertService;
     private final CcdClient ccdClient;
-    private final RetryTemplate retryTemplate;
     private final SearchCcdCaseService searchCcdCaseService;
 
     @Autowired
     public CreateCcdCaseService(IdamService idamService,
                       SscsCcdConvertService sscsCcdConvertService,
-                      CcdClient ccdClient, RetryTemplate retryTemplate,
-                                SearchCcdCaseService searchCcdCaseService) {
+                      CcdClient ccdClient,
+                      SearchCcdCaseService searchCcdCaseService) {
         this.idamService = idamService;
         this.sscsCcdConvertService = sscsCcdConvertService;
         this.ccdClient = ccdClient;
-        this.retryTemplate = retryTemplate;
         this.searchCcdCaseService = searchCcdCaseService;
     }
 
@@ -45,22 +38,13 @@ public class CreateCcdCaseService {
         log.info("Starting create case process with SC number {} and ccdID {} ...",
                 caseData.getCaseReference(), caseData.getCcdCaseId());
         try {
-            return retryTemplate.execute(getRetryCallback(caseData, idamTokens),
-                    getRecoveryCallback(caseData, idamTokens));
+            requestNewIdamTokens(idamTokens);
+            return createCaseInCcd(caseData, idamTokens);
         } catch (Throwable throwable) {
             throw new CreateCcdCaseException(String.format(
-                    "Recovery mechanism failed when creating case with SC %s and ccdID %s...",
+                    "Creating case failed with SC %s and ccdID %s...",
                     caseData.getCaseReference(), caseData.getCcdCaseId()), throwable);
         }
-    }
-
-    private RecoveryCallback<SscsCaseDetails> getRecoveryCallback(SscsCaseData caseData, IdamTokens idamTokens) {
-        return context -> {
-            log.info("Recovery method called when creating case with SC number {} and ccdID {}...",
-                    caseData.getCaseReference(), caseData.getCcdCaseId());
-            requestNewIdamTokens(idamTokens);
-            return createCaseIfDoesNotExist(caseData, idamTokens, context);
-        };
     }
 
     private void requestNewIdamTokens(IdamTokens idamTokens) {
@@ -68,33 +52,21 @@ public class CreateCcdCaseService {
         idamTokens.setServiceAuthorization(idamService.generateServiceAuthorization());
     }
 
-    private RetryCallback<SscsCaseDetails, ? extends Throwable> getRetryCallback(SscsCaseData caseData,
-                                                                             IdamTokens idamTokens) {
-        return (RetryCallback<SscsCaseDetails, Throwable>) context -> {
-            log.info("create case with SC number {} and ccdID {} and retry number {}",
-                    caseData.getCaseReference(), caseData.getCcdCaseId(), context.getRetryCount());
-            return createCaseIfDoesNotExist(caseData, idamTokens, context);
-        };
-    }
-
-    private SscsCaseDetails createCaseIfDoesNotExist(SscsCaseData caseData, IdamTokens idamTokens, RetryContext context) {
-        SscsCaseDetails sscsCaseDetails = null;
-        if (context.getRetryCount() > 0) {
-            sscsCaseDetails = searchCcdCaseService.findCaseByCaseRefOrCaseId(caseData, idamTokens);
-        }
-        return (null == sscsCaseDetails ? createCaseInCcd(caseData, idamTokens) : sscsCaseDetails);
-    }
-
     private SscsCaseDetails createCaseInCcd(SscsCaseData caseData, IdamTokens idamTokens) {
-        BenefitType benefitType = caseData.getAppeal() != null ? caseData.getAppeal().getBenefitType() : null;
-        log.info("Creating CCD case for Nino {} and benefit type {}", caseData.getGeneratedNino(), benefitType);
+        try {
+            BenefitType benefitType = caseData.getAppeal() != null ? caseData.getAppeal().getBenefitType() : null;
+            log.info("Creating CCD case for Nino {} and benefit type {}", caseData.getGeneratedNino(), benefitType);
 
-        StartEventResponse startEventResponse = ccdClient.startCaseForCaseworker(idamTokens, "appealCreated");
+            StartEventResponse startEventResponse = ccdClient.startCaseForCaseworker(idamTokens, "appealCreated");
 
-        CaseDataContent caseDataContent = sscsCcdConvertService.getCaseDataContent(caseData, startEventResponse, "SSCS - appeal created event", "Created SSCS");
-        CaseDetails caseDetails = ccdClient.submitForCaseworker(idamTokens, caseDataContent);
+            CaseDataContent caseDataContent = sscsCcdConvertService.getCaseDataContent(caseData, startEventResponse, "SSCS - appeal created event", "Created SSCS");
+            CaseDetails caseDetails = ccdClient.submitForCaseworker(idamTokens, caseDataContent);
 
-        return sscsCcdConvertService.getCaseDetails(caseDetails);
-
+            return sscsCcdConvertService.getCaseDetails(caseDetails);
+        } catch (Exception e) {
+            log.error("Failed to create ccd case for Nino - {} and Benefit type - {} but carrying on ",
+                caseData.getGeneratedNino(), caseData.getAppeal().getBenefitType().getCode(), e);
+            return SscsCaseDetails.builder().build();
+        }
     }
 }
