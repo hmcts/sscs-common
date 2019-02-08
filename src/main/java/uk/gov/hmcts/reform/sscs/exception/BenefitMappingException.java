dfc294a985eea9e220c16f8efe6e0f20df7fb323
Chris Moreton <christopher.moreton@hmcts.net>
2019-02-08 10:50:27 +0000

    Upgrade java-logging to 4.0.1

--- src/main/java/uk/gov/hmcts/reform/sscs/exception/BenefitMappingException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/exception/BenefitMappingException.java
@@ -1,12 +1,11 @@
 package uk.gov.hmcts.reform.sscs.exception;
 
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-public class BenefitMappingException extends UnknownErrorCodeException {
+public class BenefitMappingException extends RuntimeException {
 
     public BenefitMappingException(Exception ex) {
-        super(AlertLevel.P3, ex);
+
+        super(ex.getMessage());
     }
 }
--- src/main/java/uk/gov/hmcts/reform/sscs/exception/RegionalProcessingCenterServiceException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/exception/RegionalProcessingCenterServiceException.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.sscs.exception;
 
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
-
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-public class RegionalProcessingCenterServiceException extends UnknownErrorCodeException {
+public class RegionalProcessingCenterServiceException extends RuntimeException {
     public RegionalProcessingCenterServiceException(Throwable cause) {
-        super(AlertLevel.P4, cause);
+
+        super(cause.getMessage());
     }
 }
