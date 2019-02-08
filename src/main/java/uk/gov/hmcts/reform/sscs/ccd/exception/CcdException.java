00cc0de60004d0be1bc00842a5282cf53e2c61e4
Chris Moreton <christopher.moreton@hmcts.net>
2019-02-08 11:02:48 +0000

    Update CCD exceptions to use RuntimeException

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/exception/CcdException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/exception/CcdException.java
@@ -1,15 +1,13 @@
 package uk.gov.hmcts.reform.sscs.ccd.exception;
 
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
-
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-public class CcdException extends UnknownErrorCodeException {
+public class CcdException extends RuntimeException {
     public CcdException(String message) {
-        super(AlertLevel.P4, message);
+
+        super(message);
     }
 
     public CcdException(String message, Throwable ex) {
-        super(AlertLevel.P4, message, ex);
+        super(message);
     }
 }
