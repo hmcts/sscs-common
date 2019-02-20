998f9dac8ed37ad1536b0f80bc2dd6970f364e9f
david.crespo <david.crespo@hmcts.net>
2019-02-20 17:09:17 +0100

    apply patch to 0.0.132 version to fix sidam

--- src/main/java/uk/gov/hmcts/reform/sscs/idam/IdamApiClient.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/idam/IdamApiClient.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.sscs.idam;
 import org.apache.http.HttpHeaders;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,7 +21,8 @@ public interface IdamApiClient {
             @RequestHeader(HttpHeaders.AUTHORIZATION) final String authorisation,
             @RequestParam("response_type") final String responseType,
             @RequestParam("client_id") final String clientId,
-            @RequestParam("redirect_uri") final String redirectUri
+            @RequestParam("redirect_uri") final String redirectUri,
+            @RequestBody final String body
     );
 
     @RequestMapping(
@@ -33,7 +35,8 @@ public interface IdamApiClient {
             @RequestParam("grant_type") final String grantType,
             @RequestParam("redirect_uri") final String redirectUri,
             @RequestParam("client_id") final String clientId,
-            @RequestParam("client_secret") final String clientSecret
+            @RequestParam("client_secret") final String clientSecret,
+            @RequestBody final String body
     );
 
     @RequestMapping(
