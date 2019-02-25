99c8e2c5dd20767919485d40346001b8c5031afe
david.crespo <david.crespo@hmcts.net>
2019-02-25 12:01:34 +0000

    fix imports

--- src/test/java/uk/gov/hmcts/reform/sscs/idam/IdamServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/idam/IdamServiceTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.sscs.idam;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
