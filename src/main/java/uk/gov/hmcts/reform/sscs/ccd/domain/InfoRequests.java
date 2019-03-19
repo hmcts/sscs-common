c505f3f596a01e47e215f59955207e9a26a2d480
Mark Hunter <mark.hunter@agilesphere.co.uk>
2019-03-19 14:31:29 +0000

    SSCS-4777 - Fix checkstyle issue

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/InfoRequests.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/InfoRequests.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
-
-import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
