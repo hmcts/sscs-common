cafd28e77f143e6b7048531539a5c14b893251b4
jackmaloney <jack.maloney@transformuk.com>
2019-03-28 14:08:10 +0000

    Add missing unit tests

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Subscriptions.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Subscriptions.java
@@ -3,17 +3,19 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Value;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Value
 @Builder(toBuilder = true)
 public class Subscriptions {
-    Subscription appellantSubscription;
-    Subscription supporterSubscription;
-    Subscription representativeSubscription;
-    Subscription appointeeSubscription;
+    @Getter(AccessLevel.NONE) Subscription appellantSubscription;
+    @Getter(AccessLevel.NONE) Subscription supporterSubscription;
+    @Getter(AccessLevel.NONE) Subscription representativeSubscription;
+    @Getter(AccessLevel.NONE) Subscription appointeeSubscription;
 
     @JsonCreator
     public Subscriptions(@JsonProperty("appellantSubscription") Subscription appellantSubscription,
@@ -24,5 +26,22 @@ public class Subscriptions {
         this.supporterSubscription = supporterSubscription;
         this.representativeSubscription = representativeSubscription;
         this.appointeeSubscription = appointeeSubscription;
+    }
+
+
+    public Subscription getAppellantSubscription() {
+        return null != appellantSubscription ? appellantSubscription : Subscription.builder().build();
+    }
+
+    public Subscription getSupporterSubscription() {
+        return null != supporterSubscription ? supporterSubscription : Subscription.builder().build();
+    }
+
+    public Subscription getRepresentativeSubscription() {
+        return null != representativeSubscription ? representativeSubscription : Subscription.builder().build();
+    }
+
+    public Subscription getAppointeeSubscription() {
+        return null != appointeeSubscription ? appointeeSubscription : Subscription.builder().build();
     }
 }
