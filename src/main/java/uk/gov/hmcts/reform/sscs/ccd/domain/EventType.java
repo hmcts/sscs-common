f30b7f8280ac7ca3ffe5933881d62f47551bd982
jackmaloney <jack.maloney@transformuk.com>
2019-05-31 14:54:08 +0100

    Add validAppealCreated event

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/EventType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/EventType.java
@@ -58,7 +58,8 @@ public enum EventType {
     CREATE_APPEAL_PDF("createAppealPDF", 0, false),
     RESEND_CASE_TO_GAPS2("resendCaseToGAPS2", 0, false),
     VALID_APPEAL("validAppeal", 0, false),
-    INTERLOC_VALID_APPEAL("interlocValidAppeal", 0, false);
+    INTERLOC_VALID_APPEAL("interlocValidAppeal", 0, false),
+    VALID_APPEAL_CREATED("validAppealCreated", 0, false);
 
     private String type;
     private String ccdType;
