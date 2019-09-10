fe5f3e4b20c3ef24e1cb287df2aba23509f45de6
david.crespo <david.crespo@hmcts.net>
2019-09-10 15:57:17 +0100

    add new dwpState field to the sscsCaseData

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Collections;
 import java.util.List;
 import lombok.AccessLevel;
@@ -81,6 +85,7 @@ public class SscsCaseData implements CaseData {
     private String dwpFurtherInfo;
     private String interlocReferralDate;
     private String dwpRegionalCentre;
+    private String dwpState;
 
     @JsonCreator
     public SscsCaseData(@JsonProperty(value = "ccdCaseId", access = JsonProperty.Access.WRITE_ONLY) String ccdCaseId,
@@ -142,7 +147,8 @@ public class SscsCaseData implements CaseData {
                         @JsonProperty("dwpComplexAppeal") String dwpComplexAppeal,
                         @JsonProperty("dwpFurtherInfo") String dwpFurtherInfo,
                         @JsonProperty("interlocReferralDate") String interlocReferralDate,
-                        @JsonProperty("dwpRegionalCentre") String dwpRegionalCentre
+                        @JsonProperty("dwpRegionalCentre") String dwpRegionalCentre,
+                        @JsonProperty("dwpState") String dwpState
     ) {
         this.ccdCaseId = ccdCaseId;
         this.caseReference = caseReference;
@@ -204,6 +210,7 @@ public class SscsCaseData implements CaseData {
         this.dwpFurtherInfo = dwpFurtherInfo;
         this.interlocReferralDate = interlocReferralDate;
         this.dwpRegionalCentre = dwpRegionalCentre;
+        this.dwpState = dwpState;
     }
 
     @JsonIgnore
@@ -240,15 +247,15 @@ public class SscsCaseData implements CaseData {
     @JsonIgnore
     public void sortCollections() {
         if (getEvents() != null) {
-            Collections.sort(getEvents(), Collections.reverseOrder());
+            getEvents().sort(Collections.reverseOrder());
         }
 
         if (getHearings() != null) {
-            Collections.sort(getHearings(), Collections.reverseOrder());
+            getHearings().sort(Collections.reverseOrder());
         }
 
         if (getEvidence() != null && getEvidence().getDocuments() != null) {
-            Collections.sort(getEvidence().getDocuments(), Collections.reverseOrder());
+            getEvidence().getDocuments().sort(Collections.reverseOrder());
         }
     }
 
