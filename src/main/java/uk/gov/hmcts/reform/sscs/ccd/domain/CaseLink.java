2d2fa5db17c4754364da4a360621d74803ec5d5b
Chris <christopher.davidson@btinternet.com>
2019-09-12 22:58:55 +0100

    Revert "fixing caselink"
    This reverts commit 0456caf6f824e1b33ac9bb5a70725489e6b57ea2.

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CaseLink.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CaseLink.java
@@ -2,20 +2,18 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Value
+@Data
 @Builder(toBuilder = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CaseLink {
     private String caseReference;
 
     @JsonCreator
-    public CaseLink(@JsonProperty("value") String caseReference) {
+    public CaseLink(@JsonProperty("caseReference") String caseReference) {
         this.caseReference = caseReference;
     }
 }
