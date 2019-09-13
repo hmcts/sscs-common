f95b591da023c1d6a302add70ae2382dd75544e1
Chris <christopher.davidson@btinternet.com>
2019-09-13 16:37:13 +0100

    changing to value

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CaseLink.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CaseLink.java
@@ -4,16 +4,16 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Value;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Data
+@Value
 @Builder(toBuilder = true)
 public class CaseLink {
     private String caseReference;
 
     @JsonCreator
-    public CaseLink(@JsonProperty("CaseReference") String caseReference) {
+    public CaseLink(@JsonProperty("value") String caseReference) {
         this.caseReference = caseReference;
     }
 }
