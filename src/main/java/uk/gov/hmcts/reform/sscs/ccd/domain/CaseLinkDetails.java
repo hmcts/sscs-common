649ca305beff0ce98c3ee6c114390e95da12b3c6
Chris <christopher.davidson@btinternet.com>
2019-09-17 15:19:48 +0100

    new caselinkdetails with capital c

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CaseLinkDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CaseLinkDetails.java
@@ -1,21 +1,19 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CaseLinkDetails {
-    private String caseReference;
 
-    @JsonCreator
-    public CaseLinkDetails(@JsonProperty(value = "CaseReference") String caseReference) {
-        this.caseReference = caseReference;
-    }
+    @JsonProperty(value = "CaseReference")
+    private String caseReference;
 }
