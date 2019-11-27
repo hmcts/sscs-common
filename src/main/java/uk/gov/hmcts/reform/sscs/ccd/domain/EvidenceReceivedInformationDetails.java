c6494a1333fedef219f53b25c02fab6a244358fc
jackmaloney <jack.maloney@transformuk.com>
2019-11-27 16:33:05 +0000

    Evidence received lombok

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/EvidenceReceivedInformationDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/EvidenceReceivedInformationDetails.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder(toBuilder = true)
 public class EvidenceReceivedInformationDetails {
 
-    String evidenceReceivedBoolean;
-    String evidenceReceivedDate;
+    private String evidenceReceivedBoolean;
+    private String evidenceReceivedDate;
 
     @JsonCreator
     public EvidenceReceivedInformationDetails(@JsonProperty("evidenceReceivedBoolean") String evidenceReceivedBoolean,
