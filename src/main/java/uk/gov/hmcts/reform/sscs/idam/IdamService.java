9de9408afc01954af06ce073a15502fda1a4211b
srochani <sunil.rochani@hotmail.com>
2020-01-05 23:08:50 +0000

    SSCS-7047 SYA – OpenID connect – Validate the IDAM token that is return

--- src/main/java/uk/gov/hmcts/reform/sscs/idam/IdamService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/idam/IdamService.java
@@ -1,7 +1,18 @@
 package uk.gov.hmcts.reform.sscs.idam;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.factories.DefaultJWSVerifierFactory;
+import com.nimbusds.jose.jwk.AsymmetricJWK;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.SecretJWK;
+import com.nimbusds.jose.proc.JWSVerifierFactory;
+import com.nimbusds.jwt.SignedJWT;
+import java.net.URL;
+import java.security.Key;
 import java.util.Base64;
-
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +29,7 @@ public class IdamService {
 
     private final AuthTokenGenerator authTokenGenerator;
     private final IdamApiClient idamApiClient;
+    private final JWSVerifierFactory jwsVerifierFactory;
 
     @Value("${idam.oauth2.user.email}")
     private String idamOauth2UserEmail;
@@ -34,6 +46,9 @@ public class IdamService {
     @Value("${idam.oauth2.redirectUrl}")
     private String idamOauth2RedirectUrl;
 
+    @Value("${idam.oauth2.jwkUrl}")
+    private String idamJwkUrl;
+
     // Tactical idam token caching solution implemented
     // SSCS-5895 - will deliver the strategic caching solution
     private String cachedToken;
@@ -42,6 +57,7 @@ public class IdamService {
     IdamService(AuthTokenGenerator authTokenGenerator, IdamApiClient idamApiClient) {
         this.authTokenGenerator = authTokenGenerator;
         this.idamApiClient = idamApiClient;
+        this.jwsVerifierFactory = new DefaultJWSVerifierFactory();
     }
 
     public String generateServiceAuthorization() {
@@ -113,5 +129,51 @@ public class IdamService {
                 .serviceAuthorization(generateServiceAuthorization())
                 .userId(getUserId(idamOauth2Token))
                 .build();
+    }
+
+    public boolean verifyTokenSignature(String token) {
+        try {
+            SignedJWT signedJwt = SignedJWT.parse(token);
+
+            JWKSet jsonWebKeySet = loadJsonWebKeySet(idamJwkUrl);
+
+            JWSHeader jwsHeader = signedJwt.getHeader();
+            Key key = findKeyById(jsonWebKeySet, jwsHeader.getKeyID());
+
+            JWSVerifier jwsVerifier = jwsVerifierFactory.createJWSVerifier(jwsHeader, key);
+
+            return signedJwt.verify(jwsVerifier);
+        } catch (Exception e) {
+            log.error("Token validation error {}", e);
+            return false;
+        }
+    }
+
+    private JWKSet loadJsonWebKeySet(String jwksUrl) {
+        try {
+            return JWKSet.load(new URL(jwksUrl));
+        } catch (Exception e) {
+            log.error("JWKS key loading error");
+            throw new RuntimeException("JWKS error", e);
+        }
+    }
+
+    private Key findKeyById(JWKSet jsonWebKeySet, String keyId) {
+        try {
+            JWK jsonWebKey = jsonWebKeySet.getKeyByKeyId(keyId);
+            if (jsonWebKey == null) {
+                throw new RuntimeException("JWK does not exist in the key set");
+            }
+            if (jsonWebKey instanceof SecretJWK) {
+                return ((SecretJWK) jsonWebKey).toSecretKey();
+            }
+            if (jsonWebKey instanceof AsymmetricJWK) {
+                return ((AsymmetricJWK) jsonWebKey).toPublicKey();
+            }
+            throw new RuntimeException("Unsupported JWK " + jsonWebKey.getClass().getName());
+        } catch (JOSEException e) {
+            log.error("Invalid JWK key");
+            throw new RuntimeException("Invalid JWK", e);
+        }
     }
 }
