3f410a2ccd4a44bc23467eb453730623e1a9f3c8
jackmaloney <jack.maloney@transformuk.com>
2020-01-17 14:44:17 +0000

    SSCS-7120 Extend bundle config for advanced bundler

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/BundleSubfolder.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Value;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Value
+@Builder(toBuilder = true)
+public class BundleSubfolder {
+
+    private BundleSubfolderDetails value;
+
+    @JsonCreator
+    public BundleSubfolder(@JsonProperty("value") BundleSubfolderDetails value) {
+        this.value = value;
+    }
+}
