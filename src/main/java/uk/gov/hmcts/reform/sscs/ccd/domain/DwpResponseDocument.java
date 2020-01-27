23d2822c687307108e656e5a3dcc008d9db2358d
jackmaloney <jack.maloney@transformuk.com>
2020-01-27 15:30:31 +0000

    Add file name for dwp docs

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DwpResponseDocument.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DwpResponseDocument.java
@@ -5,17 +5,20 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Value
+@Data
 @Builder(toBuilder = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class DwpResponseDocument {
     private DocumentLink documentLink;
+    private String documentFileName;
 
     @JsonCreator
-    public DwpResponseDocument(@JsonProperty("documentLink") DocumentLink documentLink) {
+    public DwpResponseDocument(@JsonProperty("documentLink") DocumentLink documentLink,
+                               @JsonProperty("documentFileName") String documentFileName) {
         this.documentLink = documentLink;
+        this.documentFileName = documentFileName;
     }
 }
