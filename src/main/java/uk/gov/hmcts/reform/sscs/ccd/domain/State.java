764eec56fdd89ed221d779e86acbe892d14f13ac
Dan Hamilton <dan@emagineers.co.uk>
2020-08-03 09:15:31 +0100

    SSCS-7037 Remove pendingAppeal state

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/State.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/State.java
@@ -56,9 +56,6 @@ public enum State {
     @JsonProperty("hearing")
     HEARING("hearing"),
 
-    @JsonProperty("pendingAppeal")
-    PENDING_APPEAL("pendingAppeal"),
-
     @JsonProperty("notListable")
     NOT_LISTABLE("notListable"),
 
