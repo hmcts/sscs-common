2d94f9e0f5cea580940afc56598f56a92e05aa9e
Michael lavelle <michael@lavelle.name>
2020-08-03 11:37:16 +0100

    SSCS-7855-Adding pdf validators, and configuring SscsCaseData with pdf and adjourn case directions due date validation

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/validation/documentlink/DocumentLinkMustBePdfValidator.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.sscs.ccd.validation.documentlink;
+
+import static org.apache.commons.io.FilenameUtils.getExtension;
+import static org.apache.commons.lang3.StringUtils.equalsAnyIgnoreCase;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentLink;
+
+public class DocumentLinkMustBePdfValidator implements ConstraintValidator<DocumentLinkMustBePdf, DocumentLink> {
+
+    public static boolean isFileAPdf(DocumentLink docLink) {
+        return docLink != null
+            && isNotBlank(docLink.getDocumentUrl())
+            && equalsAnyIgnoreCase("pdf", getExtension(docLink.getDocumentFilename()));
+    }
+
+    @Override
+    public boolean isValid(DocumentLink documentLink, ConstraintValidatorContext context) {
+        if (documentLink != null) {
+            return isFileAPdf(documentLink);
+        }
+        return true;
+    }
+}
