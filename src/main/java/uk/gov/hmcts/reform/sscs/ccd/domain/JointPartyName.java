1ff35600088596e9b8fc1ae986179486ca0f74ea
jackmaloney <jack.maloney@transformuk.com>
2020-08-20 11:37:42 +0100

    Cs fix

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/JointPartyName.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/JointPartyName.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
-import org.apache.commons.lang3.StringUtils;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
