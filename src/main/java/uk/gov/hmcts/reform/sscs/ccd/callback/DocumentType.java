a3c65bda44ffbba3a03189ad1645041f54134d95
sunilrochani <sunil.rochani@hotmail.com>
2020-10-14 19:08:09 +0100

    SSCS-7984: Tribunals API/CCD UI – Processing documents for urgent hearings

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/callback/DocumentType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/callback/DocumentType.java
@@ -23,7 +23,8 @@ public enum DocumentType {
     ADJOURNMENT_NOTICE("adjournmentNotice", "Adjournment Notice"),
     REINSTATEMENT_REQUEST("reinstatementRequest", "Reinstatement request"),
     CONFIDENTIALITY_REQUEST("confidentialityRequest", "Confidentiality request"),
-    JOINT_PARTY_EVIDENCE("jointPartyEvidence", "Joint party evidence");
+    JOINT_PARTY_EVIDENCE("jointPartyEvidence", "Joint party evidence"),
+    URGENT_HEARING_REQUEST("urgentHearingRequest", "Urgent hearing request");
 
     private String value;
     private String label;
