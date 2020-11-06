452f491b2cd7b51d72bd352c3f1ddc9d3ac24c56
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2020-11-06 15:13:39 +0000

    finally a test caught an error to an updated row

--- src/test/java/uk/gov/hmcts/reform/sscs/service/VenueDataLoaderTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/VenueDataLoaderTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sscs.service;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -21,6 +22,14 @@ public class VenueDataLoaderTest {
     @Parameters({"1158", "1144", "1145", "1027", "1028", "1012"})
     public void venuesShouldNotBeActive(String id) {
         assertThat(venueDataLoader.getVenueDetailsMap().get(id).getActive(), is("No"));
+    }
+
+    @Test
+    @Parameters({"63", "94", "97", "106", "115", "1043"})
+    public void venuesShouldBeActiveAndHaveAGoogleLink(String id) {
+        assertThat(venueDataLoader.getVenueDetailsMap().get(id).getActive(), is("Yes"));
+        assertThat(venueDataLoader.getVenueDetailsMap().get(id).getUrl(), containsString("https://"));
+        assertThat(venueDataLoader.getVenueDetailsMap().get(id).getUrl(), containsString("goo"));
     }
 
 }
