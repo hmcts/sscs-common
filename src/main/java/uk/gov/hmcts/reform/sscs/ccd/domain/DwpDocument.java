9256a93d07632964908235f292ef7774984670ed
Dan Hamilton <dan@emagineers.co.uk>
2020-11-18 15:34:55 +0000

    SSCS-7789 Add Dwp Documents to case data

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DwpDocument.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DwpDocument {
+
+    private DocumentLink documentLink;
+    private String documentType;
+    private String documentDateAdded;
+
+
+    @JsonCreator
+    public DwpDocument(@JsonProperty("documentLink") DocumentLink documentLink,
+                       @JsonProperty("documentType") String documentType,
+                       @JsonProperty("documentDateAdded") String documentDateAdded) {
+        this.documentLink = documentLink;
+        this.documentType = documentType;
+        this.documentDateAdded = documentDateAdded;
+    }
+}
