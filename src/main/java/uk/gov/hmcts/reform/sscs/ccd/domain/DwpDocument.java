cb60581b5c17cb2b811da5abd64e096568b17931
Dan Hamilton <dan@emagineers.co.uk>
2020-11-20 09:41:45 +0000

    styling

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DwpDocument.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DwpDocument.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import lombok.experimental.SuperBuilder;
