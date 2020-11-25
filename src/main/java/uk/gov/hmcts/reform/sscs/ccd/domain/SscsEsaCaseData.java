d688851b08ae264130bcc8bdf89dc4c2fb26835b
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2020-11-25 09:03:23 +0000

    small changes to PR#617

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsEsaCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsEsaCaseData.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.isYes;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -10,7 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
+
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
@@ -46,7 +48,7 @@ public class SscsEsaCaseData {
     private YesNo showSchedule3ActivitiesPage;
     private YesNo doesRegulation35Apply;
     @JsonProperty("wcaAppeal")
-    private String wcaAppeal;
+    private YesNo wcaAppeal;
 
     @JsonIgnore
     public YesNo getRegulation35Selection() {
@@ -58,12 +60,7 @@ public class SscsEsaCaseData {
 
     @JsonIgnore
     public boolean isWcaAppeal() {
-        return stringToBoolean(wcaAppeal);
-    }
-
-    @JsonIgnore
-    private boolean stringToBoolean(String value) {
-        return StringUtils.equalsIgnoreCase("yes", value);
+        return isYes(wcaAppeal);
     }
 
     @JsonIgnore
--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsUcCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsUcCaseData.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.isYes;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -10,7 +12,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
@@ -46,7 +47,7 @@ public class SscsUcCaseData {
     private YesNo showSchedule7ActivitiesPage;
     private YesNo doesSchedule9Paragraph4Apply;
     @JsonProperty("lcwaAppeal")
-    private String lcwaAppeal;
+    private YesNo lcwaAppeal;
 
     @JsonIgnore
     public YesNo getSchedule9Paragraph4Selection() {
@@ -58,12 +59,7 @@ public class SscsUcCaseData {
 
     @JsonIgnore
     public boolean isLcwaAppeal() {
-        return stringToBoolean(lcwaAppeal);
-    }
-
-    @JsonIgnore
-    private boolean stringToBoolean(String value) {
-        return StringUtils.equalsIgnoreCase("yes", value);
+        return isYes(lcwaAppeal);
     }
 
     @JsonIgnore
