8ee29b05ee1f67ed7abe8114e5fd41376b5efa8f
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2020-12-08 11:40:15 +0000

    SSCS-8287 - Add/Remove UCB flag (#640)

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/EventType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/EventType.java
@@ -164,7 +164,8 @@ public enum EventType {
     AMEND_ELEMENTS_ISSUES("amendElementsIssues", 0, false),
     ABATE_CASE("abateCase", 0, false),
     PROVIDE_APPOINTEE_DETAILS("provideAppointeeDetails", 0, false),
-    DEATH_OF_APPELLANT("deathOfAppellant", 0, false);
+    DEATH_OF_APPELLANT("deathOfAppellant", 0, false),
+    UPDATE_UCB("updateUCB", 0, false);
 
 
     private String type;
