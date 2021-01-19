3694cb0ebd6c3d93353631a7d65dc422d9673aa1
jackmaloney <jack.maloney@transformuk.com>
2021-01-19 14:02:13 +0000

    SSCS-8496 Process reasonable adjustment event

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ReasonableAdjustmentStatus.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ReasonableAdjustmentStatus.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 public enum ReasonableAdjustmentStatus {
     REQUIRED("required"),
-    DONE("done");
+    ACTIONED("actioned");
     private String id;
     ReasonableAdjustmentStatus(String id) {
         this.id = id;
