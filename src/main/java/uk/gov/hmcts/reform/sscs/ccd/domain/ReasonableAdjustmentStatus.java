cd022b958ae75b5cb3ca5d8681261fa2a0ac2860
chrisdavecm <christopher.davidson@btinternet.com>
2021-01-20 13:58:53 +0000

    sscs 8495 updated annotations

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ReasonableAdjustmentStatus.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ReasonableAdjustmentStatus.java
@@ -1,14 +1,24 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum ReasonableAdjustmentStatus {
+    @JsonProperty("required")
     REQUIRED("required"),
+    @JsonProperty("actioned")
     ACTIONED("actioned");
+
     private String id;
+    
     ReasonableAdjustmentStatus(String id) {
         this.id = id;
     }
 
     public String getId() {
+        return id;
+    }
+
+    public String toString() {
         return id;
     }
 }
