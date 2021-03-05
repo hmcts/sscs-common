bdc6884353569d2bc102ffaa09d688b71c765364
Jason.Hart <jason.hart@enginegroup.com>
2021-03-05 13:10:08 +0000

    added date formatting to SelectedAudioVideoEvidenceDetails

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SelectedAudioVideoEvidenceDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SelectedAudioVideoEvidenceDetails.java
@@ -1,8 +1,12 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +14,7 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SelectedAudioVideoEvidenceDetails {
     private String documentType;
     private DocumentLink documentLink;
@@ -21,6 +26,8 @@ public class SelectedAudioVideoEvidenceDetails {
     public SelectedAudioVideoEvidenceDetails(@JsonProperty("documentType") String documentType,
                       @JsonProperty("documentLink") DocumentLink documentLink,
                       @JsonProperty("rip1Document") DocumentLink rip1Document,
+                      @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+                      @JsonSerialize(using = LocalDateSerializer.class)
                       @JsonProperty("dateAdded") LocalDate dateAdded,
                       @JsonProperty("partyUploaded") String partyUploaded) {
         this.documentType = documentType;
