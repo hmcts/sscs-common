8291b04f54e7682bf7e37ff2bdbcbce1168cf6c3
jackmaloney <jack.maloney@transformuk.com>
2021-03-12 09:49:05 +0000

    SSCS-8851 Multi bundle config

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
@@ -185,6 +185,7 @@ public class SscsCaseData implements CaseData {
     private String responseRequired;
     private String timeExtensionRequested;
     private String bundleConfiguration;
+    private String multiBundleConfiguration;
     private String pcqId;
     //Final decision notice fields
     @JsonUnwrapped
