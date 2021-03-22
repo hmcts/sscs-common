c0c363aaabb43b480fa403b223b9af8c1bc67b30
jackmaloney <jack.maloney@transformuk.com>
2021-03-22 12:09:25 +0000

    SSCS-8892 Map Carer's Allowance to 070 benefit code

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Benefit.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Benefit.java
@@ -6,38 +6,61 @@ import uk.gov.hmcts.reform.sscs.exception.BenefitMappingException;
 
 public enum Benefit {
 
-    ESA("Employment and Support Allowance"),
-    JSA("Job Seekers Allowance"),
-    PIP("Personal Independence Payment"),
-    DLA("Disability Living Allowance"),
-    UC("Universal Credit");
+    ESA("Employment and Support Allowance", "051"),
+    JSA("Job Seekers Allowance", ""),
+    PIP("Personal Independence Payment", "002"),
+    DLA("Disability Living Allowance", "037"),
+    UC("Universal Credit", "001"),
+    CARERS_ALLOWANCE("Carer's Allowance", "070");
 
     private String description;
+    private String benefitCode;
 
     private static final org.slf4j.Logger LOG = getLogger(Benefit.class);
 
-    Benefit(String description) {
+    Benefit(String description, String benefitCode) {
         this.description = description;
+        this.benefitCode = benefitCode;
     }
 
     public static Benefit getBenefitByCode(String code) {
-        Benefit b = null;
-        for (Benefit type : Benefit.values()) {
-            if (type.name().equalsIgnoreCase(code)) {
-                b = type;
+        Benefit benefit = findBenefitByShortName(code);
+        if (benefit == null) {
+            benefit = findBenefitByDescription(code);
+            if (benefit == null) {
+                BenefitMappingException benefitMappingException =
+                        new BenefitMappingException(new Exception(code + " is not a recognised benefit type"));
+                LOG.error("Benefit type mapping error", benefitMappingException);
+                throw benefitMappingException;
             }
         }
-        if (b == null) {
-            BenefitMappingException benefitMappingException =
-                    new BenefitMappingException(new Exception(code + " is not a recognised benefit type"));
-            LOG.error("Benefit type mapping error", benefitMappingException);
-            throw benefitMappingException;
+        return benefit;
+    }
+
+    private static Benefit findBenefitByShortName(String code) {
+        for (Benefit type : Benefit.values()) {
+            if (type.name().equalsIgnoreCase(code)) {
+                return type;
+            }
         }
-        return b;
+        return null;
+    }
+
+    private static Benefit findBenefitByDescription(String code) {
+        for (Benefit type : Benefit.values()) {
+            if (type.getDescription().equalsIgnoreCase(code)) {
+                return type;
+            }
+        }
+        return null;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public String getBenefitCode() {
+        return benefitCode;
     }
 
     public static Boolean isBenefitTypeValid(String field) {
