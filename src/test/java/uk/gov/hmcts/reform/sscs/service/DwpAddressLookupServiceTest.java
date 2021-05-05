22056fd69ab12874fc1f533f12f3ead525225e3e
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-05-05 17:45:39 +0100

    SSCS-9079:Carer's Allowance (SYA) - Update the FL_dwpRegionalCentre list in CCD

--- src/test/java/uk/gov/hmcts/reform/sscs/service/DwpAddressLookupServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/DwpAddressLookupServiceTest.java
@@ -305,7 +305,7 @@ public class DwpAddressLookupServiceTest {
     public void givenACarersAllowanceBenefitType_thenDefaultDwpRegionalCenter() {
         String result = dwpAddressLookup.getDefaultDwpRegionalCenterByBenefitTypeAndOffice("carersAllowance");
 
-        assertEquals("Tyneview Park DRT", result);
+        assertEquals("Carer’s Allowance Dispute Resolution Team", result);
     }
 
     @Test
