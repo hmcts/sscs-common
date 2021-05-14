4f4fa2d3bbf961a75e759cf40ba61040331fcac7
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2021-05-14 16:13:44 +0100

    SSCS-9154 - Carer's Allowance - Panel List for Notification Content (#835)

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelComposition.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum PanelComposition {
+    JUDGE_DOCTOR_AND_DISABILITY_EXPERT("judge, doctor and disability expert", "barnwr, meddyg ac arbenigwr anableddau"),
+    JUDGE_AND_A_DOCTOR("judge and a doctor", "barnwr a meddyg"),
+    JUDGE("judge", "barnwr"),
+    JUDGE_DOCTOR_AND_DISABILITY_EXPERT_IF_APPLICABLE("judge, doctor and disability expert (if applicable)", "barnwr, meddyg ac arbenigwr anabledd (os yw’n berthnasol)");
+
+    private final String english;
+    private final String welsh;
+
+    PanelComposition(String english, String welsh) {
+        this.english = english;
+        this.welsh = welsh;
+    }
+}
