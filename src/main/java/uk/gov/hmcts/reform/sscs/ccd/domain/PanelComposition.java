825fda4f4f22d577acbc87b0a07d7cc375b96bb8
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-05-14 10:02:47 +0100

    correct Welsh panel composition words

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelComposition.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelComposition.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 @Getter
 public enum PanelComposition {
     JUDGE_DOCTOR_AND_DISABILITY_EXPERT("judge, doctor and disability expert", "barnwr, meddyg ac arbenigwr anableddau"),
-    JUDGE_AND_A_DOCTOR("judge and a doctor", "barnwr a meddyg (os yw’n berthnasol)"),
+    JUDGE_AND_A_DOCTOR("judge and a doctor", "barnwr a meddyg"),
     JUDGE("judge", "barnwr"),
-    JUDGE_DOCTOR_AND_DISABILITY_EXPERT_IF_APPLICABLE("judge, doctor and disability expert (if applicable)", "barnwr a meddyg (os yw’n berthnasol)");
+    JUDGE_DOCTOR_AND_DISABILITY_EXPERT_IF_APPLICABLE("judge, doctor and disability expert (if applicable)", "barnwr, meddyg ac arbenigwr anabledd (os yw’n berthnasol)");
 
     private final String english;
     private final String welsh;
--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelCompositionTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelCompositionTest.java
@@ -14,9 +14,9 @@ public class PanelCompositionTest {
     @Test
     @Parameters(
             {"JUDGE_DOCTOR_AND_DISABILITY_EXPERT, judge\\, doctor and disability expert, barnwr\\, meddyg ac arbenigwr anableddau",
-            "JUDGE_AND_A_DOCTOR, judge and a doctor, barnwr a meddyg (os yw’n berthnasol)",
+            "JUDGE_AND_A_DOCTOR, judge and a doctor, barnwr a meddyg",
             "JUDGE, judge, barnwr",
-            "JUDGE_DOCTOR_AND_DISABILITY_EXPERT_IF_APPLICABLE, judge\\, doctor and disability expert (if applicable), barnwr a meddyg (os yw’n berthnasol)"}
+            "JUDGE_DOCTOR_AND_DISABILITY_EXPERT_IF_APPLICABLE, judge\\, doctor and disability expert (if applicable), barnwr\\, meddyg ac arbenigwr anabledd (os yw’n berthnasol)"}
     )
     public void assertAllValues(PanelComposition panelComposition, String english, String welsh) {
         assertThat(panelComposition.getEnglish(), is(english));
