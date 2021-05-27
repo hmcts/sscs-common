a39678d0d5f67cb41c65164e80f7e9a07b19ad17
rasikasri <44141067+rasikasri@users.noreply.github.com>
2021-05-27 09:29:34 +0100

    SSCS-8808:IIDB (SYA) - Allow Industrial Injuries Disablement Benefit Submission (#843)
    * SSCS-8808: IIDB (SYA)- Update: Check your answers page
    
    * SSCS-8808: IIDB (SYA)- Update: Check your answers page
    
    * SSCS-8808: IIDB (SYA)- Update: Check your answers page
    
    * SSCS-8808: IIDB (SYA)- Update: Check your answers page
    
    * SSCS-8933: IIDB - Airlookup, DWP addresses & Robot Configuration (#844)
    
    * SSCS-8933: IIDB - Airlookup, DWP addresses & Robot Configuration
    
    * SSCS-8814: IIDB (SYA)- Update: Check your answers page
    
    * SSCS-8808: IIDB (SYA)- Update: Check your answers page

--- src/main/java/uk/gov/hmcts/reform/sscs/service/DwpAddressLookupService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/DwpAddressLookupService.java
@@ -162,6 +162,10 @@ public class DwpAddressLookupService {
         return dwpMappings.getPip();
     }
 
+    public OfficeMapping[] iidbOfficeMappings() {
+        return dwpMappings.getIidb();
+    }
+
     private Optional<OfficeMapping> getOfficeMappingByDwpIssuingOffice(String dwpIssuingOffice, OfficeMapping[] mappings) {
         return stream(mappings)
                 .filter(office -> equalsAnyIgnoreCase(office.getCode(), stripToEmpty(dwpIssuingOffice)))
