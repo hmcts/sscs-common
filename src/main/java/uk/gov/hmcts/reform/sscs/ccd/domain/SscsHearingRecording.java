0d701df2f71ed83855829af5c2a7e074360efb10
Divya Arun <divya.arun@enginegroup.com>
2021-06-15 01:42:15 +0100

    SSCS-9233: Upload hearing recordings submit event

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsHearingRecording.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Value;
+import org.apache.commons.lang3.builder.CompareToBuilder;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Value
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SscsHearingRecording implements Comparable<SscsHearingRecording> {
+
+    private SscsHearingRecordingDetails value;
+
+    @JsonCreator
+    public SscsHearingRecording(@JsonProperty("value") SscsHearingRecordingDetails value) {
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(SscsHearingRecording o) {
+        return new CompareToBuilder()
+            .append(this.value.getDocumentLink().getDocumentBinaryUrl(),
+                o.getValue().getDocumentLink().getDocumentBinaryUrl())
+            .toComparison();
+    }
+}
