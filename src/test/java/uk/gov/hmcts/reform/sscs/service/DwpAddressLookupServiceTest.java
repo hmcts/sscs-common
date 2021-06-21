3a66f2d5215cfdf94915376b0dacb2236294a66f
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-06-21 15:39:05 +0100

    SSCS-9308 - CCD UI – Setting the ESA and UC dwpRegionalCentre

--- src/test/java/uk/gov/hmcts/reform/sscs/service/DwpAddressLookupServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/DwpAddressLookupServiceTest.java
@@ -292,7 +292,7 @@ public class DwpAddressLookupServiceTest {
     public void givenAEsaBenefitTypeAndDwpOffice_thenCorrectDwpRegionalCenter() {
         String result = dwpAddressLookup.getDwpRegionalCenterByBenefitTypeAndOffice("esa", "Balham DRT");
 
-        assertEquals("Sheffield DRT", result);
+        assertEquals("Balham DRT", result);
     }
 
     @Test
@@ -411,6 +411,13 @@ public class DwpAddressLookupServiceTest {
         Optional<OfficeMapping> result = dwpAddressLookup.getDefaultDwpMappingByBenefitType("maternityAllowance");
 
         assertEquals("Walsall Benefit Centre", result.get().getCode());
+    }
+
+    @Test
+    public void allDwpMappingsHaveADwpRegionCentre() {
+        stream(Benefit.values())
+                .flatMap(benefit-> stream(benefit.getOfficeMappings().apply(dwpAddressLookup)))
+                .forEach(f -> assertNotNull(f.getMapping().getDwpRegionCentre()));
     }
 
 }
