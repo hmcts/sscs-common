350f2ee6f50d600c5bd97d674f1e2c37bf58e0f5
Chris Davidson <christopher.davidson@btinternet.com>
2021-06-28 12:27:52 +0100

    sscs 9225 removed unneeded annotations

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsHearingRecordingCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsHearingRecordingCaseData.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
