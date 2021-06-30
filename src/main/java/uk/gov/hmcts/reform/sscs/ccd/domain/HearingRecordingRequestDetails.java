53b60fd8c0a9d29e68b57d21e89f54efacafb066
Chris Davidson <christopher.davidson@btinternet.com>
2021-06-30 18:03:52 +0100

    sscs 9225 removed duplicate id and added venue name

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRecordingRequestDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRecordingRequestDetails.java
@@ -12,24 +12,18 @@ import lombok.Value;
 @Builder(toBuilder = true)
 public class HearingRecordingRequestDetails {
     String requestingParty;
-    String requestedHearingId;
     String status;
-    String requestedHearingName;
     String dateRequested;
     List<SscsHearingRecording> sscsHearingRecordingList;
 
 
     @JsonCreator
     public HearingRecordingRequestDetails(@JsonProperty("requestingParty") String requestingParty,
-                                   @JsonProperty("requestedHearingId") String requestedHearingId,
                                    @JsonProperty("status") String status,
-                                   @JsonProperty("requestedHearingName") String requestedHearingName,
                                    @JsonProperty("dateRequested") String dateRequested,
                                    @JsonProperty("sscsHearingRecordingList") List<SscsHearingRecording> sscsHearingRecordingList) {
         this.requestingParty = requestingParty;
-        this.requestedHearingId = requestedHearingId;
         this.status = status;
-        this.requestedHearingName = requestedHearingName;
         this.dateRequested = dateRequested;
         this.sscsHearingRecordingList = sscsHearingRecordingList;
     }
