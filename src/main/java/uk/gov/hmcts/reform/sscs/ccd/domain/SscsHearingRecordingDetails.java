e9247eb8cbea719fbba769cd1c5e31a748b3c624
Chris Davidson <christopher.davidson@btinternet.com>
2021-06-30 18:03:52 +0100

    sscs 9225 removed duplicate id and added venue name

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsHearingRecordingDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsHearingRecordingDetails.java
@@ -18,18 +18,21 @@ public class SscsHearingRecordingDetails {
     private String hearingDate;
     private String uploadDate;
     private String hearingId;
+    private String venue;
 
     @JsonCreator
     public SscsHearingRecordingDetails(@JsonProperty("recordings") List<HearingRecordingDetails> recordings,
                                        @JsonProperty("hearingType") String hearingType,
                                        @JsonProperty("hearingDate") String hearingDate,
                                        @JsonProperty("uploadDate") String uploadDate,
-                                       @JsonProperty("hearingId") String hearingId) {
+                                       @JsonProperty("hearingId") String hearingId,
+                                       @JsonProperty("venue") String venue) {
         this.recordings = recordings;
         this.hearingType = hearingType;
         this.hearingDate = hearingDate;
         this.uploadDate = uploadDate;
         this.hearingId = hearingId;
+        this.venue = venue;
     }
 
 }
