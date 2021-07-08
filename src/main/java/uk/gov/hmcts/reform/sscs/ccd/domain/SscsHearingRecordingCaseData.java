80abba21fed5e10e6e2ff8b28e1d75ff74a8978a
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-07-08 16:19:50 +0100

    SSCS-9226: MYA: New 'Requests' Tab

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsHearingRecordingCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsHearingRecordingCaseData.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
