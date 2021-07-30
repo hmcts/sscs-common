a0b05215379c730fd11da4bc712cc5d1722b4fd0
Jason.Hart <jason.hart@enginegroup.com>
2021-07-30 10:24:07 +0100

    SSCS-9309-change to document link

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
@@ -59,7 +59,7 @@ public class SscsCaseData implements CaseData {
     private List<Bundle> caseBundles;
     private List<SscsDocument> sscsDocument;
     private List<SscsDocument> draftSscsDocument;
-    private SscsDocumentDetails withdrawalDocument;
+    private DocumentLink withdrawalDocument;
     private List<SscsFurtherEvidenceDoc> draftSscsFurtherEvidenceDocument;
     private SscsInterlocDecisionDocument sscsInterlocDecisionDocument;
     private SscsInterlocDirectionDocument sscsInterlocDirectionDocument;
