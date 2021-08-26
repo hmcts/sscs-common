dffc418a2047e9d2eafac8e8f5cf3c13d6a32cf2
Mike Lemos <gobii@me.com>
2021-08-26 13:05:16 +0100

    SSCS-9243- adding date approved

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRecordingRequestDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRecordingRequestDetails.java
@@ -3,11 +3,13 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 
-import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Value
