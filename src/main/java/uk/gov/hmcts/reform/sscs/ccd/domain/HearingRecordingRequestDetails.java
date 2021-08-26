e80e423bf554aa7dc08422b0429de37e3488caff
Mike Lemos <gobii@me.com>
2021-08-26 12:12:25 +0100

    SSCS-9243- adding date approved

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRecordingRequestDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRecordingRequestDetails.java
@@ -3,10 +3,11 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Value
@@ -15,17 +16,20 @@ import lombok.Value;
 public class HearingRecordingRequestDetails {
     private String requestingParty;
     private String dateRequested;
+    private String dateApproved;
     private DocumentLink requestDocument;
     private List<SscsHearingRecording> sscsHearingRecordingList;
 
 
     @JsonCreator
     public HearingRecordingRequestDetails(@JsonProperty("requestingParty") String requestingParty,
-                                   @JsonProperty("dateRequested") String dateRequested,
-                                   @JsonProperty("requestDocument") DocumentLink requestDocument,
-                                   @JsonProperty("sscsHearingRecordingList") List<SscsHearingRecording> sscsHearingRecordingList) {
+                                          @JsonProperty("dateRequested") String dateRequested,
+                                          @JsonProperty("dateApproved") String dateApproved,
+                                          @JsonProperty("requestDocument") DocumentLink requestDocument,
+                                          @JsonProperty("sscsHearingRecordingList") List<SscsHearingRecording> sscsHearingRecordingList) {
         this.requestingParty = requestingParty;
         this.dateRequested = dateRequested;
+        this.dateApproved = dateApproved;
         this.requestDocument = requestDocument;
         this.sscsHearingRecordingList = sscsHearingRecordingList;
     }
