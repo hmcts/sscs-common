3c73b516561d4fa57f927434bb81a346c31c4143
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-08-31 16:56:14 +0100

    set unprocessedPostponementRequest flag

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PostponementRequest.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PostponementRequest.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PostponementRequest {
+    YesNo unprocessedPostponementRequest;
     String postponementRequestHearingDateAndTime;
     String postponementRequestHearingVenue;
     String postponementRequestDetails;
