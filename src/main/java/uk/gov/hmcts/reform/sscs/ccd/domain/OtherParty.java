36a6f246f869d78c988a3f3574f33751df4cf1ef
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-11-08 18:25:26 +0000

    SSCS-9714: Child Support - Update Other Party Data Event

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OtherParty.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OtherParty.java
@@ -25,6 +25,7 @@ public class OtherParty {
     private YesNo unacceptableCustomerBehaviour;
     private HearingOptions hearingOptions;
     private HearingSubtype hearingSubtype;
+    private String isAppointee;
     private Appointee appointee;
     private Representative rep;
     private Subscription otherPartySubscription;
