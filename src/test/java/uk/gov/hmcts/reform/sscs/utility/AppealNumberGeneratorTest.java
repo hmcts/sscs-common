5a9182b30f9f25b0cf87938dcbaf52608e977a3f
srochani <sunil.rochani@hotmail.com>
2021-11-08 12:48:03 +0000

    SSCS-7821: MYA – Select case – Display tab for active and non-active cases (#912)

--- src/test/java/uk/gov/hmcts/reform/sscs/utility/AppealNumberGeneratorTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/utility/AppealNumberGeneratorTest.java
@@ -1,19 +1,19 @@
 package uk.gov.hmcts.reform.sscs.utility;
 
 import static org.junit.Assert.*;
-import static uk.gov.hmcts.reform.sscs.utility.AppealNumberGenerator.filterCaseNotDraftOrArchivedDraft;
-import static uk.gov.hmcts.reform.sscs.utility.AppealNumberGenerator.generateAppealNumber;
+import static uk.gov.hmcts.reform.sscs.utility.AppealNumberGenerator.*;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.State;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(JUnitParamsRunner.class)
 public class AppealNumberGeneratorTest {
 
     @Test
@@ -57,11 +57,68 @@ public class AppealNumberGeneratorTest {
     }
 
     @Test
+    @Parameters(method = "generateInvalidStateScenarios")
+    public void filterActiveCasesWithGivenStateThenShouldReturnFalse(State state) {
+        SscsCaseDetails caseDetails = Mockito.mock(SscsCaseDetails.class);
+        Mockito.when(caseDetails.getState()).thenReturn(state.getId());
+        boolean result = filterActiveCasesForCitizen(caseDetails);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void filterActiveCasesWithAppealCreatedStateThenShouldReturnTrue() {
+        SscsCaseDetails caseDetails = Mockito.mock(SscsCaseDetails.class);
+        Mockito.when(caseDetails.getState()).thenReturn(State.APPEAL_CREATED.getId());
+
+        boolean result = filterActiveCasesForCitizen(caseDetails);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void filterDormantCasesWithAppealCreatedStateThenShouldReturnFalse() {
+        SscsCaseDetails caseDetails = Mockito.mock(SscsCaseDetails.class);
+        Mockito.when(caseDetails.getState()).thenReturn(State.DRAFT.getId());
+
+        boolean result = filterDormantCasesForCitizen(caseDetails);
+
+        assertFalse(result);
+    }
+
+    @Test
+    @Parameters(method = "generateDormantVoidStateScenarios")
+    public void filterDormantCasesWithGivenStateThenShouldReturnTrue(State state) {
+        SscsCaseDetails caseDetails = Mockito.mock(SscsCaseDetails.class);
+        Mockito.when(caseDetails.getState()).thenReturn(state.getId());
+
+        boolean result = filterDormantCasesForCitizen(caseDetails);
+
+        assertTrue(result);
+    }
+
+    @Test
     public void shouldReturnTrueWhereCaseIsNull() {
         SscsCaseDetails caseDetails = null;
 
         boolean result = filterCaseNotDraftOrArchivedDraft(caseDetails);
 
         assertTrue(result);
+    }
+
+    private Object[] generateInvalidStateScenarios() {
+        return new Object[]{
+            State.DORMANT_APPEAL_STATE,
+            State.VOID_STATE,
+            State.DRAFT,
+            State.DRAFT_ARCHIVED
+        };
+    }
+
+    private Object[] generateDormantVoidStateScenarios() {
+        return new Object[]{
+            State.DORMANT_APPEAL_STATE,
+            State.VOID_STATE
+        };
     }
 }
