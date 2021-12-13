01bd0aead15cec8c5d6b14f31f2818ba1fe34274
Mike Lemos <gobii@me.com>
2021-12-13 15:45:34 +0000

    SSCS-9871 added other party reissue options

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OtherPartyOptionDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OtherPartyOptionDetails.java
@@ -13,13 +13,16 @@ import lombok.Data;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OtherPartyOptionDetails {
 
-    private String otherPartyName;
+    private String otherPartyOptionId;
+    private String otherPartyOptionName;
     private YesNo resendToOtherParty;
 
     @JsonCreator
-    public OtherPartyOptionDetails(@JsonProperty("otherPartyName") String otherPartyName,
+    public OtherPartyOptionDetails(@JsonProperty("otherPartyOptionId") String otherPartyOptionId,
+                                   @JsonProperty("otherPartyOptionName") String otherPartyOptionName,
                                    @JsonProperty("resendToOtherParty") YesNo resendToOtherParty) {
-        this.otherPartyName = otherPartyName;
+        this.otherPartyOptionId = otherPartyOptionId;
+        this.otherPartyOptionName = otherPartyOptionName;
         this.resendToOtherParty = resendToOtherParty;
     }
 }
