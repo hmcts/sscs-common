f905bb2089600579fca1206025a33b7a9ab2f189
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2022-01-17 12:25:23 +0000

    SSCS-9903 Location data – Populate location data on creation SYA and case update

--- src/main/java/uk/gov/hmcts/reform/sscs/model/CourtVenue.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/CourtVenue.java
@@ -2,12 +2,16 @@ package uk.gov.hmcts.reform.sscs.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourtVenue {
     @JsonProperty("court_venue_id")
     private String courtVenueId;
