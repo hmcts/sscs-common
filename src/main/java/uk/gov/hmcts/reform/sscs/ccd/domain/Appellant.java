5dd6f96418e2207d860efec1b8e92d4588045157
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-02-23 14:36:20 +0000

    SSCS-10056 Party Abstract

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Appellant.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Appellant.java
@@ -1,44 +1,18 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-@Builder(toBuilder = true)
-public class Appellant {
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonInclude
+public class Appellant extends Party {
 
-    private Name name;
-    private Address address;
-    private Contact contact;
-    private Identity identity;
-    private String isAppointee;
-    private Appointee appointee;
-    private String isAddressSameAsAppointee;
-    private YesNo confidentialityRequired;
-    private Role role;
-
-    @JsonCreator
-    public Appellant(@JsonProperty("name") Name name,
-                     @JsonProperty("address") Address address,
-                     @JsonProperty("contact") Contact contact,
-                     @JsonProperty("identity") Identity identity,
-                     @JsonProperty("isAppointee") String isAppointee,
-                     @JsonProperty("appointee") Appointee appointee,
-                     @JsonProperty("isAddressSameAsAppointee") String isAddressSameAsAppointee,
-                     @JsonProperty("confidentialityRequired") YesNo confidentialityRequired,
-                     @JsonProperty("role") Role role) {
-        this.name = name;
-        this.address = address;
-        this.contact = contact;
-        this.identity = identity;
-        this.isAppointee = isAppointee;
-        this.appointee = appointee;
-        this.isAddressSameAsAppointee = isAddressSameAsAppointee;
-        this.confidentialityRequired = confidentialityRequired;
-        this.role = role;
-    }
 }
