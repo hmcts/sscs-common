e32381262a35fca4ca9870dbdb6d534c2f1428cd
valantinas <rokas.valantinas@hmcts.net>
2022-03-11 12:26:25 +0000

    SSCS-10218 Add caseworker reference data API feign clients

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/client/CaseWorkerRefUsersDataApi.java
@@ -0,0 +1,63 @@
+package uk.gov.hmcts.reform.sscs.client;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import uk.gov.hmcts.reform.sscs.model.CaseWorkerProfilesDeletionResponse;
+import uk.gov.hmcts.reform.sscs.model.CaseWorkersProfileCreationRequest;
+import uk.gov.hmcts.reform.sscs.model.UserRequest;
+
+
+@FeignClient(
+    name = "caseworker-ref-data-api",
+    url = "${caseworker-ref.api.url}"
+)
+public interface CaseWorkerRefUsersDataApi {
+    String SERVICE_AUTHORIZATION = "serviceAuthorization";
+
+    @RequestMapping(
+        method = RequestMethod.POST,
+        value = "refdata/case-worker/users",
+        headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<Object> createCaseWorkerProfiles(
+        @RequestHeader(AUTHORIZATION) String authorisation,
+        @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
+        @RequestBody List<CaseWorkersProfileCreationRequest>
+            caseWorkersProfileCreationRequest
+    );
+
+
+    @RequestMapping(
+        method = RequestMethod.POST,
+        value = "refdata/case-worker/users/fetchUsersById",
+        headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<Object> fetchCaseworkersById(
+        @RequestHeader(AUTHORIZATION) String authorisation,
+        @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
+        @RequestBody UserRequest userRequest
+    );
+
+
+    @RequestMapping(
+        method = RequestMethod.DELETE,
+        value = "refdata/case-worker/users",
+        headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<CaseWorkerProfilesDeletionResponse> deleteCaseWorkerProfileByIdOrEmailPattern(
+        @RequestHeader(AUTHORIZATION) String authorisation,
+        @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
+        @RequestParam(value = "userId", required = false) String userId,
+        @RequestParam(value = "emailPattern", required = false) String emailPattern
+    );
+}
---
+++ src/test/java/uk/gov/hmcts/reform/sscs/utility/TrimStringFieldsTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.sscs.utility;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+
+class TrimStringFieldsTest {
+
+    TrimStringFields underTest = new TrimStringFields();
+    JsonParser jp = mock(JsonParser.class);
+    DeserializationContext dc = mock(DeserializationContext.class);
+
+    @Test
+    void shouldTrimStringsFields() throws IOException {
+
+        // given
+        JsonNode jsonNode = new TextNode("test   ");
+        when(jp.readValueAsTree()).thenReturn(jsonNode);
+
+        // when
+        String deserializedText = (String) underTest.deserialize(jp, dc);
+
+        // then
+        assertThat(deserializedText).isEqualTo("test");
+    }
+}
