3642034fba14822a7ea0ca836c218c27c3c35ba9
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-03-14 16:14:39 +0000

    SSCS-10222 Remove redunant code from both enums

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRoute.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRoute.java
@@ -1,22 +1,12 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 
-@Getter
-@RequiredArgsConstructor
-@FieldDefaults(makeFinal=true, level= AccessLevel.PRIVATE)
 public enum HearingRoute {
 
-    @JsonEnumDefaultValue
     @JsonProperty
-    LIST_ASSIST("listAssist"),
+    LIST_ASSIST,
     @JsonProperty
-    GAPS("gaps");
+    GAPS
 
-    String type;
 }
