14ae0e8509c1139188ba1ca66b43bfa35110d9f4
Del Attipoe <delali.attipoe@hmcts.net>
2022-03-15 16:22:16 +0000

    SSCS-10220 Add Service Bus Stream functionality.

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/hearings/HearingRequest.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.sscs.model.hearings;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import uk.gov.hmcts.reform.sscs.ccd.domain.HearingRoute;
+import uk.gov.hmcts.reform.sscs.ccd.domain.HearingState;
+import uk.gov.hmcts.reform.sscs.model.servicebus.SessionAwareRequest;
+
+@Data
+@Builder(builderMethodName = "internalBuilder")
+@RequiredArgsConstructor
+public class HearingRequest implements SessionAwareRequest {
+
+    @NonNull
+    private final String ccdCaseId;
+
+    private final HearingRoute hearingRoute;
+
+    private final HearingState hearingState;
+
+    public static HearingRequestBuilder builder(String ccdCaseId) {
+        return internalBuilder().ccdCaseId(ccdCaseId);
+    }
+
+    @Override
+    @JsonIgnore
+    public String getSessionId() {
+        return ccdCaseId;
+    }
+
+    public static class HearingRequestBuilder {}
+}
