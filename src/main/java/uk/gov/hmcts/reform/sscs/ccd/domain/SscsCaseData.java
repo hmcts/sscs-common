1cef56580f21948bf573a3dc81a5dc7ef9e4e709
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-03-16 14:19:07 +0000

    SSCS-10056 Fixing Typo

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
@@ -399,7 +399,7 @@ public class SscsCaseData implements CaseData {
     }
 
     @JsonIgnore
-    public boolean isJointPartyAddressSameAsAppeallant() {
+    public boolean isJointPartyAddressSameAsAppellant() {
         return stringToBoolean(jointPartyAddressSameAsAppellant);
     }
 
