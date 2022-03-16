09a8253e383d12aa1e183320b9d19952a5e81797
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-03-16 14:04:02 +0000

    SSCS-10222 Remove unused imports

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/robotics/RoboticsCallbackHandler.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.sscs.robotics;
+
+import static java.util.Objects.requireNonNull;
+
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+
+public class RoboticsCallbackHandler {
+
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        requireNonNull(callback, "callback must not be null");
+        requireNonNull(callbackType, "callbacktype must not be null");
+
+        return callbackType.equals(CallbackType.ABOUT_TO_SUBMIT) && callback.getEvent() == EventType.RESEND_CASE_TO_GAPS2;
+    }
+
+    public void handle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+    }
+}
