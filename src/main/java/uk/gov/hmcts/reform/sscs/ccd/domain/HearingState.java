426c941567314f6503e7a695cf5d891b57af0ace
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-03-16 13:49:36 +0000

    SSCS-10222 Update hearing state

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingState.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingState.java
@@ -1,10 +1,32 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public enum HearingState {
 
     @JsonProperty
-    HEARING_CREATED
+    CREATE_HEARING("createHearing", "Create Hearing"),
+    @JsonProperty
+    UPDATED_CASE("updatedCase", "Case Updated"),
+    @JsonProperty
+    UPDATE_HEARING("updateHearing", "Update Hearing"),
+    @JsonProperty
+    CANCEL_HEARING("cancelHearing", "Cancel Hearing"),
+    @JsonProperty
+    PARTY_NOTIFIED("partyNotified", "Parties Notified");
+
+    private String state;
+    private String description;
+
+    @JsonValue
+    public String getState() {
+        return state;
+    }
 
 }
