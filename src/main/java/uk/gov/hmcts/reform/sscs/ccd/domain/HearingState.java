13ce573908b5582b67851836cb33fa10aa57d14f
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-03-17 16:42:59 +0000

    SSCS-10222 Remove excess code again

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingState.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingState.java
@@ -1,31 +1,11 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum HearingState {
 
-    @JsonProperty
-    CREATE_HEARING("createHearing", "Create Hearing"),
-    @JsonProperty
-    UPDATED_CASE("updatedCase", "Case Updated"),
-    @JsonProperty
-    UPDATE_HEARING("updateHearing", "Update Hearing"),
-    @JsonProperty
-    CANCEL_HEARING("cancelHearing", "Cancel Hearing"),
-    @JsonProperty
-    PARTY_NOTIFIED("partyNotified", "Parties Notified");
-
-    private final String state;
-    private final String description;
-
-    @JsonValue
-    public String getState() {
-        return state;
-    }
+    CREATE_HEARING,
+    UPDATED_CASE,
+    UPDATE_HEARING,
+    CANCEL_HEARING,
+    PARTY_NOTIFIED
 
 }
