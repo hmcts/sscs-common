3e23651f76839f88a6038c01175d29290a02c374
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-03-18 11:13:16 +0000

    SSCS-10222 Add state to enum value

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRoute.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingRoute.java
@@ -1,8 +1,24 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum HearingRoute {
 
-    LIST_ASSIST,
-    GAPS
+    @JsonProperty
+    LIST_ASSIST("listAssist"),
+    @JsonProperty
+    GAPS("gaps");
+
+    private final String state;
+
+    @JsonValue
+    public String getState() {
+        return state;
+    }
 
 }
