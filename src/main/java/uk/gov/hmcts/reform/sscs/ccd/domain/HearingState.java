8265b0eaaced5aea33ed20638c1a87f4d257f1da
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-03-18 11:13:16 +0000

    SSCS-10222 Add state to enum value

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingState.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingState.java
@@ -1,11 +1,30 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum HearingState {
 
-    CREATE_HEARING,
-    UPDATED_CASE,
-    UPDATE_HEARING,
-    CANCEL_HEARING,
-    PARTY_NOTIFIED
+    @JsonProperty
+    CREATE_HEARING("createHearing"),
+    @JsonProperty
+    UPDATED_CASE("updatedCase"),
+    @JsonProperty
+    UPDATE_HEARING("updateHearing"),
+    @JsonProperty
+    CANCEL_HEARING("cancelHearing"),
+    @JsonProperty
+    PARTY_NOTIFIED("partyNotified");
+
+    private final String state;
+
+    @JsonValue
+    public String getState() {
+        return state;
+    }
 
 }
