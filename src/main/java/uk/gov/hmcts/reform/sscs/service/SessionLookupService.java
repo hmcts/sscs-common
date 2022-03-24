a98a660cf8dcaf1b0967f5587139afbf70bdc8af
suvarnathekkoot <suvarna.thekkoot@HMCTS.NET>
2022-03-24 08:44:46 +0000

    Fixing checkstyle warnings

--- src/main/java/uk/gov/hmcts/reform/sscs/service/SessionLookupService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/SessionLookupService.java
@@ -1,24 +1,25 @@
 package uk.gov.hmcts.reform.sscs.service;
 
+import static com.google.common.collect.Maps.newHashMap;
+
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.sscs.model.CaseCodeMappingDetails;
-
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.model.CaseCodeMappingDetails;
 
-import static com.google.common.collect.Maps.newHashMap;
 
 @Service
 @Slf4j
 public class SessionLookupService {
+
     private static final String CSV_FILE_PATH = "reference-data/casecodeDetails.csv";
     private final Map<String, CaseCodeMappingDetails> caseCodeDetailsMap = newHashMap();
 
@@ -26,7 +27,6 @@ public class SessionLookupService {
     protected void init() {
         InputStream is = getClass().getClassLoader().getResourceAsStream(CSV_FILE_PATH);
         try (CSVReader reader = new CSVReader(new InputStreamReader(is))) {
-
             List<String[]> linesList = reader.readAll();
             linesList.forEach(line -> {
                 CaseCodeMappingDetails caseCodeDetails = CaseCodeMappingDetails.builder()
@@ -41,7 +41,7 @@ public class SessionLookupService {
                         .panelMembers(line[8])
                         .build();
                 caseCodeDetailsMap.put(line[0], caseCodeDetails);
-                    }
+            }
             );
         } catch (IOException e) {
             log.error("Error occurred while loading the casecodeDetails  reference data file: " + CSV_FILE_PATH + e);
