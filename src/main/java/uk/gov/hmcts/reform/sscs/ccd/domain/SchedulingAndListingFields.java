ec71f1b0fb698f05f57a758d8538d548a348ec94
Del Attipoe <delali.attipoe@hmcts.net>
2022-03-28 17:44:32 +0100

    SSCS-10321 Add active hearing fields.

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SchedulingAndListingFields.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SchedulingAndListingFields {
+    //private HearingRoute hearingRoute; //Will be added once sscs-10220 is merged---
+    //private HearingState hearingState;
+    private Long activeHearingId;
+
+    private Long activeHearingVersionNumber;
+}
--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
@@ -341,6 +341,10 @@ public class SscsCaseData implements CaseData {
     @Getter(AccessLevel.NONE)
     private WorkAllocationFields workAllocationFields;
 
+    @JsonUnwrapped
+    @Getter(AccessLevel.NONE)
+    private SchedulingAndListingFields schedulingAndListingFields;
+
     @JsonIgnore
     private EventDetails getLatestEvent() {
         return events != null && !events.isEmpty() ? events.get(0).getValue() : null;
@@ -586,6 +590,15 @@ public class SscsCaseData implements CaseData {
         }
         return workAllocationFields;
     }
+
+    @JsonIgnore
+    public SchedulingAndListingFields getSchedulingAndListingFields() {
+        if (schedulingAndListingFields == null) {
+            this.schedulingAndListingFields = new SchedulingAndListingFields();
+        }
+        return schedulingAndListingFields;
+    }
+
 
     @JsonIgnore
     public Optional<Benefit> getBenefitType() {
