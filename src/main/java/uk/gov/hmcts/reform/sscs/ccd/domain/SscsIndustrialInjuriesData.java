b17320f4252d461df26390a81e223271a1483cee
suetlaiHMCTS <suetlai.chan@hmcts.net>
2022-03-29 11:01:43 +0100

    SSCS-10146 remove unuse library

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsIndustrialInjuriesData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsIndustrialInjuriesData.java
@@ -1,9 +1,7 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
