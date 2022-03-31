9af07a35fcd552a7d2c2c6beb89772a39f91050a
Del Attipoe <del.attipoe@version1.com>
2022-03-31 19:39:50 +0100

    SSCS-10220 Exclude jackson XML dataformat from Azure dependencies.

--- src/main/java/uk/gov/hmcts/reform/sscs/model/servicebus/SessionAwareServiceBusMessagingService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/servicebus/SessionAwareServiceBusMessagingService.java
@@ -3,24 +3,22 @@ package uk.gov.hmcts.reform.sscs.model.servicebus;
 import com.azure.messaging.servicebus.ServiceBusMessage;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import uk.gov.hmcts.reform.sscs.service.servicebus.SessionAwareMessagingService;
 
 @Slf4j
+@RequiredArgsConstructor
 public class SessionAwareServiceBusMessagingService implements SessionAwareMessagingService {
 
     private final ServiceBusSenderClient senderClient;
 
-    public SessionAwareServiceBusMessagingService(
-        ServiceBusSenderClient senderClient) {
-        this.senderClient = senderClient;
-    }
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public boolean sendMessage(SessionAwareRequest message) {
 
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
 
             ServiceBusMessage serviceBusMessage = new ServiceBusMessage(objectMapper.writeValueAsString(message));
             serviceBusMessage.setSessionId(message.getSessionId());
