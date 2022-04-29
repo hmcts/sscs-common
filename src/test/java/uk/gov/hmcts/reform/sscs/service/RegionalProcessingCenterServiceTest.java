8e7c498c8e329a3b7f39d806b27a397043fefc08
Del Attipoe <delali.attipoe@hmcts.net>
2022-04-29 17:49:03 +0100

    SSCS-10223 Add Leeds RPC

--- src/test/java/uk/gov/hmcts/reform/sscs/service/RegionalProcessingCenterServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/RegionalProcessingCenterServiceTest.java
@@ -75,7 +75,6 @@ public class RegionalProcessingCenterServiceTest {
             .phoneNumber("0300 123 1142")
             .faxNumber("0870 324 0109")
             .email("Liverpool_SYA_Resp@justice.gov.uk")
-            .hearingRoute(HearingRoute.GAPS)
             .build();
 
         RegionalProcessingCenter cardiffRpc = RegionalProcessingCenter.builder()
@@ -89,7 +88,6 @@ public class RegionalProcessingCenterServiceTest {
             .phoneNumber("0300 123 1142")
             .faxNumber("0870 739 4438")
             .email("Cardiff_SYA_Respon@justice.gov.uk")
-            .hearingRoute(HearingRoute.GAPS)
             .build();
 
 
@@ -119,7 +117,6 @@ public class RegionalProcessingCenterServiceTest {
         assertEquals("0300 123 1142", regionalProcessingCenter.getPhoneNumber());
         assertEquals("0870 324 0109", regionalProcessingCenter.getFaxNumber());
         assertEquals("Liverpool_SYA_Resp@justice.gov.uk", regionalProcessingCenter.getEmail());
-        assertEquals("GAPS", regionalProcessingCenter.getHearingRoute().toString().toUpperCase());
     }
 
     @Test
