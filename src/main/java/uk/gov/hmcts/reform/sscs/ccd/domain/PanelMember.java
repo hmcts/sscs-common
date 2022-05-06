d6f3ba87c8535a819d03473b4e572c7a6b79f00f
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-05-06 15:10:20 +0100

    SSCS-10116 Default Business Rules

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMember.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import static uk.gov.hmcts.reform.sscs.ccd.domain.PanelMemberMedicallyQualified.getPanelMemberMedicallyQualified;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum PanelMember {
+    // TODO These are based on mock data and subject to change.
+    SALARIED_JUDGE("BBA3-?","Salaried Judge"),
+    JUDGE("BBA3-?","Judge"),
+    DOCTOR("BBA3-?","Doctor"),
+    SPECIALIST_MEMBER("BBA3-?","Specialist Member"),
+    DQPM("BBA3-DQPM", "Disability Qualified Panel Member"),
+    MQPM1("BBA3-MQPM1", "Medically Qualified Panel Member"),
+    MQPM2("BBA3-MQPM2", "Medically Qualified Panel Member"),
+    FQPM("BBA3-FQPM", "Financially Qualified Panel Member"),
+    RMM("BBA3-RMM", "Regional Medical Member");
+
+    private final String reference;
+    private final String description;
+
+    public String getReference(String panelMemberSubtypeCcdRef) {
+        PanelMemberMedicallyQualified subType = getPanelMemberMedicallyQualified(panelMemberSubtypeCcdRef);
+        return String.format("%s-%s", reference, subType.getHmcReference());
+    }
+}
