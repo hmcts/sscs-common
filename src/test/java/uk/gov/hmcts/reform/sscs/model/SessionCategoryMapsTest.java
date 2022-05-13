2114cbf0d1e06ece8411bae3abadc4b50d7a0add
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-05-13 12:03:48 +0100

    SSCS-10116 Component Refactor

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/model/SessionCategoryMapsTest.java
@@ -0,0 +1,99 @@
+package uk.gov.hmcts.reform.sscs.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import uk.gov.hmcts.reform.sscs.ccd.domain.BenefitCode;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Issue;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SessionCategory;
+import uk.gov.hmcts.reform.sscs.service.SessionCategoryMapsComponent;
+
+public class SessionCategoryMapsTest {
+
+    SessionCategoryMapsComponent sessionCategoryMaps = new SessionCategoryMapsComponent();
+
+    @DisplayName("When valid Benefit Code and Issue Code is given to getHearingDuration the valid mapping is returned")
+    @Test
+    public void getSessionCategory() {
+        SessionCategoryMap sessionCategory = sessionCategoryMaps
+                .getSessionCategory("003", "LE", false, false);
+
+        assertThat(sessionCategory).isNotNull();
+        assertThat(sessionCategory.getBenefitCode()).isEqualTo(BenefitCode.PIP_REASSESSMENT_CASE);
+        assertThat(sessionCategory.getIssue()).isEqualTo(Issue.LE);
+        assertThat(sessionCategory.getCategory()).isEqualTo(SessionCategory.CATEGORY_03);
+    }
+
+    @DisplayName("When valid Benefit Code, Issue Code and a secondDoctor is given to getHearingDuration"
+            + "the valid mapping is returned")
+    @Test
+    public void getSessionCategorySecondDoctor() {
+        SessionCategoryMap sessionCategory = sessionCategoryMaps
+                .getSessionCategory("064", "GC", true, false);
+
+        assertThat(sessionCategory).isNotNull();
+        assertThat(sessionCategory.getBenefitCode()).isEqualTo(BenefitCode.INDUSTRIAL_DEATH_BENEFIT);
+        assertThat(sessionCategory.getIssue()).isEqualTo(Issue.GC);
+        assertThat(sessionCategory.getCategory()).isEqualTo(SessionCategory.CATEGORY_06);
+    }
+
+    @DisplayName("When valid Benefit Code, Issue Code and no secondDoctor is given to getHearingDuration"
+            + "the valid mapping is returned")
+    @Test
+    public void getSessionCategoryNoSecondDoctor() {
+        SessionCategoryMap sessionCategory = sessionCategoryMaps
+                .getSessionCategory("064", "GC", false, false);
+
+        assertThat(sessionCategory).isNotNull();
+        assertThat(sessionCategory.getBenefitCode()).isEqualTo(BenefitCode.INDUSTRIAL_DEATH_BENEFIT);
+        assertThat(sessionCategory.getIssue()).isEqualTo(Issue.GC);
+        assertThat(sessionCategory.getCategory()).isEqualTo(SessionCategory.CATEGORY_05);
+    }
+
+    @DisplayName("When valid Benefit Code, Issue Code but secondDoctor shouldn't be given to getHearingDuration"
+            + "null is returned")
+    @Test
+    public void getSessionCategorySecondDoctorInvalid() {
+        SessionCategoryMap sessionCategory = sessionCategoryMaps
+                .getSessionCategory("003", "LE", true, false);
+
+        assertThat(sessionCategory).isNull();
+    }
+
+    @DisplayName("When valid Benefit Code, Issue Code and a secondDoctor is given to getHearingDuration"
+            + "the valid mapping is returned")
+    @Test
+    public void getSessionCategoryFqpmRequired() {
+        SessionCategoryMap sessionCategory = sessionCategoryMaps
+                .getSessionCategory("023", "RB", false, true);
+
+        assertThat(sessionCategory).isNotNull();
+        assertThat(sessionCategory.getBenefitCode()).isEqualTo(BenefitCode.CHILD_SUPPORT_REFORMS);
+        assertThat(sessionCategory.getIssue()).isEqualTo(Issue.RB);
+        assertThat(sessionCategory.getCategory()).isEqualTo(SessionCategory.CATEGORY_02);
+    }
+
+    @DisplayName("When valid Benefit Code, Issue Code and no secondDoctor is given to getHearingDuration"
+            + "the valid mapping is returned")
+    @Test
+    public void getSessionCategoryFqpmNotRequired() {
+        SessionCategoryMap sessionCategory = sessionCategoryMaps
+                .getSessionCategory("023", "RB", false, false);
+
+        assertThat(sessionCategory).isNotNull();
+        assertThat(sessionCategory.getBenefitCode()).isEqualTo(BenefitCode.CHILD_SUPPORT_REFORMS);
+        assertThat(sessionCategory.getIssue()).isEqualTo(Issue.RB);
+        assertThat(sessionCategory.getCategory()).isEqualTo(SessionCategory.CATEGORY_01);
+    }
+
+    @DisplayName("When valid Benefit Code, Issue Code but fqpmRequired shouldn't be given to getHearingDuration"
+            + "null is returned")
+    @Test
+    public void getSessionCategoryFqpmRequiredInvalid() {
+        SessionCategoryMap sessionCategoryMap = sessionCategoryMaps
+                .getSessionCategory("003", "LE", false, true);
+
+        assertThat(sessionCategoryMap).isNull();
+    }
+}
