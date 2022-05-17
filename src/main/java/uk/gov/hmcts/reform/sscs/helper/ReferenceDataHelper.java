28613454a7368cf856dc777bf06d4e53f2601ed5
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-05-17 10:48:30 +0100

    SSCS-10116 Enhancing Error and Test Logs

--- src/main/java/uk/gov/hmcts/reform/sscs/helper/ReferenceDataHelper.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/helper/ReferenceDataHelper.java
@@ -1,11 +1,16 @@
 package uk.gov.hmcts.reform.sscs.helper;
 
+import static java.util.Collections.nCopies;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.collect.Maps;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import uk.gov.hmcts.reform.sscs.exception.ReferenceDataImportException;
 import uk.gov.hmcts.reform.sscs.utility.JsonDataReader;
@@ -28,6 +33,29 @@ public final class ReferenceDataHelper {
     }
 
     public static <T> Map<T, T> generateHashMap(Collection<T> collection) {
-        return Maps.uniqueIndex(collection, reference -> reference);
+        try {
+            return Maps.uniqueIndex(collection, reference -> reference);
+        } catch (IllegalArgumentException exception) {
+            throw new ReferenceDataImportException(String
+                    .format("Reference Data Invalid, there are the following duplicates:%n%s%nError Message: %s",
+                            getDuplicatesString(collection), exception.getMessage()), exception);
+        }
+
+    }
+
+    public static <T> String getDuplicatesString(Collection<T> collection) {
+        return getDuplicates(collection)
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.joining("\n - ", " - ", ""));
+    }
+
+    public static <T> List<T> getDuplicates(Collection<T> collection) {
+        return collection.stream()
+                .collect(groupingBy(identity(), counting()))
+                .entrySet().stream()
+                .filter(n -> n.getValue() > 1)
+                .flatMap(n -> nCopies(n.getValue().intValue(), n.getKey()).stream())
+                .collect(toList());
     }
 }
