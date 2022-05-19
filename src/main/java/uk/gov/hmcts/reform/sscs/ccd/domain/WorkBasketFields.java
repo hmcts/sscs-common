e0f2cf3bc35ea66f612e70431a298b53b992855d
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-05-19 15:40:10 +0100

    SSCS-10242 WorkBasketFields

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/WorkBasketFields.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkBasketFields {
+    // TODO Find out if its a good idea/worth to moving the other search fields here?
+
+    @JsonProperty("workBasketHearingDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate hearingDate;
+}
