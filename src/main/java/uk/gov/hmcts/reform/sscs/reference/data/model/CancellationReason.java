9651271dbbedd38c31e64e423d7f3d48b728397d
Benson Li <benson.li@evoco.co.uk>
2022-05-23 09:41:15 +0100

    SSCS-10286 Previous Work
    Update for the comment
    
    add a function for get cancellation reason by value
    
    Update for formatting
    
    add cancellation reason
    
    cherry pick commits onto a fresh branch
    
    update for the comment
    
    Update for the comment
    
    add a function for get cancellation reason by value
    
    Update for formatting
    
    add cancellation reason

--- src/main/java/uk/gov/hmcts/reform/sscs/reference/data/model/CancellationReason.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/reference/data/model/CancellationReason.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sscs.reference.data.model;
 
+import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,14 +8,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CancellationReason {
 
-    WITHDRAWN("BBA3-withdrawn", "Withdrawn"),
-    STRUCK_OUT("BBA3-struckOut", "Struck Out"),
-    PARTY_UNABLE_TO_ATTEND("BBA3-partyUnableToAttend", "Party unable to attend"),
     EXCLUSION("BBA3-exclusion", "Exclusion"),
+    INCOMPLETE_TRIBUNAL("BBA3-incompleteTribunal", "Incomplete Tribunal"),
     LAPSED("BBA3-lapsed", "Lapsed"),
-    OTHER("BBA3-other", "Other");
+    LISTED_IN_ERROR("BBA3-listedInError", "Listed In Error"),
+    OTHER("BBA3-other", "Other"),
+    PARTY_DID_NOT_ATTEND("BBA3-partyDidNotAttend", "Party Did Not Attend"),
+    PARTY_UNABLE_TO_ATTEND("BBA3-partyUnableToAttend", "Party unable to attend"),
+    STRUCK_OUT("BBA3-struckOut", "Struck Out"),
+    WITHDRAWN("BBA3-withdrawn", "Withdrawn");
 
     private final String hmcReference;
-    private final String value;
+    private final String label;
+
+    public static CancellationReason getCancellationReasonByHmcReference(String value) {
+        return Arrays.stream(CancellationReason.values())
+                .filter(sl -> sl.getHmcReference().equals(value))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static CancellationReason getCancellationReasonByLabel(String value) {
+        return Arrays.stream(CancellationReason.values())
+                .filter(sl -> sl.getLabel().equals(value))
+                .findFirst()
+                .orElse(null);
+    }
 
 }
