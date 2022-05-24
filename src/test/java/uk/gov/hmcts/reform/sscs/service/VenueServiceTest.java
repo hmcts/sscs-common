b07bb3138fe0cc2406840901ef1cedd6dc40d7ce
Waqas Arshid <waqas.arshid@hmcts.net>
2022-05-24 16:51:41 +0100

    SSCS-10517 added new VenueService method to get venue by postcode

--- src/test/java/uk/gov/hmcts/reform/sscs/service/VenueServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/VenueServiceTest.java
@@ -19,27 +19,35 @@ public class VenueServiceTest {
     public static final String PROCESSING_VENUE_1 = "test_place";
     public static final String PROCESSING_VENUE_2 = "test_other_place";
 
-    @InjectMocks
-    private VenueService venueService;
-
     @Mock
     private VenueDataLoader venueDataLoader;
 
     @Mock
     private AirLookupService airLookupService;
+
+    @InjectMocks
+    private VenueService venueService;
     
     @Test
     public void getHearingLocations_shouldReturnCorrespondingEpimsIdForVenue() {
-
         setupVenueMaps();
 
         Optional<String> result = venueService.getEpimsIdForVenue(PROCESSING_VENUE_1);
 
         assertThat(result).isPresent();
-
         String epimsId = result.get();
+        assertThat(epimsId).isEqualTo("987632");
+    }
 
-        assertThat(epimsId).isEqualTo("9876");
+    @Test
+    public void givenRpcPostcode_shouldReturnCorrespondingEpimsIdForVenue() {
+        setupVenueMaps();
+
+        Optional<String> result = venueService.getEpimsIdForVenueByPostcode("LN5 7PS");
+
+        assertThat(result).isPresent();
+        String epimsId = result.get();
+        assertThat(epimsId).isEqualTo("233333");
     }
 
     private void setupVenueMaps() {
@@ -48,14 +56,23 @@ public class VenueServiceTest {
 
         Map<String, VenueDetails> venueDetailsMap = Map.of(
             "68", VenueDetails.builder()
-                .epimsId("9876")
+                .epimsId("987632")
                 .build(),
             "2", VenueDetails.builder()
-                .epimsId("1111")
+                .epimsId("111111")
+                .build());
+
+        Map<String, VenueDetails> venueDetailsMapByPostcode = Map.of(
+            "LN5 7PS", VenueDetails.builder()
+                .epimsId("233333")
+                .build(),
+            "MK9 2AJ", VenueDetails.builder()
+                .epimsId("111111")
                 .build());
 
         when(airLookupService.getLookupVenueIdByAirVenueName()).thenReturn(venueIdMap);
         when(venueDataLoader.getVenueDetailsMap()).thenReturn(venueDetailsMap);
+        when(venueDataLoader.getVenueDetailsMapByPostcode()).thenReturn(venueDetailsMapByPostcode);
     }
 
 }
