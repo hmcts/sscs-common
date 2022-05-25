47a1c1df97c344a69c09328023f37c47f4a3043a
Del Attipoe <delali.attipoe@hmcts.net>
2022-05-25 13:17:40 +0100

    Replace gson with Jackson.

--- src/main/java/uk/gov/hmcts/reform/sscs/model/dwp/OfficeAddress.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/dwp/OfficeAddress.java
@@ -1,10 +1,16 @@
 package uk.gov.hmcts.reform.sscs.model.dwp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OfficeAddress {
     private String line1;
     private String line2;
