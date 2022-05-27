9eebf84de22997d4db9671f49ec526397ddcbe3d
Lucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-05-27 18:18:01 +0100

    SSCS-10286 Adding getVenuesByEpims methods

--- src/main/java/uk/gov/hmcts/reform/sscs/service/VenueDataLoader.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/VenueDataLoader.java
@@ -1,14 +1,20 @@
 package uk.gov.hmcts.reform.sscs.service;
 
 import static com.google.common.collect.Maps.newHashMap;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.isYes;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -22,6 +28,7 @@ public class VenueDataLoader {
 
     private static final String CSV_FILE_PATH = "reference-data/sscs-venues.csv";
     private final Map<String, VenueDetails> venueDetailsMap = newHashMap();
+    private final Map<String, List<VenueDetails>> venueDetailsEpimsMap = newHashMap();
     private final Map<String, VenueDetails> venueDetailsMapByVenueName = newHashMap();
 
     @PostConstruct
@@ -31,26 +38,27 @@ public class VenueDataLoader {
 
             List<String[]> linesList = reader.readAll();
             linesList.forEach(line -> {
-                VenueDetails venueDetails = VenueDetails.builder()
-                        .venueId(line[0])
-                        .threeDigitReference(line[1])
-                        .regionalProcessingCentre(line[2])
-                        .venName(line[3])
-                        .venAddressLine1(line[4])
-                        .venAddressLine2(line[5])
-                        .venAddressTown(line[6])
-                        .venAddressCounty(line[7])
-                        .venAddressPostcode(line[8])
-                        .venAddressTelNo(line[9])
-                        .districtId(line[10])
-                        .url(line[11])
-                        .active(line[12])
-                        .gapsVenName(line[13])
-                        .comments(line[14])
-                        .epimsId(line[15])
-                        .build();
-                venueDetailsMap.put(line[0], venueDetails);
-                venueDetailsMapByVenueName.put(line[3] + line[8], venueDetails);
+                    VenueDetails venueDetails = VenueDetails.builder()
+                            .venueId(line[0])
+                            .threeDigitReference(line[1])
+                            .regionalProcessingCentre(line[2])
+                            .venName(line[3])
+                            .venAddressLine1(line[4])
+                            .venAddressLine2(line[5])
+                            .venAddressTown(line[6])
+                            .venAddressCounty(line[7])
+                            .venAddressPostcode(line[8])
+                            .venAddressTelNo(line[9])
+                            .districtId(line[10])
+                            .url(line[11])
+                            .active(line[12])
+                            .gapsVenName(line[13])
+                            .comments(line[14])
+                            .epimsId(line[15])
+                            .build();
+                    venueDetailsMap.put(line[0], venueDetails);
+                    venueDetailsMapByVenueName.put(line[3] + line[8], venueDetails);
+                    mapEpimsVenueDetails(line, venueDetails);
                 }
             );
         } catch (IOException e) {
@@ -58,6 +66,15 @@ public class VenueDataLoader {
         } catch (CsvException e) {
             log.error("Error occurred while loading the sscs venues reference data file: " + CSV_FILE_PATH + e);
         }
+    }
+
+    private void mapEpimsVenueDetails(String[] line, VenueDetails venueDetails) {
+        List<VenueDetails> epimsVenues = venueDetailsEpimsMap.get(line[15]);
+        if (isNull(epimsVenues)) {
+            epimsVenues = new ArrayList<>();
+        }
+        epimsVenues.add(venueDetails);
+        venueDetailsEpimsMap.put(line[15], epimsVenues);
     }
 
     public Map<String, VenueDetails> getVenueDetailsMap() {
@@ -82,5 +99,24 @@ public class VenueDataLoader {
             }
         }
         return (venue != null) ? venue.getName() : null;
+    }
+
+    public List<VenueDetails> getVenuesByEpims(String epimsId) {
+        return Optional.ofNullable(venueDetailsEpimsMap.get(epimsId))
+            .orElse(List.of());
+    }
+
+    public VenueDetails getAnActiveVenueByEpims(String epimsId) {
+        return getVenuesByEpims(epimsId)
+            .stream()
+            .filter(Objects::nonNull)
+            .filter(this::isActiveVenue)
+            .findAny()
+            .orElse(null);
+    }
+
+    public boolean isActiveVenue(VenueDetails venueDetails) {
+        return nonNull(venueDetails)
+            && isYes(venueDetails.getActive());
     }
 }
