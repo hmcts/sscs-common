5c0035311d153e93d3d88d6d0789d6c8614b25b5
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-06-01 14:11:22 +0100

    SSCS-10286 Add caseStateUpdate to CancellationReason

--- src/main/java/uk/gov/hmcts/reform/sscs/reference/data/model/CancellationReason.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/reference/data/model/CancellationReason.java
@@ -1,25 +1,30 @@
 package uk.gov.hmcts.reform.sscs.reference.data.model;
 
+import static uk.gov.hmcts.reform.sscs.ccd.domain.State.DORMANT_APPEAL_STATE;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.State.READY_TO_LIST;
+
 import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import uk.gov.hmcts.reform.sscs.ccd.domain.State;
 
 @Getter
 @RequiredArgsConstructor
 public enum CancellationReason {
 
-    EXCLUSION("BBA3-exclusion", "Exclusion"),
-    INCOMPLETE_TRIBUNAL("BBA3-incompleteTribunal", "Incomplete Tribunal"),
-    LAPSED("BBA3-lapsed", "Lapsed"),
-    LISTED_IN_ERROR("BBA3-listedInError", "Listed In Error"),
-    OTHER("BBA3-other", "Other"),
-    PARTY_DID_NOT_ATTEND("BBA3-partyDidNotAttend", "Party Did Not Attend"),
-    PARTY_UNABLE_TO_ATTEND("BBA3-partyUnableToAttend", "Party unable to attend"),
-    STRUCK_OUT("BBA3-struckOut", "Struck Out"),
-    WITHDRAWN("BBA3-withdrawn", "Withdrawn");
+    EXCLUSION("BBA3-exclusion", "Exclusion", READY_TO_LIST),
+    INCOMPLETE_TRIBUNAL("BBA3-incompleteTribunal", "Incomplete Tribunal", READY_TO_LIST),
+    LAPSED("BBA3-lapsed", "Lapsed", DORMANT_APPEAL_STATE),
+    LISTED_IN_ERROR("BBA3-listedInError", "Listed In Error", READY_TO_LIST),
+    OTHER("BBA3-other", "Other", READY_TO_LIST),
+    PARTY_DID_NOT_ATTEND("BBA3-partyDidNotAttend", "Party Did Not Attend", READY_TO_LIST),
+    PARTY_UNABLE_TO_ATTEND("BBA3-partyUnableToAttend", "Party unable to attend", READY_TO_LIST),
+    STRUCK_OUT("BBA3-struckOut", "Struck Out", DORMANT_APPEAL_STATE),
+    WITHDRAWN("BBA3-withdrawn", "Withdrawn", DORMANT_APPEAL_STATE);
 
     private final String hmcReference;
     private final String label;
+    private final State caseStateUpdate;
 
     public static CancellationReason getCancellationReasonByHmcReference(String value) {
         return Arrays.stream(CancellationReason.values())
