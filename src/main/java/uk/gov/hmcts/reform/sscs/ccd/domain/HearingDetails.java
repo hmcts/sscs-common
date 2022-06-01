d53bef075cb648585aefde06ea24d8fc46625af7
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-06-01 18:07:31 +0100

    SSCS-10286 Update to HearingDetails

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingDetails.java
@@ -1,46 +1,37 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import static java.util.Objects.nonNull;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Value
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder(toBuilder = true)
 public class HearingDetails {
     private Venue venue;
     private String hearingDate;
     private String time;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
     private String adjourned;
     private String eventDate;
     private String hearingId;
     private String venueId;
 
-    @JsonCreator
-    public HearingDetails(@JsonProperty("venue") Venue venue,
-                          @JsonProperty("hearingDate") String hearingDate,
-                          @JsonProperty("time") String time,
-                          @JsonProperty("adjourned") String adjourned,
-                          @JsonProperty("eventDate") String eventDate,
-                          @JsonProperty("hearingId") String hearingId,
-                          @JsonProperty("venueId") String venueId) {
-        this.venue = venue;
-        this.hearingDate = hearingDate;
-        this.time = time;
-        this.adjourned = adjourned;
-        this.eventDate = eventDate;
-        this.hearingId = hearingId;
-        this.venueId = venueId;
-    }
-
     @JsonIgnore
     public LocalDateTime getHearingDateTime() {
-        return LocalDateTime.of(LocalDate.parse(hearingDate), LocalTime.parse(time));
+        return nonNull(startDateTime)
+            ? startDateTime
+            : LocalDateTime.of(LocalDate.parse(hearingDate), LocalTime.parse(time));
     }
 }
