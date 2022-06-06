0bcdf600769683b31a26c8861b6941b786eadd30
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-06-06 15:48:20 +0100

    SSCS-10286 Removing states covered by adjournment/postponement journey

--- src/main/java/uk/gov/hmcts/reform/sscs/reference/data/model/CancellationReason.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/reference/data/model/CancellationReason.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.sscs.reference.data.model;
 
 import static uk.gov.hmcts.reform.sscs.ccd.domain.State.DORMANT_APPEAL_STATE;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.State.NOT_LISTABLE;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.State.READY_TO_LIST;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
@@ -15,16 +13,16 @@ public enum CancellationReason {
 
     WITHDRAWN("withdraw", "Withdrawn", DORMANT_APPEAL_STATE),
     STRUCK_OUT("struck", "Struck Out", DORMANT_APPEAL_STATE),
-    PARTY_UNABLE_TO_ATTEND("unable", "Party Unable To Attend", READY_TO_LIST),
-    EXCLUSION("exclusio", "Exclusion", READY_TO_LIST),
-    INCOMPLETE_TRIBUNAL("incompl", "Incomplete Tribunal", READY_TO_LIST),
-    LISTED_IN_ERROR("listerr", "Listed In Error", READY_TO_LIST),
-    OTHER("other", "Other", READY_TO_LIST),
-    NOT_READY("notready", "No longer ready for hearing", NOT_LISTABLE),
-    SETTLED("settled", "Settled", NOT_LISTABLE),
-    JUDICIAL_DIRECTION("jodir", "Judicial direction", NOT_LISTABLE),
-    FEE_NOT_PAID("notpaid", "Fee not paid", NOT_LISTABLE),
-    PARTY_DID_NOT_ATTEND("notatt", "Party Did Not Attend", READY_TO_LIST),
+    PARTY_UNABLE_TO_ATTEND("unable", "Party Unable To Attend", null),
+    EXCLUSION("exclusio", "Exclusion", null),
+    INCOMPLETE_TRIBUNAL("incompl", "Incomplete Tribunal", null),
+    LISTED_IN_ERROR("listerr", "Listed In Error", null),
+    OTHER("other", "Other", null),
+    NOT_READY("notready", "No longer ready for hearing", null),
+    SETTLED("settled", "Settled", null),
+    JUDICIAL_DIRECTION("jodir", "Judicial direction", null),
+    FEE_NOT_PAID("notpaid", "Fee not paid", null),
+    PARTY_DID_NOT_ATTEND("notatt", "Party Did Not Attend", null),
     LAPSED("lapsed", "Lapsed", DORMANT_APPEAL_STATE);
 
     @JsonValue
