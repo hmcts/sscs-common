77e7769359ed96054abe8f26ad6dbc2833547d1f
Harry Henry <Harry.Henry@version1.com>
2022-06-09 15:58:43 +0100

    update annotations for new class

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OverrideSchedulingListingFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OverrideSchedulingListingFields.java
@@ -1,7 +1,19 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OverrideSchedulingListingFields {
 
     @JsonProperty("overrideDuration")
