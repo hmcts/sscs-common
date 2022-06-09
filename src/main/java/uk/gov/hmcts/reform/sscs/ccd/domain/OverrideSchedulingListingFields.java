803acbfa2167d5b4b6742aab3c27acf784c7a1af
Harry Henry <Harry.Henry@version1.com>
2022-06-09 18:32:47 +0100

    address pr comments

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OverrideSchedulingListingFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OverrideSchedulingListingFields.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +15,5 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OverrideSchedulingListingFields {
 
-    @JsonProperty("overrideDuration")
     private int overrideDuration;
 }
