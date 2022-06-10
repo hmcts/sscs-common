8bd3ac03d584a87243f383e73dacf6430fddc4f2
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-06-10 10:27:31 +0100

    10260 Add Hearing Start and End Time

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingDetails.java
@@ -1,17 +1,21 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import static java.util.Objects.nonNull;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Value
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder(toBuilder = true)
 public class HearingDetails {
     private Venue venue;
@@ -21,26 +25,14 @@ public class HearingDetails {
     private String eventDate;
     private String hearingId;
     private String venueId;
-
-    @JsonCreator
-    public HearingDetails(@JsonProperty("venue") Venue venue,
-                          @JsonProperty("hearingDate") String hearingDate,
-                          @JsonProperty("time") String time,
-                          @JsonProperty("adjourned") String adjourned,
-                          @JsonProperty("eventDate") String eventDate,
-                          @JsonProperty("hearingId") String hearingId,
-                          @JsonProperty("venueId") String venueId) {
-        this.venue = venue;
-        this.hearingDate = hearingDate;
-        this.time = time;
-        this.adjourned = adjourned;
-        this.eventDate = eventDate;
-        this.hearingId = hearingId;
-        this.venueId = venueId;
-    }
-
+    
+    private LocalDateTime start;
+    private LocalDateTime end;
     @JsonIgnore
     public LocalDateTime getHearingDateTime() {
+        if (nonNull(start)) {
+            return start;
+        }
         return LocalDateTime.of(LocalDate.parse(hearingDate), LocalTime.parse(time));
     }
 }
