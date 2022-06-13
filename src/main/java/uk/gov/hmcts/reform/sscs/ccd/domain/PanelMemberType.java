d1c29b814e33ecd4e14300c2f99f25ab9438dbfc
MohammedHMCTS <95023176+MohammedHMCTS@users.noreply.github.com>
2022-06-13 13:33:20 +0100

    SSCS-10643 Add PanelMemberType

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberType.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+@Getter
+public enum PanelMemberType {
+
+    TRIBUNALS_MEMBER_DISABILITY(44, "Tribunal Member Disability"),
+    TRIBUNALS_MEMBER_FINANCIALLY_QUALIFIED(50, "Tribunal Member Financially Qualified"),
+    TRIBUNALS_MEMBER_LAY(55, "Tribunal Member Lay"),
+    TRIBUNALS_MEMBER_MEDICAL(58, "Tribunal Member Medical"),
+    TRIBUNALS_MEMBER_OPTOMETRIST(60, "Tribunal Member Optometrist"),
+    TRIBUNALS_MEMBER_SERVICE(81, "Tribunal Member Service"),
+    TRIBUNALS_MEMBER(85, "Tribunal Member");
+
+    private final int key;
+    private final String en;
+
+
+    public static PanelMemberType getPanelMemberType(int key) {
+        return Arrays.stream(values())
+                .filter(panelMember -> panelMember.key == key)
+                .findFirst()
+                .orElse(null);
+    }
+}
