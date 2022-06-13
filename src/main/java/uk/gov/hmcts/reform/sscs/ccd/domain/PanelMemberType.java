539df2556a5f924cc0d39d062f5746aecce9e0ca
MohammedHMCTS <95023176+MohammedHMCTS@users.noreply.github.com>
2022-06-13 15:51:25 +0100

    SSCS-10643 Lots of changes

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberType.java
@@ -8,20 +8,20 @@ import lombok.Getter;
 @Getter
 public enum PanelMemberType {
 
-    TRIBUNALS_MEMBER_DISABILITY(44, "Tribunal Member Disability"),
-    TRIBUNALS_MEMBER_FINANCIALLY_QUALIFIED(50, "Tribunal Member Financially Qualified"),
-    TRIBUNALS_MEMBER_LAY(55, "Tribunal Member Lay"),
-    TRIBUNALS_MEMBER_MEDICAL(58, "Tribunal Member Medical"),
-    TRIBUNALS_MEMBER_OPTOMETRIST(60, "Tribunal Member Optometrist"),
-    TRIBUNALS_MEMBER_SERVICE(81, "Tribunal Member Service"),
-    TRIBUNALS_MEMBER(85, "Tribunal Member");
+    TRIBUNALS_MEMBER_DISABILITY("44", "Tribunal Member Disability"),
+    TRIBUNALS_MEMBER_FINANCIALLY_QUALIFIED("50", "Tribunal Member Financially Qualified"),
+    TRIBUNALS_MEMBER_LAY("55", "Tribunal Member Lay"),
+    TRIBUNALS_MEMBER_MEDICAL("58", "Tribunal Member Medical"),
+    TRIBUNALS_MEMBER_OPTOMETRIST("60", "Tribunal Member Optometrist"),
+    TRIBUNALS_MEMBER_SERVICE("81", "Tribunal Member Service"),
+    TRIBUNALS_MEMBER("85", "Tribunal Member");
 
-    private final int key;
+    private final String reference;
     private final String en;
 
-    public static PanelMemberType getPanelMemberType(int key) {
+    public static PanelMemberType getPanelMemberType(String reference) {
         return Arrays.stream(values())
-                .filter(panelMember -> panelMember.key == key)
+                .filter(panelMember -> panelMember.reference.equals(reference))
                 .findFirst()
                 .orElse(null);
     }
