f3635b396fe8c813dd940c64aad155a62dd2f7e0
Lucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-06-16 11:34:20 +0100

    SSCS-10242 QA Demo Branch (#1233)
    SSCS-10242 WorkBasketFields

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/WorkBasketFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/WorkBasketFields.java
@@ -1,8 +1,12 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +17,13 @@ import lombok.NoArgsConstructor;
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class WorkBasketFields {
     // TODO Find out if its a good idea/worth to moving the other search fields here?
+
+    @JsonProperty("workBasketHearingDate")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate hearingDate;
 
     @JsonProperty("workBasketHearingEpimsId")
     private String hearingEpimsId;
