7fd2d3a1197e908f98591353ff7b6d46b24cf0ed
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-07-01 12:13:00 +0100

    SSCS-10087 Override Fields

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OverrideFields.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.sscs.reference.data.model.HearingChannel;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude
+public class OverrideFields {
+    private Number duration;
+    private ReservedToMember reservedToJudge;
+    private HearingInterpreter appellantInterpreter;
+    private HearingChannel appellantHearingChannel;
+    private HearingWindow hearingWindow;
+    private YesNo autoList;
+    private List<CcdValue<String>> hearingVenueEpimsId;
+    private YesNo poToAttend;
+}
