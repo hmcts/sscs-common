39b21ff2cc20eedbe3310d3dce9c52e750e59e88
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-07-08 15:33:59 +0100

    SSCS-10371 JudicialRefData
    Co-Authored-By: Jonathan-Weston1 <94540707+Jonathan-Weston1@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/client/JudicialMemberAppointments.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.reform.sscs.model.client;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JudicialMemberAppointments {
+
+
+    private String appointment;
+    @JsonProperty("appointment_type")
+    private String appointmentType;
+    @JsonProperty("base_location_id")
+    private String baseLocationId;
+    @JsonProperty("cft_region")
+    private String cftRegion;
+    @JsonProperty("cft_region_id")
+    private String cftRegionId;
+    @JsonProperty("court_name")
+    private String courtName;
+    @JsonProperty("epimms_id")
+    private String epimmsId;
+    @JsonProperty("is_principal_appointment")
+    private Boolean isPrincipalAppointment;
+    private String location;
+    @JsonProperty("location_id")
+    private String locationId;
+    private List<String> roles;
+    @JsonProperty("service_code")
+    private String serviceCode;
+    @JsonProperty("start_date")
+    private LocalDateTime startDate;
+    @JsonProperty("end_date")
+    private LocalDateTime endDate;
+}
---
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/client/JudicialRefDataUsersRequest.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.sscs.model.client;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JudicialRefDataUsersRequest {
+
+    String ccdServiceName;
+    @JsonProperty("object_ids")
+    List<String> objectIds;
+    @JsonProperty("sidam_ids")
+    List<String> sidamIds;
+
+}
