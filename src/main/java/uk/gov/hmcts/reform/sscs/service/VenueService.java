7162c0f2041b32c8402f2d648abb16341d4f23b4
DGordonV1 <DGordonV1@github.com>
2022-08-08 09:29:31 +0100

    SSCS-10332 - Update VenueDataLoader and VenueService to get regional epimsIds for an RPC name

--- src/main/java/uk/gov/hmcts/reform/sscs/service/VenueService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/VenueService.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.sscs.service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.model.VenueDetails;
@@ -25,6 +28,16 @@ public class VenueService {
             .orElseThrow(() -> {
                 throw new IllegalStateException("Unable to find epims ID for processing venue: " + processingVenue);
             });
+    }
+
+    public List<String> getRegionalEpimsIdsForRpc(String rpc) {
+
+        return venueDataLoader.getVenueDetailsMapByRpc()
+                .entrySet()
+                .stream()
+                .filter(entry -> rpc.equals(entry.getKey().getName()))
+                .map(entry -> entry.getValue().getEpimsId())
+                .collect(Collectors.toList());
     }
 
     public VenueDetails getVenueDetailsForActiveVenueByEpimsId(String epimsId) {
