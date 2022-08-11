062b7f489c4d51896d31b988c92654d3d6da2575
KirsanovI <ivan.kirsanov@hmcts.net>
2022-08-11 09:15:27 +0200

    SSCS-10773: unused import removed

--- src/main/java/uk/gov/hmcts/reform/sscs/reference/data/model/HearingChannel.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/reference/data/model/HearingChannel.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.sscs.reference.data.model;
 
 import com.fasterxml.jackson.annotation.JsonValue;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
