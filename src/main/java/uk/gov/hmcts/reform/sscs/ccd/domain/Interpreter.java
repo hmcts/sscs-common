b84eadb98f618a91bace9e869c4f1c879ebe2023
DGordonV1 <david.gordon@hmcts.net>
2022-08-18 16:02:26 +0100

    SSCS-10810 - Add Intepreter domain object class to facilitate changes in hearings-api project for selection of EntityRoleCode

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Interpreter.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@SuperBuilder
+@RequiredArgsConstructor
+@JsonInclude
+@EqualsAndHashCode(callSuper = true)
+public class Interpreter extends Entity {
+
+}
