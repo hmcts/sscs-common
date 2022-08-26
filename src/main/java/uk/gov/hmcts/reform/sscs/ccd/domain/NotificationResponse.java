430edae0ab0fb34ae246e6977c620de5de771467
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-08-26 14:12:33 +0100

    SSCS-10815 Parties Notified Callback

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/NotificationResponse.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime responseReceived;
+    private CorrespondenceType correspondenceType;
+    private String recipientPartyId;
+    private EventType event;
+}
