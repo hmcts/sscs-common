342680a1bd6d1c2acf00c36370f03a924e68ec36
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-09-05 11:57:51 +0100

    SSCS-10798 Reworking Grant Postponement Hearing Process

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Postponement.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Postponement {
+    private YesNo unprocessedPostponement;
+    private EventType postponementEvent;
+}
