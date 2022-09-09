a78719d9fe030e73cc7b839f89d2389085b87d7e
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-09-09 18:03:00 +0100

    SSCS-10882 Fixing Conflicting IDs

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/util/CaseDataUtils.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/util/CaseDataUtils.java
@@ -23,7 +23,7 @@ public final class CaseDataUtils {
     public static SscsCaseData buildCaseData(final String surname) {
         return buildCaseData(surname, "PIP", "DWP PIP (1)");
     }
-    
+
     public static SscsCaseData buildCaseData(final String surname, String benefitTypeCode, String issuingOffice) {
         Name name = Name.builder()
                 .title("Mr")
