91c07cc1a23801951977abfb5be54304ee1609a8
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-10-05 16:14:20 +0100

    SSCS-10542 Set Aside

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DocumentStaging.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DocumentStaging {
+    @JsonProperty("previewDocument")
+    private DocumentLink previewDocument;
+    @JsonProperty("filename")
+    private String filename;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonProperty("dateAdded")
+    private LocalDate dateAdded;
+}
