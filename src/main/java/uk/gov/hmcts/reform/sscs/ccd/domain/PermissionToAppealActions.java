b33fc387bebdf195d333299c65ac15f47283720f
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-10-14 10:21:24 +0100

    SSCS-10542 Map Action To Event

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PermissionToAppealActions.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PermissionToAppealActions.java
@@ -6,11 +6,14 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum PermissionToAppealActions {
-    REFUSE("refuse","Refuse Permission to Appeal Application");
+public enum PermissionToAppealActions implements CcdCallbackMap {
+    REFUSE("refuse","Refuse Permission to Appeal Application", null, "", "");
 
     private final String ccdDefinition;
     private final String descriptionEn;
+    private final EventType callbackEvent;
+    private final String callbackSummary;
+    private final String callbackDescription;
 
     @Override
     @JsonValue
