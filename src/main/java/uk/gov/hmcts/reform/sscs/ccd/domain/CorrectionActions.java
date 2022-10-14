cdb28105ea7f464a8bc6bcd0dc1c632246a5687c
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-10-14 10:21:24 +0100

    SSCS-10542 Map Action To Event

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CorrectionActions.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CorrectionActions.java
@@ -1,16 +1,25 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.CORRECTION_GRANTED;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.CORRECTION_REFUSED;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.CORRECTION_SEND_TO_JUDGE;
+
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum CorrectionActions {
-    REFUSE("refuse","Refuse Correction Application");
+public enum CorrectionActions implements CcdCallbackMap {
+    GRANT("grant","Grant Correction Application", CORRECTION_GRANTED, "Correction application granted", "Correction application granted"),
+    REFUSE("refuse","Refuse Correction Application", CORRECTION_REFUSED, "Correction application Refused", "Correction application Refused"),
+    SEND_TO_JUDGE("sendToJudge","Send Correction Application to Judge", CORRECTION_SEND_TO_JUDGE, "Correction application send to Judge (Admin)", "Correction application send to Judge (Admin)");
 
     private final String ccdDefinition;
     private final String descriptionEn;
+    private final EventType callbackEvent;
+    private final String callbackSummary;
+    private final String callbackDescription;
 
     @Override
     @JsonValue
