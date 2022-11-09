6ee6f98043ced3ecec70b37debffdddbb62975c7
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2022-11-09 15:40:58 +0000

    SSCS-11092 moves adjourn-related SscsCaseData props into own JsonUnwrapped class Adjournment
    modifies all properties from String to more reasonable types eg enum, YesNo, LocalDate
    
    tidies SscsCaseData a bit to remove some warnings

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/AdjournCaseNextHearingDateType.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AdjournCaseNextHearingDateType {
+    DATE_TO_BE_FIXED("dateToBeFixed", "Date to be fixed"),
+    FIRST_AVAILABLE_DATE_AFTER("firstAvailableDateAfter", "First available date after"),
+    FIRST_AVAILABLE_DATE("firstAvailableDate", "First available date");
+
+    private final String ccdDefinition;
+    private final String descriptionEn;
+
+    @Override
+    @JsonValue
+    public String toString() {
+        return ccdDefinition;
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Adjournment.java
@@ -0,0 +1,95 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.sscs.ccd.validation.documentlink.DocumentLinkMustBePdf;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Adjournment {
+    @JsonProperty("adjournCaseGenerateNotice")
+    private YesNo generateNotice;
+    @JsonProperty("adjournCaseTypeOfHearing")
+    private AdjournCaseTypeOfHearing typeOfHearing;
+    @JsonProperty("adjournCaseCanCaseBeListedRightAway")
+    private YesNo canCaseBeListedRightAway;
+    @JsonProperty("adjournCaseAreDirectionsBeingMadeToParties")
+    private YesNo areDirectionsBeingMadeToParties;
+    @JsonProperty("adjournCaseDirectionsDueDateDaysOffset")
+    private String directionsDueDateDaysOffset;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonProperty("adjournCaseDirectionsDueDateDaysOffset")
+    private LocalDate directionsDueDate;
+    @JsonProperty("adjournCaseTypeOfNextHearing")
+    private AdjournCaseTypeOfHearing typeOfNextHearing;
+    @JsonProperty("adjournCaseNextHearingVenue")
+    private AdjournCaseNextHearingVenue nextHearingVenue;
+    @JsonProperty("adjournCaseNextHearingVenueSelected")
+    private DynamicList nextHearingVenueSelected;
+    @JsonProperty("adjournCasePanelMembersExcluded")
+    private AdjournCasePanelMembersExcluded panelMembersExcluded;
+    @JsonProperty("adjournCaseDisabilityQualifiedPanelMemberName")
+    private String disabilityQualifiedPanelMemberName;
+    @JsonProperty("adjournCaseMedicallyQualifiedPanelMemberName")
+    private String medicallyQualifiedPanelMemberName;
+    @JsonProperty("adjournCaseOtherPanelMemberName")
+    private String otherPanelMemberName;
+    @JsonProperty("adjournCaseNextHearingListingDurationType")
+    private AdjournCaseNextHearingDurationType nextHearingListingDurationType;
+    @JsonProperty("adjournCaseNextHearingListingDuration")
+    private int nextHearingListingDuration;
+    @JsonProperty("adjournCaseNextHearingListingDurationUnits")
+    private AdjournCaseNextHearingDurationUnits nextHearingListingDurationUnits;
+    @JsonProperty("adjournCaseInterpreterRequired")
+    private YesNo interpreterRequired;
+    @JsonProperty("adjournCaseInterpreterLanguageList")
+    private DynamicList interpreterLanguage;
+    @JsonProperty("adjournCaseNextHearingDateType")
+    private AdjournCaseNextHearingDateType nextHearingDateType;
+    @JsonProperty("adjournCaseNextHearingDateOrPeriod")
+    private AdjournCaseNextHearingDateOrPeriod nextHearingDateOrPeriod;
+    @JsonProperty("adjournCaseNextHearingDateOrTime")
+    private String nextHearingDateOrTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonProperty("adjournCaseNextHearingFirstAvailableDateAfterDate")
+    private LocalDate nextHearingFirstAvailableDateAfterDate;
+    @JsonProperty("adjournCaseNextHearingFirstAvailableDateAfterPeriod")
+    private AdjournCaseNextHearingPeriod nextHearingFirstAvailableDateAfterPeriod;
+    @JsonProperty("adjournCaseTime")
+    private AdjournCaseTime time;
+    @JsonProperty("adjournCaseReasons")
+    private List<CollectionItem<String>> reasons;
+    @JsonProperty("adjournCaseAdditionalDirections")
+    private List<CollectionItem<String>> additionalDirections;
+
+    @DocumentLinkMustBePdf(message = "You need to upload PDF documents only")
+    @JsonProperty("adjournCasePreviewDocument")
+    private DocumentLink previewDocument;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonProperty("adjournCaseGeneratedDate")
+    private LocalDate generatedDate;
+    @JsonProperty("isAdjournmentInProgress")
+    private YesNo isAdjournmentInProgress;
+
+}
