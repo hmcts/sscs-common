c311cdbd6f19cffab7e0b82b8d44096da38f94d8
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2022-11-09 15:40:58 +0000

    SSCS-11092 moves adjourn-related SscsCaseData props into own JsonUnwrapped class Adjournment
    modifies all properties from String to more reasonable types eg enum, YesNo, LocalDate
    
    tidies SscsCaseData a bit to remove some warnings

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/AdjournCaseNextHearingVenue.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AdjournCaseNextHearingVenue {
+    SOMEWHERE_ELSE("somewhereElse", "Somewhere else"),
+    SAME_VENUE("sameVenue", "Same venue");
+
+    private final String ccdDefinition;
+    private final String descriptionEn;
+
+    @Override
+    @JsonValue
+    public String toString() {
+        return ccdDefinition;
+    }
+}
