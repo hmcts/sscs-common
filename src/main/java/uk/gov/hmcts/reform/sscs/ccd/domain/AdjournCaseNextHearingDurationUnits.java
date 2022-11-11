b54e1f40fed5bab3db8a3fee46802d8f3e8de144
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2022-11-11 11:21:30 +0000

    SSCS-11092 adds custom getters for AdjournCaseNextHearingDurationUnits & AdjournCasePanelMembersExcluded

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/AdjournCaseNextHearingDurationUnits.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/AdjournCaseNextHearingDurationUnits.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,5 +20,12 @@ public enum AdjournCaseNextHearingDurationUnits {
     @JsonValue
     public String toString() {
         return ccdDefinition;
+    }
+    
+    public static AdjournCaseNextHearingDurationUnits getDurationUnitsByCcdDefinition(String ccdDefinition) {
+        return Arrays.stream(AdjournCaseNextHearingDurationUnits.values())
+            .filter(adjournCaseNextHearingDurationUnits -> Objects.equals(adjournCaseNextHearingDurationUnits.ccdDefinition, ccdDefinition))
+            .findFirst()
+            .orElse(null);
     }
 }
