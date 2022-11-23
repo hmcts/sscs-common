63fb20c4c4581522737b5d4090beb101b80a22f4
nickhill111 <nickhill11@live.co.uk>
2022-11-23 16:50:21 +0000

    add @JsonIgnore to new functions SSCS-11090

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SchedulingAndListingFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SchedulingAndListingFields.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 import static java.util.Objects.isNull;
 import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
@@ -24,10 +25,12 @@ public class SchedulingAndListingFields {
     private OverrideFields defaultListingValues;
     private List<AmendReason> amendReasons;
 
+    @JsonIgnore
     public boolean isMissingListingRequirements() {
         return isMissingListingRequirements(overrideFields) && isMissingListingRequirements(defaultListingValues);
     }
 
+    @JsonIgnore
     private boolean isMissingListingRequirements(OverrideFields overrideFields) {
         return isNull(overrideFields) || isEmpty(overrideFields.getDuration());
     }
