896a8c2d54961f7267fab3799f67f4d5eebdb273
nickhill111 <nickhill11@live.co.uk>
2023-01-23 10:55:27 +0000

    remove unused imports SSCS-10762

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
