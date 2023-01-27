016f041da34637b35dd08bf927fb9735fd5699fd
nickhill111 <nickhill11@live.co.uk>
2023-01-27 15:14:56 +0000

    add reservedJudge field SSCS-10893

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
@@ -41,6 +41,7 @@ import org.apache.commons.lang3.StringUtils;
 import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
 import uk.gov.hmcts.reform.sscs.ccd.validation.groups.UniversalCreditValidationGroup;
 import uk.gov.hmcts.reform.sscs.ccd.validation.localdate.LocalDateMustNotBeInFuture;
+import uk.gov.hmcts.reform.sscs.model.client.JudicialUser;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
@@ -171,6 +172,7 @@ public class SscsCaseData implements CaseData {
     private String documentSentToDwp;
     private String directionDueDate;
     private String reservedToJudge;
+    private JudicialUser reservedJudge;
     private List<CaseLink> linkedCase;
     private String isWaiverNeeded;
     private List<String> waiverDeclaration;
