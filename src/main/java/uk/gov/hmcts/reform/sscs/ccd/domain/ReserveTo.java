027eb5402e64fbacbf53c7d1e51558b1e8ba55fb
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2023-02-07 11:47:55 +0000

    SSCS-10822 creates ReserveTo class and moves reservedJudge and reservedDistrictTribunalJudge into it

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ReserveTo.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ReserveTo.java
@@ -2,23 +2,22 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.sscs.model.client.JudicialUserBase;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class SchedulingAndListingFields {
-    private HearingRoute hearingRoute;
-    private HearingState hearingState;
-    private ReserveTo reserveTo;
-    private OverrideFields overrideFields;
-    private OverrideFields defaultListingValues;
-    private List<AmendReason> amendReasons;
+@JsonInclude
+public class ReserveTo {
+    @JsonProperty("reservedJudge")
+    private JudicialUserBase reservedJudge;
+    @JsonProperty("reservedDistrictTribunalJudge")
+    private YesNo reservedDistrictTribunalJudge;
 }
