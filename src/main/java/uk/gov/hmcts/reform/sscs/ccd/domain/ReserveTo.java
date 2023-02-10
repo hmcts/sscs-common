da23bb26211f47f4f46295a44b182481109b16ae
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2023-02-10 11:51:21 +0000

    SSCS-10822 removes @JsonProperty annotations

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ReserveTo.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ReserveTo.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,8 +15,6 @@ import uk.gov.hmcts.reform.sscs.model.client.JudicialUserBase;
 @NoArgsConstructor
 @JsonInclude
 public class ReserveTo {
-    @JsonProperty("reservedJudge")
     private JudicialUserBase reservedJudge;
-    @JsonProperty("reservedDistrictTribunalJudge")
     private YesNo reservedDistrictTribunalJudge;
 }
