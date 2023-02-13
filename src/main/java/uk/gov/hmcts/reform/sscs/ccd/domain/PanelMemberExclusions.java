a840f85e83fc51d46133fab4fb8b84f20b05db5c
nickhill111 <nickhill11@live.co.uk>
2023-02-13 16:16:36 +0000

    add json property SSCS-10865

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberExclusions.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberExclusions.java
@@ -3,20 +3,21 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
-import lombok.AllArgsConstructor;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.sscs.model.client.JudicialUserBase;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @Builder(toBuilder = true)
-@AllArgsConstructor
-@NoArgsConstructor
 @JsonInclude
 public class PanelMemberExclusions {
+    @JsonProperty("arePanelMembersExcluded")
     private YesNo arePanelMembersExcluded;
+    @JsonProperty("excludedPanelMembers")
     private List<JudicialUserBase> excludedPanelMembers;
+    @JsonProperty("arePanelMembersReserved")
     private YesNo arePanelMembersReserved;
 }
