991ce68a4e963e9c79e98402312c4476f622824d
nickhill111 <nickhill11@live.co.uk>
2023-02-14 11:39:28 +0000

    add jsonproperty to panelmemberexclusions SSCS-10865

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SchedulingAndListingFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SchedulingAndListingFields.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -18,6 +20,7 @@ public class SchedulingAndListingFields {
     private HearingRoute hearingRoute;
     private HearingState hearingState;
     private JudicialUserBase reservedJudge;
+    @JsonProperty
     private PanelMemberExclusions panelMemberExclusions;
     private OverrideFields overrideFields;
     private OverrideFields defaultListingValues;
@@ -69,7 +72,7 @@ public class SchedulingAndListingFields {
     }
 
     @SuppressWarnings("unused")
-    @JsonIgnore
+    @JsonProperty
     public void setPanelMemberExclusions(PanelMemberExclusions panelMemberExclusions) {
         this.panelMemberExclusions = panelMemberExclusions;
     }
