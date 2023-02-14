6bfcaa692e76eb91bc7f90ebeb74210d409d2d42
nickhill111 <nickhill11@live.co.uk>
2023-02-14 12:01:07 +0000

    add argsconstructors SSCS-10865

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberExclusions.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberExclusions.java
@@ -5,25 +5,19 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.sscs.model.client.JudicialUserBase;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude
 public class PanelMemberExclusions {
-
-    @SuppressWarnings("unused")
-    public PanelMemberExclusions(YesNo arePanelMembersExcluded,
-                                 List<JudicialUserBase> excludedPanelMembers,
-                                 YesNo arePanelMembersReserved) {
-        this.arePanelMembersExcluded = arePanelMembersExcluded;
-        this.excludedPanelMembers = excludedPanelMembers;
-        this.arePanelMembersReserved = arePanelMembersReserved;
-    }
-
     @JsonProperty("arePanelMembersExcluded")
     private YesNo arePanelMembersExcluded;
     @JsonProperty("excludedPanelMembers")
