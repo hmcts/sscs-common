0e705572841e70c19c53c34ee111ec404aaf112f
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2023-03-02 09:48:13 +0000

    SSCS-10576 adds Correction requested dwp state

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/callback/ScannedDocumentType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/callback/ScannedDocumentType.java
@@ -6,15 +6,15 @@ import lombok.Getter;
 public enum ScannedDocumentType {
 
     CHERISHED("cherished", "Cherished"),
-    OTHER("other", "Other"),
-    FORM("form", "Form"),
-    COVERSHEET("coversheet", "Coversheet"),
-    REINSTATEMENT_REQUEST("reinstatementRequest", "Reinstatement request"),
     CONFIDENTIALITY_REQUEST("confidentialityRequest", "Confidentiality request"),
-    URGENT_HEARING_REQUEST("urgentHearingRequest", "Urgent hearing request"),
+    CORRECTION_APPLICATION("correctionApplication", "Correction application"),
+    COVERSHEET("coversheet", "Coversheet"),
+    FORM("form", "Form"),
+    OTHER("other", "Other"),
     POSTPONEMENT_REQUEST("postponementRequest", "Postponement request"),
+    REINSTATEMENT_REQUEST("reinstatementRequest", "Reinstatement request"),
     SET_ASIDE_APPLICATION("setAsideApplication", "Set aside application"),
-    CORRECTION_APPLICATION("correctionApplication", "Correction application");
+    URGENT_HEARING_REQUEST("urgentHearingRequest", "Urgent hearing request");
 
     private String value;
     private String label;
