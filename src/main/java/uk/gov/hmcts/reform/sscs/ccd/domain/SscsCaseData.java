25273dadedcc9734c4d223a98e8942fdee132ea5
nickhill111 <nickhill11@live.co.uk>
2023-03-08 11:45:52 +0000

    change reservetojudge to judicialuserbase SSCS-11496

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseData.java
@@ -41,6 +41,7 @@ import org.apache.commons.lang3.StringUtils;
 import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
 import uk.gov.hmcts.reform.sscs.ccd.validation.groups.UniversalCreditValidationGroup;
 import uk.gov.hmcts.reform.sscs.ccd.validation.localdate.LocalDateMustNotBeInFuture;
+import uk.gov.hmcts.reform.sscs.model.client.JudicialUserBase;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
@@ -170,7 +171,7 @@ public class SscsCaseData implements CaseData {
     private String urgentHearingOutcome;
     private String documentSentToDwp;
     private String directionDueDate;
-    private String reservedToJudge;
+    private JudicialUserBase reservedToJudgeInterloc;
     private List<CaseLink> linkedCase;
     private String isWaiverNeeded;
     private List<String> waiverDeclaration;
