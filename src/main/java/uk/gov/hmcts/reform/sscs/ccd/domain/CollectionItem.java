29f33bdedfb226591cc762d44b5cde0d9e9d62be
nickhill111 <nickhill11@live.co.uk>
2023-03-09 09:36:49 +0000

    remove unused import SSCS-10865

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CollectionItem.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CollectionItem.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
-import uk.gov.hmcts.reform.sscs.model.client.JudicialUserBase;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Value
