772515a749fc13a07417ea19ab729fb563343310
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2023-03-21 16:19:26 +0000

    SSCS-10581 adds admin action correction event
    adds AdminCorrectionType enum

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/AdminCorrectionType.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AdminCorrectionType {
+    BODY,
+    HEADER
+}
