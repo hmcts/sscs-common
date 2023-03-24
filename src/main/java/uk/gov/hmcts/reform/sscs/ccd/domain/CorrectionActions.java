d93ecca4837f8678edcd1f958e86bfd723620990
nickhill111 <nickhill11@live.co.uk>
2023-03-24 14:39:09 +0000

    add new dwpstate SSCS-10587

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CorrectionActions.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/CorrectionActions.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.CORRECTION_GRANTED;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.CORRECTION_REFUSED;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.CORRECTION_SEND_TO_JUDGE;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.DORMANT;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
@@ -12,9 +11,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CorrectionActions implements CcdCallbackMap {
-    GRANT("grant","Grant Correction Application", CORRECTION_GRANTED, "Correction application granted", "Correction application granted", State.DORMANT_APPEAL_STATE),
-    REFUSE("refuse","Refuse Correction Application", CORRECTION_REFUSED, "Correction application Refused", "Correction application Refused", null),
-    SEND_TO_JUDGE("sendToJudge","Send Correction Application to Judge", CORRECTION_SEND_TO_JUDGE, "Correction application send to Judge (Admin)", "Correction application send to Judge (Admin)", null);
+    GRANT("grant","Grant Correction Application", CORRECTION_GRANTED, "Correction application granted", "Correction application granted", State.DORMANT_APPEAL_STATE, DwpState.CORRECTED_DECISION_NOTICE_ISSUED),
+    REFUSE("refuse","Refuse Correction Application", CORRECTION_REFUSED, "Correction application Refused", "Correction application Refused", null, null),
+    SEND_TO_JUDGE("sendToJudge","Send Correction Application to Judge", CORRECTION_SEND_TO_JUDGE, "Correction application send to Judge (Admin)", "Correction application send to Judge (Admin)", null, null);
 
     private final String ccdDefinition;
     private final String descriptionEn;
@@ -22,7 +21,7 @@ public enum CorrectionActions implements CcdCallbackMap {
     private final String callbackSummary;
     private final String callbackDescription;
     private final State postCallbackState;
-    private final DwpState postCallbackDwpState = null;
+    private final DwpState postCallbackDwpState;
     private final InterlocReviewState postCallbackInterlocState = null;
     private final InterlocReferralReason postCallbackInterlocReason = null;
 
