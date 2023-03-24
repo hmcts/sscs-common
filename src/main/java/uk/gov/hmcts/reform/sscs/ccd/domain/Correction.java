69cfa467c32086d9df30153abfd687cfd4809e48
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2023-03-24 11:53:35 +0000

    SSCS-10581 adds JsonInclude to Correction

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Correction.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Correction.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Correction {
     private AdminCorrectionType adminCorrectionType;
     private CorrectionActions action;
