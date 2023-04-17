0e33dd23fb81c310075a2cc8eef08c922b32af74
nickhill111 <nickhill11@live.co.uk>
2023-04-17 15:47:34 +0100

    update user role to have a label as well SSCS-10035

--- src/main/java/uk/gov/hmcts/reform/sscs/idam/UserRole.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/idam/UserRole.java
@@ -1,21 +1,19 @@
 package uk.gov.hmcts.reform.sscs.idam;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
-    TCW("caseworker-sscs-registrar"),
-    CTSC_CLERK("caseworker-sscs-clerk"),
-    DWP("caseworker-sscs-dwpresponsewriter"),
-    JUDGE("caseworker-sscs-judge"),
-    SUPER_USER("caseworker-sscs-superuser"),
-    SYSTEM_USER("caseworker-sscs-systemupdate"),
-    CITIZEN("citizen");
+    TCW("caseworker-sscs-registrar", "Registrar"),
+    CTSC_CLERK("caseworker-sscs-clerk", "Clerk"),
+    DWP("caseworker-sscs-dwpresponsewriter", "FTA"),
+    JUDGE("caseworker-sscs-judge", "Judge"),
+    SUPER_USER("caseworker-sscs-superuser", "Super User"),
+    SYSTEM_USER("caseworker-sscs-systemupdate", "System User"),
+    CITIZEN("citizen", "citizen");
 
     private final String value;
-
-    UserRole(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    private final String label;
 }
