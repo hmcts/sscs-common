744545854c2f27f698a2681f7dd64a01d14e4f34
nickhill111 <nickhill11@live.co.uk>
2023-04-20 14:10:07 +0100

    capitalise label SSCS-10523

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DwpState.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DwpState.java
@@ -41,7 +41,7 @@ public enum DwpState {
     SET_ASIDE_REQUESTED("setAsideRequested", "Set Aside Requested"),
     STATEMENT_OF_REASONS_GRANTED("statementOfReasonsGranted", "SOR Granted"),
     STATEMENT_OF_REASONS_REFUSED("statementOfReasonsRefused", "SOR Refused"),
-    STATEMENT_OF_REASONS_REQUESTED("statementOfReasonsRequested", "SOR requested"),
+    STATEMENT_OF_REASONS_REQUESTED("statementOfReasonsRequested", "SOR Requested"),
     STRIKE_OUT_ACTIONED("strikeOutActioned", "Strike-out actioned"),
     STRUCK_OUT("struckOut", "Struck out"),
     SUPPLEMENTARY_RESPONSE("supplementaryResponse", "Supplementary response"),
