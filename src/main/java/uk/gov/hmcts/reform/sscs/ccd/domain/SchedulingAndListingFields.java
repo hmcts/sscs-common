040f529668c4219d1ef072e7c8266d3c8e60805b
nickhill111 <nickhill11@live.co.uk>
2023-06-13 10:02:22 +0100

    fix sandl imports SSCS-10865

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SchedulingAndListingFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SchedulingAndListingFields.java
@@ -2,9 +2,8 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
