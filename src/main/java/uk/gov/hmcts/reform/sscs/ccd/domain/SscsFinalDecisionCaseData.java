edb41c09222019ef9938e39f3a7eaa2074ddc71e
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2023-07-05 14:37:44 +0100

    SSCS-10581 whitespace

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsFinalDecisionCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsFinalDecisionCaseData.java
@@ -51,5 +51,4 @@ public class SscsFinalDecisionCaseData {
     public boolean isDailyLivingAndOrMobilityDecision() {
         return StringUtils.equalsIgnoreCase("yes", writeFinalDecisionIsDescriptorFlow);
     }
-
 }
