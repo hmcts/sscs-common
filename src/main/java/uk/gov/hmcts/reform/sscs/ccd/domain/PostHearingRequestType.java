90f1ffa476f62c0629d3d99238504c0b5ef35601
paul-pearson <paul.pearson@version1.com>
2023-07-20 16:33:08 +0100

    SSCS-11486 Set new referral reasons in PostHearingRequestType

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PostHearingRequestType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PostHearingRequestType.java
@@ -9,7 +9,11 @@ import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.LIBERTY_TO_APPLY_REQ
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.PERMISSION_TO_APPEAL_REQUEST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.SET_ASIDE_REQUEST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.SOR_REQUEST;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReferralReason.POST_HEARING_REQUEST;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReferralReason.REVIEW_CORRECTION_APPLICATION;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReferralReason.REVIEW_LIBERTY_TO_APPLY_APPLICATION;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReferralReason.REVIEW_PERMISSION_TO_APPEAL_APPLICATION;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReferralReason.REVIEW_SET_ASIDE_APPLICATION;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReferralReason.REVIEW_STATEMENT_OF_REASONS_APPLICATION;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReviewState.AWAITING_ADMIN_ACTION;
 
 import com.fasterxml.jackson.annotation.JsonValue;
@@ -19,11 +23,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PostHearingRequestType implements CcdCallbackMap {
-    CORRECTION("correction","Correction", CORRECTION_REQUEST, "Correction Request Made", "Correction Request Made", CORRECTION_REQUESTED, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST),
-    LIBERTY_TO_APPLY("libertyToApply","Liberty to Apply", LIBERTY_TO_APPLY_REQUEST, "Liberty to Apply Request Made", "Liberty to Apply Request Made", LIBERTY_TO_APPLY_REQUESTED, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST),
-    PERMISSION_TO_APPEAL("permissionToAppeal","Permission to Appeal", PERMISSION_TO_APPEAL_REQUEST, "Permission to Appeal Request Made", "Permission to Appeal Request Made", null, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST),
-    SET_ASIDE("setAside","Set Aside", SET_ASIDE_REQUEST, "Set Aside Request Made", "Set Aside Request Made", SET_ASIDE_REQUESTED, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST),
-    STATEMENT_OF_REASONS("statementOfReasons","Statement of Reasons", SOR_REQUEST, "Statement of Reasons Request Made", "Statement of Reasons Request Made", STATEMENT_OF_REASONS_REQUESTED, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST);
+    CORRECTION("correction","Correction", CORRECTION_REQUEST, "Correction Request Made", "Correction Request Made", CORRECTION_REQUESTED, AWAITING_ADMIN_ACTION, REVIEW_CORRECTION_APPLICATION),
+    LIBERTY_TO_APPLY("libertyToApply","Liberty to Apply", LIBERTY_TO_APPLY_REQUEST, "Liberty to Apply Request Made", "Liberty to Apply Request Made", LIBERTY_TO_APPLY_REQUESTED, AWAITING_ADMIN_ACTION, REVIEW_LIBERTY_TO_APPLY_APPLICATION),
+    PERMISSION_TO_APPEAL("permissionToAppeal","Permission to Appeal", PERMISSION_TO_APPEAL_REQUEST, "Permission to Appeal Request Made", "Permission to Appeal Request Made", null, AWAITING_ADMIN_ACTION, REVIEW_PERMISSION_TO_APPEAL_APPLICATION),
+    SET_ASIDE("setAside","Set Aside", SET_ASIDE_REQUEST, "Set Aside Request Made", "Set Aside Request Made", SET_ASIDE_REQUESTED, AWAITING_ADMIN_ACTION, REVIEW_SET_ASIDE_APPLICATION),
+    STATEMENT_OF_REASONS("statementOfReasons","Statement of Reasons", SOR_REQUEST, "Statement of Reasons Request Made", "Statement of Reasons Request Made", STATEMENT_OF_REASONS_REQUESTED, AWAITING_ADMIN_ACTION, REVIEW_STATEMENT_OF_REASONS_APPLICATION);
 
     private final String ccdDefinition;
     private final String descriptionEn;
