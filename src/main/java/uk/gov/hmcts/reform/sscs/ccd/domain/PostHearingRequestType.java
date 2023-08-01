e3609c326f3741e6c5cc723c4537107da2d2351b
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-08-01 11:17:40 +0100

    update review state SSCS-10588

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PostHearingRequestType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PostHearingRequestType.java
@@ -12,6 +12,7 @@ import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.SET_ASIDE_REQUEST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.SOR_REQUEST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReferralReason.POST_HEARING_REQUEST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReviewState.AWAITING_ADMIN_ACTION;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReviewState.REVIEW_BY_JUDGE;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
@@ -20,11 +21,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PostHearingRequestType implements CcdCallbackMap {
-    CORRECTION("correction","Correction", CORRECTION_REQUEST, "Correction Request Made", "Correction Request Made", CORRECTION_REQUESTED, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST),
-    LIBERTY_TO_APPLY("libertyToApply","Liberty to Apply", LIBERTY_TO_APPLY_REQUEST, "Liberty to Apply Request Made", "Liberty to Apply Request Made", LIBERTY_TO_APPLY_REQUESTED, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST),
-    PERMISSION_TO_APPEAL("permissionToAppeal","Permission to Appeal", PERMISSION_TO_APPEAL_REQUEST, "Permission to Appeal Request Made", "Permission to Appeal Request Made", PERMISSION_TO_APPEAL_REQUESTED, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST),
-    SET_ASIDE("setAside","Set Aside", SET_ASIDE_REQUEST, "Set Aside Request Made", "Set Aside Request Made", SET_ASIDE_REQUESTED, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST),
-    STATEMENT_OF_REASONS("statementOfReasons","Statement of Reasons", SOR_REQUEST, "Statement of Reasons Request Made", "Statement of Reasons Request Made", STATEMENT_OF_REASONS_REQUESTED, AWAITING_ADMIN_ACTION, POST_HEARING_REQUEST);
+    CORRECTION("correction","Correction", CORRECTION_REQUEST, "Correction Request Made", "Correction Request Made", CORRECTION_REQUESTED, AWAITING_ADMIN_ACTION),
+    LIBERTY_TO_APPLY("libertyToApply","Liberty to Apply", LIBERTY_TO_APPLY_REQUEST, "Liberty to Apply Request Made", "Liberty to Apply Request Made", LIBERTY_TO_APPLY_REQUESTED, AWAITING_ADMIN_ACTION),
+    PERMISSION_TO_APPEAL("permissionToAppeal","Permission to Appeal", PERMISSION_TO_APPEAL_REQUEST, "Permission to Appeal Request Made", "Permission to Appeal Request Made", PERMISSION_TO_APPEAL_REQUESTED, REVIEW_BY_JUDGE),
+    SET_ASIDE("setAside","Set Aside", SET_ASIDE_REQUEST, "Set Aside Request Made", "Set Aside Request Made", SET_ASIDE_REQUESTED, AWAITING_ADMIN_ACTION),
+    STATEMENT_OF_REASONS("statementOfReasons","Statement of Reasons", SOR_REQUEST, "Statement of Reasons Request Made", "Statement of Reasons Request Made", STATEMENT_OF_REASONS_REQUESTED, AWAITING_ADMIN_ACTION);
 
     private final String ccdDefinition;
     private final String descriptionEn;
@@ -33,7 +34,7 @@ public enum PostHearingRequestType implements CcdCallbackMap {
     private final String callbackDescription;
     private final DwpState postCallbackDwpState;
     private final InterlocReviewState postCallbackInterlocState;
-    private final InterlocReferralReason postCallbackInterlocReason;
+    private final InterlocReferralReason postCallbackInterlocReason = POST_HEARING_REQUEST;
 
     @Override
     @JsonValue
