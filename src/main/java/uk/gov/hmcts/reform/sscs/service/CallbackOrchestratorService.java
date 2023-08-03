3ef57cb7ff98e23f60a3e7fff0690546c911061d
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-08-03 15:36:31 +0100

    remove return status code SSCS-10587

--- src/main/java/uk/gov/hmcts/reform/sscs/service/CallbackOrchestratorService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/CallbackOrchestratorService.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts.reform.sscs.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
@@ -18,14 +16,13 @@ public class CallbackOrchestratorService {
     private final CallbackOrchestratorApi callbackOrchestratorApi;
     private final IdamService idamService;
 
-    public HttpStatusCode sendMessageToCallbackOrchestrator(Callback<SscsCaseData> callback) {
+    @SuppressWarnings("unused")
+    public void sendMessageToCallbackOrchestrator(Callback<SscsCaseData> callback) {
         IdamTokens idamTokens = idamService.getIdamTokens();
 
         log.info("Sending message to the callback orchestrator for the event {}", callback.getEvent());
 
-        ResponseEntity<String> response = callbackOrchestratorApi.sendMessageToCallbackOrchestrator(
+        callbackOrchestratorApi.sendMessageToCallbackOrchestrator(
                 idamTokens.getIdamOauth2Token(), idamTokens.getServiceAuthorization(), callback);
-
-        return response.getStatusCode();
     }
 }
