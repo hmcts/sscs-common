8ac097bcf3f8ceae3f1f59863ffd668a06beefbd
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-09-18 11:29:14 +0100

    add correct null checking SSCS-11339

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/JudicialUserPanel.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/JudicialUserPanel.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
@@ -27,13 +28,14 @@ public class JudicialUserPanel {
     @SuppressWarnings("unused")
     @JsonIgnore
     public List<JudicialUserBase> getAllPanelMembers() {
+        List<JudicialUserBase> allPanelMembers;
         if (nonNull(panelMembers)) {
-            List<JudicialUserBase> allPanelMembers = panelMembers.stream().map(CollectionItem::getValue).collect(Collectors.toList());
+            allPanelMembers = panelMembers.stream().map(CollectionItem::getValue).toList();
             allPanelMembers.add(0, assignedTo);
-
-            return allPanelMembers;
+        } else {
+            allPanelMembers = List.of(assignedTo);
         }
 
-        return List.of(assignedTo);
+        return allPanelMembers.stream().filter(Objects::nonNull).toList();
     }
 }
