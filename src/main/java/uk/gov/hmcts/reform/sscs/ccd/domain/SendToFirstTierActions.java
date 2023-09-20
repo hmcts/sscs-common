4f080e28323f65147304d41a3488a700c001e6c0
NickHill111 <47982351+nickhill111@users.noreply.github.com>
2023-09-20 16:59:00 +0100

    Sscs 11339 remit to first tier (#1521)
    * Common: add decision remitted
    SSCS-11339
    
    * remove whitespcae
    SSCS-11339
    
    * add judicialuserpanel type
    SSCS-11339
    
    * add method to return all panel members
    SSCS-11339
    
    * add document type
    SSCS-11339
    
    * add judicial user panel getter
    SSCS-11339
    
    * put judicaluserpanel in hearing details
    SSCS-11339
    
    * add new function to get judicial user from personal code
    SSCS-11339
    
    * update to have a list of panel members
    SSCS-11339
    
    * add null check
    SSCS-11339
    
    * add correct null checking
    SSCS-11339
    
    * refactor null checks
    SSCS-11339
    
    * put into hearing details object
    SSCS-11339
    
    * fix error
    SSCS-11339
    
    * add back to judicial user panel
    SSCS-11339
    
    * add json property
    SSCS-11339
    
    * rejig getAllPanelMembers
    SSCS-11339
    
    * fix getAllPanelMembers and do a little cleanup
    SSCS-11339
    
    * retrigger build
    SSCS-11339
    
    * update review by judge
    SSCS-11339
    
    ---------
    
    Co-authored-by: Nicholas Hill <nicholas.hill@HMCTS.net>

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SendToFirstTierActions.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SendToFirstTierActions.java
@@ -4,20 +4,22 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReviewState.REVIEW_BY_JUDGE;
+
 @Getter
 @AllArgsConstructor
 public enum SendToFirstTierActions implements CcdCallbackMap {
-
-    DECISION_REMADE("remade", "Decision remade by UT", EventType.UPPER_TRIBUNAL_DECISION, "Decision remade by UT", "Decision remade by UT"),
-    DECISION_REFUSED("refused", "Decision refused by UT", EventType.UPPER_TRIBUNAL_DECISION, "Decision refused by UT", "Decision refused by UT");
+    DECISION_REMADE("remade", "Decision remade by UT", EventType.UPPER_TRIBUNAL_DECISION, "Decision remade by UT", "Decision remade by UT", null, null),
+    DECISION_REFUSED("refused", "Decision refused by UT", EventType.UPPER_TRIBUNAL_DECISION, "Decision refused by UT", "Decision refused by UT", null, null),
+    DECISION_REMITTED("remitted", "Remit from UT", EventType.UPPER_TRIBUNAL_DECISION, "Remit from UT", "Remit from UT", null, REVIEW_BY_JUDGE);
 
     private final String ccdDefinition;
     private final String descriptionEn;
     private final EventType callbackEvent;
     private final String callbackSummary;
     private final String callbackDescription;
-    private final DwpState postCallbackDwpState = null;
-    private final InterlocReviewState postCallbackInterlocState = null;
+    private final DwpState postCallbackDwpState;
+    private final InterlocReviewState postCallbackInterlocState;
     private final InterlocReferralReason postCallbackInterlocReason = null;
 
     @Override
