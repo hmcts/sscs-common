e090e4511bc827ff292ce0bd40eec58ed25bee38
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-09-20 12:51:07 +0100

    update callback summary SSCS-10581

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/AdminCorrectionType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/AdminCorrectionType.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AdminCorrectionType implements CcdCallbackMap {
-    BODY("bodyCorrection", "Body correction - Send to judge", EventType.ADMIN_CORRECTION_BODY, "Admin correction body", "Admin correction body", null, InterlocReviewState.REVIEW_BY_JUDGE, InterlocReferralReason.REVIEW_CORRECTION_REQUEST),
+    BODY("bodyCorrection", "Body correction - Send to judge", EventType.ADMIN_CORRECTION_BODY, "Correction Sent to Judge", "Correction Sent to Judge", null, InterlocReviewState.REVIEW_BY_JUDGE, InterlocReferralReason.REVIEW_CORRECTION_REQUEST),
     HEADER("headerCorrection", "Header correction", EventType.ADMIN_CORRECTION_HEADER, "Admin correction header", "Admin correction header", DwpState.CORRECTED_DECISION_NOTICE_ISSUED, InterlocReviewState.NONE, null);
 
     private final String ccdDefinition;
