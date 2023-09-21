780852911bcc3b5b7a58e7d0126ac582725f7f1b
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-09-21 15:09:09 +0100

    remove unused import

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingDetails.java
@@ -5,7 +5,6 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDate;
