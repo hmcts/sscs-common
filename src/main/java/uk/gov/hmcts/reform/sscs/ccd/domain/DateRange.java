a571bafa8aba80420e560dbf3220422190905e30
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-10-16 16:01:27 +0100

    common: add methods to get the correct dates SSCS-12058

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DateRange.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DateRange.java
@@ -1,17 +1,22 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
 
+import java.time.LocalDate;
+
+import static java.util.Objects.isNull;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Value
 @Builder(toBuilder = true)
 public class DateRange {
-    private String start;
-    private String end;
+    String start;
+    String end;
 
     @JsonCreator
     public DateRange(@JsonProperty("start") String start,
@@ -20,4 +25,28 @@ public class DateRange {
         this.end = end;
     }
 
+    @SuppressWarnings("unused")
+    @JsonIgnore
+    public LocalDate getStartDate() {
+        return getLocalDate(start, end);
+    }
+
+    @SuppressWarnings("unused")
+    @JsonIgnore
+    public LocalDate getEndDate() {
+        return getLocalDate(end, start);
+    }
+
+    @JsonIgnore
+    private LocalDate getLocalDate(String date1, String date2) {
+        if (isNull(date1)) {
+            if (isNull(date2)) {
+                return null;
+            }
+
+            return LocalDate.parse(date2);
+        }
+
+        return LocalDate.parse(date1);
+    }
 }
