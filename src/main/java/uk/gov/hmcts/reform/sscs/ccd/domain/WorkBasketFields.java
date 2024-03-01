b3fbea6b038da97d7919875de2d9730e412bdff9
DGordonV1 <david.gordon@hmcts.net>
2024-03-01 16:07:37 +0000

    SSCS-12031 - Update to string

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/WorkBasketFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/WorkBasketFields.java
@@ -7,9 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
