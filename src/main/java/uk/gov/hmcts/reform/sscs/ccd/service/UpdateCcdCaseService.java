51cf7d074faba32dee7c427354e95fb63f547b54
andrewmcmahon <andrew.mcmahon@solirius.com>
2024-03-27 12:15:29 +0000

    Initial override

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/service/UpdateCcdCaseService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/service/UpdateCcdCaseService.java
@@ -35,9 +35,9 @@ public class UpdateCcdCaseService {
     }
 
     @Retryable
-    public SscsCaseDetails updateCaseV2(Long caseId, String eventType, String summary, String description, IdamTokens idamTokens, Consumer<SscsCaseDetails> mutator) {
-        return updateCaseV2(caseId, eventType, idamTokens, caseDetails -> {
-            mutator.accept(caseDetails);
+    public SscsCaseDetails updateCaseV2(Long caseId, String eventType, String summary, String description, IdamTokens idamTokens, Consumer<SscsCaseData> mutator) {
+        return updateCaseV2(caseId, eventType, idamTokens, (Function<SscsCaseData, UpdateResult>) data -> {
+            mutator.accept(data);
             return new UpdateResult(summary, description);
         });
     }
@@ -48,6 +48,38 @@ public class UpdateCcdCaseService {
     }
 
     public record UpdateResult(String summary, String description) { }
+
+    /**
+     * Update a case while making correct use of CCD's optimistic locking.
+     * Changes can be made to case data by the provided consumer which will always be provided
+     * the current version of case data from CCD's start event.
+     */
+    @Retryable
+    public SscsCaseDetails updateCaseV2(Long caseId, String eventType, IdamTokens idamTokens, Function<SscsCaseData, UpdateResult> mutator) {
+        log.info("UpdateCaseV2 for caseId {} and eventType {}", caseId, eventType);
+        StartEventResponse startEventResponse = ccdClient.startEvent(idamTokens, caseId, eventType);
+        var data = sscsCcdConvertService.getCaseData(startEventResponse.getCaseDetails().getData());
+
+        /**
+         * @see uk.gov.hmcts.reform.sscs.ccd.deserialisation.SscsCaseCallbackDeserializer#deserialize(String)
+         * setCcdCaseId & sortCollections are called above, so this functionality has been replicated here preserving existing logic
+         */
+        data.setCcdCaseId(caseId.toString());
+        data.sortCollections();
+
+        var result = mutator.apply(data);
+        CaseDataContent caseDataContent = sscsCcdConvertService.getCaseDataContent(data, startEventResponse, result.summary, result.description);
+
+        return sscsCcdConvertService.getCaseDetails(ccdClient.submitEventForCaseworker(idamTokens, caseId, caseDataContent));
+    }
+
+    @Retryable
+    public SscsCaseDetails updateCaseV2(Long caseId, String eventType, String summary, String description, IdamTokens idamTokens, Consumer<SscsCaseDetails> mutator) {
+        return updateCaseV2(caseId, eventType, idamTokens, (Function<SscsCaseDetails, UpdateResult>) caseDetails -> {
+            mutator.accept(caseDetails);
+            return new UpdateResult(summary, description);
+        });
+    }
 
     /**
      * Update a case while making correct use of CCD's optimistic locking.
