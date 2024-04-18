64da578e9502dc76f66808bbba1eeaa3992b5ee0
Gemma Talbot <gemma.talbot@solirius.com>
2024-04-18 14:10:10 +0100

    static calculateMaxDwpResponseDays

--- src/main/java/uk/gov/hmcts/reform/sscs/utility/dwpResponseUtil.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/utility/dwpResponseUtil.java
@@ -5,7 +5,7 @@ public class dwpResponseUtil {
     public static final int MAX_DWP_RESPONSE_DAYS = 28;
     public static final int MAX_DWP_RESPONSE_DAYS_CHILD_SUPPORT = 42;
 
-    public int calculateMaxDwpResponseDays(String benefitCode) {
+    public static int calculateMaxDwpResponseDays(String benefitCode) {
         if (benefitCode == "childSupport" ) {
             return MAX_DWP_RESPONSE_DAYS_CHILD_SUPPORT;
         }
