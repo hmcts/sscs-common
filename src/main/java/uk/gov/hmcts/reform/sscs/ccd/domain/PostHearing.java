3f144552b1158844f68e4b929f94f03e8f19dbee
Andrei Trushkov <andrei.trushkov@hmcts.net>
2024-04-19 17:00:49 +0100

    new field for wa

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PostHearing.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PostHearing.java
@@ -34,6 +34,9 @@ public class PostHearing {
     @JsonProperty("statementOfReasons")
     private StatementOfReasons statementOfReasons;
     @Getter(AccessLevel.NONE)
+    @JsonProperty("sorRequestInTime")
+    private YesNo sorRequestInTime;
+    @Getter(AccessLevel.NONE)
     @JsonProperty("permissionToAppeal")
     private PermissionToAppeal permissionToAppeal;
     @Getter(AccessLevel.NONE)
@@ -98,20 +101,12 @@ public class PostHearing {
     @SuppressWarnings("unused")
     @JsonIgnore
     public RequestFormat getRequestFormat() {
-        switch (requestType) {
-            case SET_ASIDE:
-                return getSetAside().getRequestFormat();
-            case CORRECTION:
-                return getCorrection().getRequestFormat();
-            case STATEMENT_OF_REASONS:
-                return getStatementOfReasons().getRequestFormat();
-            case LIBERTY_TO_APPLY:
-                return getLibertyToApply().getRequestFormat();
-            case PERMISSION_TO_APPEAL:
-                return getPermissionToAppeal().getRequestFormat();
-            default:
-                break;
-        }
-        return null;
+        return switch (requestType) {
+            case SET_ASIDE -> getSetAside().getRequestFormat();
+            case CORRECTION -> getCorrection().getRequestFormat();
+            case STATEMENT_OF_REASONS -> getStatementOfReasons().getRequestFormat();
+            case LIBERTY_TO_APPLY -> getLibertyToApply().getRequestFormat();
+            case PERMISSION_TO_APPEAL -> getPermissionToAppeal().getRequestFormat();
+        };
     }
 }
