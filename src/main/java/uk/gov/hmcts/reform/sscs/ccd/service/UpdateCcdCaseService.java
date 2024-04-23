73813dae0d8f5f10d8995b511ca5b61cca128b8e
nitinprabhuhmcts <nitin.prabhu2@hmcts.net>
2024-04-24 00:11:07 +0100

    Update recover params

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/service/UpdateCcdCaseService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/service/UpdateCcdCaseService.java
@@ -54,7 +54,6 @@ public class UpdateCcdCaseService {
      * Changes can be made to case data by the provided consumer which will always be provided
      * the current version of case data from CCD's start event.
      */
-    @Retryable(maxAttempts = 2)
     public SscsCaseDetails updateCaseV2(Long caseId, String eventType, IdamTokens idamTokens, Function<SscsCaseData, UpdateResult> mutator) {
         log.info("UpdateCaseV2 for caseId {} and eventType {}", caseId, eventType);
         StartEventResponse startEventResponse = ccdClient.startEvent(idamTokens, caseId, eventType);
@@ -122,22 +121,12 @@ public class UpdateCcdCaseService {
      * Need to provide this so that recoverable/non-recoverable exception doesn't get wrapped in an IllegalArgumentException
      */
     @Recover
-    public SscsCaseDetails recoverUpdateCaseV2(Long caseId, String eventType, IdamTokens idamTokens, Function<SscsCaseData, UpdateResult> mutator) {
+    public SscsCaseDetails recoverUpdateCaseV2(Long caseId, String eventType, String summary, String description, IdamTokens idamTokens, Consumer<SscsCaseData> mutator) {
         log.error("In recover method(recoverUpdateCaseV2) for caseId {} and eventType {}", caseId, eventType);
 
-        StartEventResponse startEventResponse = ccdClient.startEvent(idamTokens, caseId, eventType);
-        var data = sscsCcdConvertService.getCaseData(startEventResponse.getCaseDetails().getData());
-
-        /**
-         * @see uk.gov.hmcts.reform.sscs.ccd.deserialisation.SscsCaseCallbackDeserializer#deserialize(String)
-         * setCcdCaseId & sortCollections are called above, so this functionality has been replicated here preserving existing logic
-         */
-        data.setCcdCaseId(caseId.toString());
-        data.sortCollections();
-
-        var result = mutator.apply(data);
-        CaseDataContent caseDataContent = sscsCcdConvertService.getCaseDataContent(data, startEventResponse, result.summary, result.description);
-
-        return sscsCcdConvertService.getCaseDetails(ccdClient.submitEventForCaseworker(idamTokens, caseId, caseDataContent));
+        return updateCaseV2(caseId, eventType, idamTokens, data -> {
+            mutator.accept(data);
+            return new UpdateResult(summary, description);
+        });
     }
 }
