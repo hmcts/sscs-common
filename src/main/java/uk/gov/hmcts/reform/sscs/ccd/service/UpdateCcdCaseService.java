23b877353144f5e602dc5961cc8d2c353039c6d6
adusumillipraveen <praveen.adusumilli@hmcts.net>
2024-04-26 09:28:00 +0100

    test use of deprecated method

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/service/UpdateCcdCaseService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/service/UpdateCcdCaseService.java
@@ -73,6 +73,10 @@ public class UpdateCcdCaseService {
         return sscsCcdConvertService.getCaseDetails(ccdClient.submitEventForCaseworker(idamTokens, caseId, caseDataContent));
     }
 
+    /**
+     * @deprecated  Use UpdateCaseV2 methods as this could cause potential data issues.
+     * */
+    @Deprecated(forRemoval = true)
     @Retryable
     public SscsCaseDetails updateCase(SscsCaseData caseData, Long caseId, String eventType, String summary, String description, IdamTokens idamTokens) {
         log.info("UpdateCase for caseId {} and eventType {}", caseId, eventType);
@@ -82,7 +86,10 @@ public class UpdateCcdCaseService {
 
         return sscsCcdConvertService.getCaseDetails(ccdClient.submitEventForCaseworker(idamTokens, caseId, caseDataContent));
     }
-
+    /**
+     * @deprecated  Use UpdateCaseV2 methods as this could cause potential data issues.
+     * */
+    @Deprecated(forRemoval = true)
     public SscsCaseDetails updateCase(SscsCaseData caseData, Long caseId, String eventId, String eventToken, String eventType, String summary,
                                         String description, IdamTokens idamTokens) {
         log.info("UpdateCase for caseId {} eventToken {} and eventType {}", caseId, eventToken, eventType);
