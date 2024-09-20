2c4e3e96680418a05af423d6e172776362820ac1
Katalin Kovacs <katalin.kovacs@solirius.com>
2024-09-20 12:03:19 +0100

    Revert "JSON property update"
    This reverts commit b31a8a70b85add58f6281d1e6eb22979caa1cbc2.

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/UploadParty.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/UploadParty.java
@@ -11,7 +11,7 @@ public enum UploadParty {
     CTSC("ctsc", "CTSC clerk"),
     @JsonProperty("appellant")
     APPELLANT("appellant", "Appellant"),
-    @JsonProperty("appointee")
+    @JsonProperty("appellant")
     APPOINTEE("appointee", "Appointee"),
     @JsonProperty("rep")
     REP("rep", "Representative"),
