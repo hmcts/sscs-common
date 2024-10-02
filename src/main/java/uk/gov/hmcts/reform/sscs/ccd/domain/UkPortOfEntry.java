5a5c6a7aabb230d5c9295760f1732b3e19e2c6c9
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-10-02 13:28:41 +0100

    SSCSCI-1144: Create enum for UkPortOfEntry

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/UkPortOfEntry.java
@@ -0,0 +1,117 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+public enum UkPortOfEntry {
+
+    ABERDEEN("Aberdeen", "Sea traffic", "GB000434"),
+    ABERDEEN_AIRPORT("Aberdeen Airport", "Air traffic", "GB000411"),
+    AVONMOUTH_1("Avonmouth 1", "Sea traffic", "GB000033"),
+    BARROW_IN_FURNESS("Barrow in Furness", "Sea traffic", "GB003010"),
+    BARRY_DOCK("Barry Dock", "Sea traffic", "GB000125"),
+    BERWICK_UPON_TWEED("Berwick upon Tweed", "Sea traffic", "GB003020"),
+    BIGGIN_HILL("Biggin Hill", "Air traffic", "GB003290"),
+    BIRMINGHAM_AIRPORT("Birmingham Airport", "Air traffic", "GB000011"),
+    BLACKPOOL_INTERNATIONAL_AIRPORT("Blackpool International Airport", "Air traffic", "GB003030"),
+    BORDER_FORCE_HARWICH("Border Force, Harwich", "Sea traffic", "GB000052"),
+    BOSTON("Boston", "Sea traffic", "GB000244"),
+    BOURNEMOUTH_HURN_AIRPORT("Bournemouth (Hurn) Airport", "Air traffic", "GB003060"),
+    BRIZE_NORTON("Brize Norton", "Air traffic", "GB003070"),
+    CAMBRIDGE_AIRPORT("Cambridge Airport", "Air traffic", "GB003080"),
+    CARDIFF("Cardiff", "Sea traffic", "GB000328"),
+    CARDIFF_INTERNATIONAL_AIRPORT("Cardiff International Airport", "Air traffic", "GB000122"),
+    CHATHAM("Chatham", "Sea traffic", "GB000480"),
+    COVENTRY_INTERNATIONAL_HUB("Coventry International Hub", "Not applicable", "GB000500"),
+    DONCASTER("Doncaster", "Air traffic", "GB001011"),
+    DOVER_OTS_FREIGHT_CLEARANCE("Dover (OTS) Freight Clearance", "Sea traffic", "GB000040"),
+    DOVER_FOLKESTONE_EUROTUNNEL_FREIGHT("Dover/ Folkestone Eurotunnel Freight", "Road and rail traffic", "GB000060"),
+    DUNDEE_SEA("Dundee (Sea)", "Sea traffic", "GB003100"),
+    DURHAM_TEES_VALLEY_TEESIDE_APT("Durham Tees Valley (Teeside Apt)", "Air traffic", "GB003180"),
+    EDINBURGH_AIRPORT("Edinburgh Airport", "Air traffic", "GB000128"),
+    EXETER_AIRPORT("Exeter Airport", "Air traffic", "GB000297"),
+    FALMOUTH("Falmouth", "Sea traffic", "GB000108"),
+    FARNBOROUGH_AIRPORT("Farnborough Airport", "Air traffic", "GB003110"),
+    FELIXSTOWE("Felixstowe", "Sea traffic", "GB000051"),
+    FOLKESTONE("Folkestone", "Sea traffic", "GB000041"),
+    GLASGOW_DOCKS("Glasgow Docks", "Sea traffic", "GB003040"),
+    GLOUCESTER_STAVERTON_AIRPORT("Gloucester (Staverton) Airport", "Air traffic", "GB003120"),
+    GRANGEMOUTH("Grangemouth", "Sea traffic", "GB000059"),
+    GREAT_YARMOUTH("Great Yarmouth", "Sea traffic", "GB000054"),
+    GRIMSBY("Grimsby", "Sea traffic", "GB000073"),
+    HARTLEPOOL("Hartlepool", "Sea traffic", "GB003140"),
+    HOLYHEAD("Holyhead", "Sea traffic", "GB000124"),
+    HULL("Hull", "Sea and road traffic", "GB000072"),
+    HUMBERSIDE_INTERNATIONAL_AIRPORT("Humberside International Airport", "Air traffic", "GB003130"),
+    HWDC_LANGLEY("HWDC Langley", "Not applicable", "GB000501"),
+    IMMINGHAM("Immingham", "Sea and road traffic", "GB000074"),
+    INVERNESS("Inverness", "Sea traffic", "GB003150"),
+    ISLE_OF_MAN_CUSTOMS_EXCISE("Isle of Man Customs & Excise", "Air and sea traffic", "GB000450"),
+    JERSEY_CUSTOMS_AND_IMMIGRATION("Jersey Customs and Immigration", "Air and sea traffic", "GB000465"),
+    KEMBLE_AIRPORT("Kemble Airport", "Air traffic", "GB003160"),
+    KINGS_LYNN("King’s Lynn", "Sea traffic", "GB000055"),
+    KIRKWALL_ORKNEY_ISLANDS("Kirkwall (Orkney Islands)", "Sea traffic", "GB003170"),
+    LEEDS_BRADFORD_AIRPORT("Leeds Bradford Airport", "Air traffic", "GB000087"),
+    LERWICK("Lerwick", "Air and sea traffic", "GB001016"),
+    LIVERPOOL_JOHN_LENNON_AIRPORT("Liverpool John Lennon Airport", "Air traffic", "GB003190"),
+    LIVERPOOL_SEAFORTH_S2_BERTH("Liverpool Seaforth S2 berth", "Sea traffic", "GB000080"),
+    LONDON_ASHFORD_LYDD_AIRPORT("London Ashford (Lydd) Airport", "Air and sea traffic", "GB003200"),
+    LONDON_CITY_AIRPORT("London City Airport", "Air traffic", "GB000130"),
+    LONDON_GATEWAY_PORT("London Gateway Port", "Sea traffic", "GB000170"),
+    LONDON_GATWICK_AIRPORT_CARGO_DIST("London Gatwick Airport Cargo Dist", "Air traffic", "GB000085"),
+    LONDON_HEATHROW_CARGO("London Heathrow Cargo", "Air traffic", "GB000084"),
+    LONDON_SOUTHEND_AIRPORT("London Southend Airport", "Air traffic", "GB000091"),
+    LUTON_AIRPORT_FCT("Luton Airport FCT", "Air traffic", "GB000102"),
+    MANCHESTER("Manchester", "Air traffic", "GB000191"),
+    MANCHESTER_DOCKS("Manchester Docks", "Sea traffic", "GB003220"),
+    NEWCASTLE_AIRPORT("Newcastle Airport", "Air traffic", "GB000098"),
+    NEWCASTLE_UPON_TYNE("Newcastle-upon-Tyne", "Air, sea and road traffic", "GB000218"),
+    NEWHAVEN("Newhaven", "Sea and road traffic", "GB003250"),
+    NEWPORT("Newport", "Sea traffic", "GB000049"),
+    NORWICH_AIRPORT("Norwich Airport", "Air traffic", "GB000058"),
+    OXFORD_KIDLINGTON_AIRPORT("Oxford (Kidlington) Airport", "Air traffic", "GB003230"),
+    PEMBROKE_DOCK("Pembroke Dock", "Sea traffic", "GB000126"),
+    PETERHEAD("Peterhead", "Sea traffic", "GB000416"),
+    PLYMOUTH("Plymouth", "Air traffic", "GB000250"),
+    PLYMOUTH_DOCKS("Plymouth Docks", "Sea traffic", "GB003050"),
+    POOLE_FERRY_TERMINAL("Poole Ferry Terminal", "Sea traffic", "GB000291"),
+    PORTREE_ISLE_OF_SKYE("Portree (Isle of Skye)", "Sea traffic", "GB003240"),
+    PORTSMOUTH("Portsmouth", "Sea traffic", "GB000292"),
+    PRESTWICK_AIRPORT("Prestwick Airport", "Air traffic", "GB000070"),
+    RAMSGATE("Ramsgate", "Sea traffic", "GB000047"),
+    SCOTLAND_FRONTIER("Scotland Frontier", "Air traffic", "GB000067"),
+    SHEERNESS("Sheerness", "Sea traffic", "GB000045"),
+    SOUTHAMPTON("Southampton", "Sea traffic", "GB000290"),
+    SOUTHAMPTON_EASTLEIGH_AIRPORT("Southampton (Eastleigh) Airport", "Air traffic", "GB003090"),
+    ST_PETER_PORT_GUERNSEY("St Peter Port, Guernsey", "Air and sea traffic", "GB000461"),
+    STANSTED_AIRPORT_FCT("Stansted Airport FCT", "Air traffic", "GB000121"),
+    STORNOWAY_ISLE_OF_LEWIS("Stornoway (Isle of Lewis)", "Sea traffic", "GB003260"),
+    STORNOWAY_AIRPORT("Stornoway Airport", "Air traffic", "GB003270"),
+    SWANSEA("Swansea", "Sea traffic", "GB000123"),
+    TEESPORT("Teesport", "Sea traffic", "GB000219"),
+    THAMES_EUROPORT_DIFT("Thames Europort (DIFT)", "Sea traffic", "GB000099"),
+    THAMESPORT("Thamesport", "Sea traffic", "GB000150"),
+    TILBURY("Tilbury", "Sea traffic", "GB000093"),
+    UKBA_EAST_MIDLANDS_AIRPORT("UKBA, East Midlands Airport", "Air traffic", "GB000245"),
+    UKBF_BRISTOL_INTERNATIONAL_AIRPORT("UKBF, Bristol International Airport", "Air traffic", "GB000029"),
+    WORKINGTON("Workington", "Sea traffic", "GB003280");
+
+    private final String label;
+    private final String trafficType;
+    private final String locationCode;
+
+    UkPortOfEntry(String label, String trafficType, String locationCode) {
+        this.label = label;
+        this.trafficType = trafficType;
+        this.locationCode = locationCode;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getTrafficType() {
+        return trafficType;
+    }
+
+    public String getLocationCode() {
+        return locationCode;
+    }
+}
