c8f966dffab23a2bd805e2e0057d772d361257e3
Katalin Kovacs <katalin.kovacs@solirius.com>
2024-10-14 08:45:12 +0100

    update to hearingOutcomeDetails

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingOutcome.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingOutcome.java
@@ -1,27 +1,20 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
-import org.apache.commons.lang3.builder.CompareToBuilder;
-import org.apache.commons.lang3.math.NumberUtils;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Value
 @Builder(toBuilder = true)
-public class HearingOutcome implements Comparable<HearingOutcome> {
-    private HearingOutcomeDetails value;
+public class HearingOutcome {
+    private List<HearingOutcomeDetails> value;
 
     @JsonCreator
-    public HearingOutcome(@JsonProperty("value") HearingOutcomeDetails value) {this.value = value; }
+    public HearingOutcome(@JsonProperty("value") List<HearingOutcomeDetails> value) {this.value = value; }
 
-    @Override
-    public int compareTo(HearingOutcome o) {
-        return new CompareToBuilder()
-            .append(NumberUtils.createInteger(this.value.getCompletedHearingId()),
-                NumberUtils.createInteger(o.getValue().getCompletedHearingId()))
-            .toComparison();
-    }
 }
