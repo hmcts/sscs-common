925cf49cdee57683130005fb6bb48024f568f99c
Timi Olubusi <timiolubusi@icloud.com>
2024-10-14 16:07:28 +0100

    Fix for the existing test for the new ibcaReference tests

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Identity.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Identity.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class Identity {
     private String dob;
     @NationalInsuranceNumber(groups = UniversalCreditValidationGroup.class)
     private String nino;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String ibcaReference;
 
     @JsonCreator
