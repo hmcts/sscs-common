da07867b4d45a9d0d08a7fa27151b2d28158a5b7
Katalin Kovacs <katalin.kovacs@solirius.com>
2024-10-15 11:03:20 +0100

    update to hearingOutcomeDetails

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingOutcomeDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingOutcomeDetails.java
@@ -1,7 +1,12 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +21,15 @@ import uk.gov.hmcts.reform.sscs.reference.data.model.HearingChannel;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class HearingOutcomeDetails {
     private DynamicList completedHearing;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime hearingStart;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime hearingEnd;
+    private String venueName;
     private String completedHearingId;
     private String hearingOutcome;
     private YesNo didPoAttendHearing;
+    private HearingChannel outcomeHearingChannel;
 }
