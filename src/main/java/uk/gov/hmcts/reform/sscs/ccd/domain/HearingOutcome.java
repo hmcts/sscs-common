e4a9839c34cfd84fd5cd523f2279b7218216119d
Katalin Kovacs <katalin.kovacs@solirius.com>
2024-10-17 17:45:48 +0100

    Created new objects for Hearing Outcome

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingOutcome.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Value;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Value
+@Builder(toBuilder = true)
+public class HearingOutcome {
+    private HearingOutcomeDetails value;
+
+    @JsonCreator
+    public HearingOutcome(@JsonProperty("value") HearingOutcomeDetails value) { this.value = value; }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingOutcomeDetails.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.sscs.reference.data.model.HearingChannel;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class HearingOutcomeDetails {
+    private String completedHearingId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime hearingStartDateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime hearingEndDateTime;
+    private String hearingOutcomeId;
+    private YesNo didPoAttendHearing;
+    private HearingChannel hearingChannelId;
+    private Venue venue;
+    private String epimsId;
+}
