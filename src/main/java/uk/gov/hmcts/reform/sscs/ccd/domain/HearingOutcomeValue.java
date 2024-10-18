137fc81c100b22efc9fb4f7b1cfca2dfaf2f2144
Katalin Kovacs <katalin.kovacs@solirius.com>
2024-10-18 13:41:00 +0100

    Made HOV value

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingOutcomeValue.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/HearingOutcomeValue.java
@@ -2,16 +2,12 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Value;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Data
+@Value
 @Builder(toBuilder = true)
-@AllArgsConstructor
-@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class HearingOutcomeValue {
     private String hearingOutcomeId;
