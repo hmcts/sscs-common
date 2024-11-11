00135611a45da2ec1246028c977ed8b18956f78c
Linus Norton <linusnorton@gmail.com>
2024-11-11 21:01:59 +0000

    SB3 upgrade WIP

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/validation/string/StringNoSpecialCharactersValidator.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/validation/string/StringNoSpecialCharactersValidator.java
@@ -2,8 +2,8 @@ package uk.gov.hmcts.reform.sscs.ccd.validation.string;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
 
 public class StringNoSpecialCharactersValidator implements ConstraintValidator<StringNoSpecialCharacters, String> {
 
