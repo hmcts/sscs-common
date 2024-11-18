8fc380c5d01a0b83b93fbca234344f5ca5d61c58
Timi Olubusi <timiolubusi@icloud.com>
2024-11-18 15:01:23 +0000

    SSCSCI-1406: Cleaning up imports

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/BenefitType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/BenefitType.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
