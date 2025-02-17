090e3e05fd4053e417dcd1d20840db0e1b1fda7c
Nilay <nilay.patel@solirius.com>
2025-02-17 14:41:45 +0000

    SSCSCI-1456 (#1703)
    * Revert "Revert "SSCSCI-1456" (#1702)"
    
    This reverts commit e59dd2e9e72361409c26a233a6d8e4e692e4fbd0.
    
    * allowing hmcHearingType to be missing from CaseField
    
    * addressing comments
    
    * fix

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OverrideFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/OverrideFields.java
@@ -25,6 +25,8 @@ public class OverrideFields {
     private HearingWindow hearingWindow;
     private YesNo autoList;
     private List<CcdValue<CcdValue<String>>> hearingVenueEpimsIds;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private HmcHearingType hmcHearingType;
 
     @SuppressWarnings("unused")
     @JsonIgnore
