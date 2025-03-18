cc46d289b93c0e8ef473dfbd0d6a99f2502c47f4
david-gimelle <david.gimelle@gmail.com>
2025-03-18 12:44:48 +0000

    Add FtaCommunication class

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/FtaCommunication.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class FtaCommunication {
+
+    private LocalDateTime requestDateTime;
+    private String requestTopic;
+    private String requestText;
+    private String requestUserName;
+
+}
+
