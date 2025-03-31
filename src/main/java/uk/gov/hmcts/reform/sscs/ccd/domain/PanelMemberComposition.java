581d79fd1079b429607fa0af282c0ea773e037e1
Gemma Talbot <gemma.talbot@solirius.com>
2025-03-31 14:03:54 +0100

    add panel member object

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Value;
+import uk.gov.hmcts.reform.sscs.reference.data.model.JudicialMemberType;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder(toBuilder = true)
+public class PanelMemberComposition {
+    private JudicialMemberType panelCompositionJudge;
+    private PanelMemberType panelCompositionMemberMedical1;
+    private PanelMemberType panelCompositionMemberMedical2;
+    private PanelMemberType panelCompositionMemberDisabilityQualified;
+}
