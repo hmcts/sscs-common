8f97f83bd2a9fae347dc971e24ccec778e9bc431
Gemma Talbot <gemma.talbot@solirius.com>
2025-04-01 11:58:38 +0100

    add annotations

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
@@ -1,12 +1,19 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.sscs.reference.data.model.JudicialMemberType;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 @Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PanelMemberComposition {
     private JudicialMemberType panelCompositionJudge;
     private PanelMemberType panelCompositionMemberMedical1;
