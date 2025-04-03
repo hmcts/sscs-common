7c856c1de7a565964d295c3ebc40ea0df66ce17b
Gemma Talbot <gemma.talbot@solirius.com>
2025-04-03 16:07:49 +0100

    mimic RegionalProcessingCenter.java

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
@@ -2,23 +2,32 @@ package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Value;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.util.List;
-import java.util.Objects;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
 @Value
 @Builder(toBuilder = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PanelMemberComposition {
-    private PanelMemberCompositionDetails value;
 
     @JsonCreator
-    public PanelMemberComposition(@JsonProperty("value") PanelMemberCompositionDetails value) { this.value = value; }
+    public PanelMemberComposition(@JsonProperty("panelCompositionJudge") String panelCompositionJudge,
+                                  @JsonProperty("panelCompositionMemberMedical1") String panelCompositionMemberMedical1,
+                                  @JsonProperty("panelCompositionMemberMedical2") String panelCompositionMemberMedical2,
+                                  @JsonProperty("panelCompositionDisabilityAndFqMember") String panelCompositionDisabilityAndFqMember) {
+        this.panelCompositionJudge = panelCompositionJudge;
+        this.panelCompositionMemberMedical1 = panelCompositionMemberMedical1;
+        this.panelCompositionMemberMedical2 = panelCompositionMemberMedical2;
+        this.panelCompositionDisabilityAndFqMember = panelCompositionDisabilityAndFqMember;
+    }
+
+    String panelCompositionJudge;
+
+    String panelCompositionMemberMedical1;
+
+    String panelCompositionMemberMedical2;
+
+    String panelCompositionDisabilityAndFqMember;
+
 }
