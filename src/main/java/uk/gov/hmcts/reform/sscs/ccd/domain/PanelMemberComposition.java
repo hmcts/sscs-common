bcfbafbc5c2447bfd7f11788f1be77fe0bf5e0a3
Gemma Talbot <gemma.talbot@solirius.com>
2025-04-04 10:07:56 +0100

    hashim panel comp

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
@@ -1,35 +1,27 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Value;
-
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Value
+
+@Data
 @Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PanelMemberComposition {
 
-    @JsonCreator
-    public PanelMemberComposition(@JsonProperty("panelCompositionJudge") String panelCompositionJudge,
-                                  @JsonProperty("panelCompositionMemberMedical1") String panelCompositionMemberMedical1,
-                                  @JsonProperty("panelCompositionMemberMedical2") String panelCompositionMemberMedical2,
-                                  @JsonProperty("panelCompositionDisabilityAndFqMember") List<String> panelCompositionDisabilityAndFqMember) {
-        this.panelCompositionJudge = panelCompositionJudge;
-        this.panelCompositionMemberMedical1 = panelCompositionMemberMedical1;
-        this.panelCompositionMemberMedical2 = panelCompositionMemberMedical2;
-        this.panelCompositionDisabilityAndFqMember = panelCompositionDisabilityAndFqMember;
-    }
 
-    String panelCompositionJudge;
+    private String panelCompositionJudge;
 
-    String panelCompositionMemberMedical1;
+    private String panelCompositionMemberMedical1;
 
-    String panelCompositionMemberMedical2;
+    private String panelCompositionMemberMedical2;
 
-    List<String> panelCompositionDisabilityAndFqMember;
+    private List<String> panelCompositionDisabilityAndFqMember;
 
 }
