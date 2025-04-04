683841062223dd5fb5bb57082fe2a0385243a032
Gemma Talbot <gemma.talbot@solirius.com>
2025-04-04 14:59:55 +0100

    store panel members as strings

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
@@ -1,24 +1,27 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.reform.sscs.reference.data.model.JudicialMemberType;
 
-import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PanelMemberComposition {
-    private JudicialMemberType panelCompositionJudge;
-    private PanelMemberType panelCompositionMemberMedical1;
-    private PanelMemberType panelCompositionMemberMedical2;
-    private List<PanelMemberType> panelCompositionDisabilityAndFqMember;
+
+
+    private String panelCompositionJudge;
+
+    private String panelCompositionMemberMedical1;
+
+    private String panelCompositionMemberMedical2;
+
+    private List<String> panelCompositionDisabilityAndFqMember;
+
 }
