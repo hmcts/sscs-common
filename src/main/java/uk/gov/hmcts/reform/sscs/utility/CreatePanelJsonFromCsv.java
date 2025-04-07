244693922b52d946a865d52fd67607e5f65e05a0
gemmatalbot <116297417+gemmatalbot@users.noreply.github.com>
2025-04-07 15:05:42 +0100

    Update CreatePanelJsonFromCsv.java

--- src/main/java/uk/gov/hmcts/reform/sscs/utility/CreatePanelJsonFromCsv.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/utility/CreatePanelJsonFromCsv.java
@@ -51,6 +51,7 @@ public class CreatePanelJsonFromCsv {
             Path path = Paths.get("src/main/resources/reference-data/" + LocalDate.now().toString().replace("-", "")
                     .concat("_" + "johTierJsonMapping.json"));
             Files.write(path, panelCompositionObject.toString(4).getBytes()); // Indent with 4 spaces
+            
         } catch (IOException e) {
             throw new RuntimeException("Error reading file", e);
         }
