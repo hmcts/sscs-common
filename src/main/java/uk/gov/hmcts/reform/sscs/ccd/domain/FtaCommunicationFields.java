61f05c9c895549ebc171ec023eff1af7cbfbd789
nilay <nilay.patel@solirius.com>
2025-04-08 13:02:43 +0100

    adding due date

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/FtaCommunicationFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/FtaCommunicationFields.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -31,6 +31,9 @@ public class FtaCommunicationFields {
     private CommunicationRequestTopic tribunalRequestTopic;
     private String tribunalRequestQuestion;
     private TribunalRequestType tribunalRequestType;
+
+    private LocalDate tribunalResponseDueDate;
+    private LocalDate ftaResponseDueDate;
 
     private YesNo awaitingInfoFromFta;
     private YesNo infoProvidedByFta;
