3582779730314d3734492fb2bbcb31b375cde10d
nilay <nilay.patel@solirius.com>
2025-04-09 13:14:18 +0100

    adding case level due date for filter and removing dropdown filter

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/FtaCommunicationFields.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/FtaCommunicationFields.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -32,7 +32,7 @@ public class FtaCommunicationFields {
     private String tribunalRequestQuestion;
     private TribunalRequestType tribunalRequestType;
 
-    private FtaCommunicationFilter ftaCommunicationFilter;
-    private TribunalCommunicationFilter tribunalCommunicationFilter;
+    private LocalDate tribunalResponseDueDate;
+    private LocalDate ftaResponseDueDate;
 }
 
