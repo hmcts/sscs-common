95649e3fd86fbae66fda66eea2c038141bd46629
hashimalisolirius <HashimAli@HMCTS.NET>
2025-04-09 12:14:52 +0100

    move getRoleTypes to common

--- src/main/java/uk/gov/hmcts/reform/sscs/reference/data/service/PanelCategoryService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/reference/data/service/PanelCategoryService.java
@@ -1,22 +1,29 @@
 package uk.gov.hmcts.reform.sscs.reference.data.service;
 
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.isYes;
 import static uk.gov.hmcts.reform.sscs.reference.data.helper.ReferenceDataHelper.getReferenceData;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.reference.data.model.BenefitRoleRelationType;
 import uk.gov.hmcts.reform.sscs.reference.data.model.PanelCategory;
 
 @Getter
 @Setter
+@Slf4j
 @Component
 public class PanelCategoryService {
     private static final String JSON_DATA_LOCATION = "reference-data/panel-category-map.json";
     private List<PanelCategory> panelCategories;
+    @Value("${feature.default-panel-comp.enabled}")
+    private boolean defaultPanelCompEnabled;
 
     public PanelCategoryService() {
         panelCategories = getReferenceData(JSON_DATA_LOCATION, new TypeReference<>() {});
@@ -27,5 +34,20 @@ public class PanelCategoryService {
         return panelCategories.stream()
                 .filter(panelCategorySelection::equals)
                 .findFirst().orElse(null);
+    }
+
+    public List<String> getRoleTypes(SscsCaseData caseData) {
+        if (defaultPanelCompEnabled) {
+            String benefitIssueCode = caseData.getBenefitCode() + caseData.getIssueCode();
+            String specialismCount = caseData.getSscsIndustrialInjuriesData().getPanelDoctorSpecialism() != null
+                    ? caseData.getSscsIndustrialInjuriesData().getSecondPanelDoctorSpecialism() != null
+                    ? "2" : "1" : null;
+            String isFqpm =  isYes(caseData.getIsFqpmRequired()) ? "true" : null;
+            PanelCategory panelComp = getPanelCategory(benefitIssueCode, specialismCount, isFqpm);
+            log.info("Panel Category Map for Case {}: {}", caseData.getCcdCaseId(), panelComp);
+            return panelComp != null ? panelComp.getJohTiers() : Collections.emptyList();
+        } else {
+            return BenefitRoleRelationType.findRoleTypesByBenefitCode(caseData.getBenefitCode());
+        }
     }
 }
--- src/test/java/uk/gov/hmcts/reform/sscs/service/PanelCategoryServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/PanelCategoryServiceTest.java
@@ -1,15 +1,41 @@
 package uk.gov.hmcts.reform.sscs.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsIndustrialInjuriesData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.YesNo;
 import uk.gov.hmcts.reform.sscs.reference.data.model.PanelCategory;
 import uk.gov.hmcts.reform.sscs.reference.data.service.PanelCategoryService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PanelCategoryServiceTest {
 
     private final PanelCategoryService panelCategoryService = new PanelCategoryService();
+    private static final String TRIBUNAL_MEMBER_MEDICAL = "58";
+    private static final String TRIBUNALS_MEMBER_FINANCIALLY_QUALIFIED = "50";
+    private SscsCaseData caseData;
+
+
+    @Before
+    public void setUp() throws Exception {
+        ReflectionTestUtils.setField(panelCategoryService, "defaultPanelCompEnabled", true);
+        caseData = SscsCaseData.builder()
+                .sscsIndustrialInjuriesData(SscsIndustrialInjuriesData.builder().build())
+                .benefitCode("001")
+                .ccdCaseId("1234")
+                .issueCode("AD")
+                .build();
+    }
 
     @Test
     @DisplayName("Valid call to getPanelCategory should return correct johTier")
@@ -33,7 +59,7 @@ public class PanelCategoryServiceTest {
     public void getPanelCategoryWithFQPM() {
         PanelCategory result = panelCategoryService.getPanelCategory("016CC", null, "true");
         assertThat(result).isNotNull();
-        assertThat(result.getJohTiers().stream().anyMatch("50"::equals)).isTrue();
+        assertThat(result.getJohTiers().stream().anyMatch(TRIBUNALS_MEMBER_FINANCIALLY_QUALIFIED::equals)).isTrue();
     }
 
     @Test
@@ -43,7 +69,55 @@ public class PanelCategoryServiceTest {
         PanelCategory twoSpecialismResult = panelCategoryService.getPanelCategory("067CB", "2", null);
         assertThat(oneSpecialismResult).isNotNull();
         assertThat(twoSpecialismResult).isNotNull();
-        assertThat(oneSpecialismResult.getJohTiers().stream().filter("58"::equals).count()).isEqualTo(1);
-        assertThat(twoSpecialismResult.getJohTiers().stream().filter("58"::equals).count()).isEqualTo(2);
+        assertThat(oneSpecialismResult.getJohTiers().stream().filter(TRIBUNAL_MEMBER_MEDICAL::equals).count()).isEqualTo(1);
+        assertThat(twoSpecialismResult.getJohTiers().stream().filter(TRIBUNAL_MEMBER_MEDICAL::equals).count()).isEqualTo(2);
     }
+
+    @DisplayName("getRoleTypes returns an valid list")
+    @Test
+    public void testGetRoles() {
+        List<String> result = panelCategoryService.getRoleTypes(caseData);
+        assertThat(result).isNotEmpty();
+        assertThat(result.get(0)).isEqualTo("84");
+    }
+
+    @DisplayName("getRoleTypes should return an empty list when an invalid benefit issue code is given")
+    @Test
+    public void testGetRolesWithInvalidBenefitIssueCode() {
+        caseData.setBenefitCode(null);
+        caseData.setIssueCode(null);
+        List<String> result = panelCategoryService.getRoleTypes(caseData);
+        assertThat(result).isEmpty();
+    }
+
+    @DisplayName("getRoleTypes should return an empty list when feature flag is turned off and case is not IIDB or CS")
+    @Test
+    public void testGetRolesWithFeatureFlagOff() {
+        ReflectionTestUtils.setField(panelCategoryService, "defaultPanelCompEnabled", false);
+        List<String> result = panelCategoryService.getRoleTypes(caseData);
+        assertThat(result).isEmpty();
+    }
+
+    @DisplayName("getRoleTypes should return a medical member when specialism is on casedata")
+    @Test
+    public void testGetRolesWithSpecialism() {
+        caseData.getSscsIndustrialInjuriesData().setPanelDoctorSpecialism("dummy");
+        caseData.setBenefitCode("067");
+        caseData.setIssueCode("CB");
+        List<String> result = panelCategoryService.getRoleTypes(caseData);
+        assertThat(result).isNotEmpty();
+        assertThat(result.stream().anyMatch(TRIBUNAL_MEMBER_MEDICAL::equals)).isTrue();
+    }
+
+    @DisplayName("getRoleTypes should return FQPM when FQPM = yes on casedata")
+    @Test
+    public void testGetRolesWithFQPM() {
+        caseData.setIsFqpmRequired(YesNo.YES);
+        caseData.setBenefitCode("016");
+        caseData.setIssueCode("CC");
+        List<String> result = panelCategoryService.getRoleTypes(caseData);
+        assertThat(result).isNotEmpty();
+        assertThat(result.stream().anyMatch(TRIBUNALS_MEMBER_FINANCIALLY_QUALIFIED::equals)).isTrue();
+    }
+
 }
