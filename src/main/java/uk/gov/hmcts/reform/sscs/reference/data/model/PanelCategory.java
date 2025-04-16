61ddb234eb7b73721c1d97492379bc35ed9fd77c
hashimalisolirius <140517911+hashimalisolirius@users.noreply.github.com>
2025-04-16 10:03:23 +0100

    SSCSCI-1727: Add panel category map to common (#1721)
    * Add panel category map to common
    
    * update field names
    
    * update PanelCategoryMap
    
    * update json
    
    * add class for converting file from csv to json
    
    * change arraylist to list
    remove check for 85 values
    
    * update test
    
    * update variable names
    
    * remove hashmap from PanelCategoryService
    
    * update filter logic and remove references to map
    
    * move getRoleTypes to common
    
    * remove CreatePanelJsonFromCsv.java
    
    * remove feature flag
    
    * elastic search upgrade
    
    ---------
    
    Co-authored-by: gokul <69240498+gokul-sol@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/reference/data/model/PanelCategory.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.sscs.reference.data.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+import java.util.Objects;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@Data
+@Slf4j
+public class PanelCategory {
+    private String benefitIssueCode;
+    private String category;
+    private String specialismCount;
+    private String fqpm;
+    private List<String> johTiers;
+
+    public PanelCategory(String benefitIssueCode, String specialismCount, String fqpm) {
+        this.benefitIssueCode = benefitIssueCode;
+        this.specialismCount = specialismCount;
+        this.fqpm = fqpm;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PanelCategory that = (PanelCategory) o;
+        return Objects.equals(benefitIssueCode, that.benefitIssueCode)
+                && Objects.equals(specialismCount, that.specialismCount)
+                && Objects.equals(fqpm, that.fqpm);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(benefitIssueCode, specialismCount, fqpm);
+    }
+
+}
