53c0868cc1fbf4007f1c31abadea96a14da9a62e
Gemma Talbot <gemma.talbot@solirius.com>
2025-04-16 19:05:25 +0100

    add caseDetails to SessionAwareMessagingService

--- src/main/java/uk/gov/hmcts/reform/sscs/model/servicebus/NoOpMessagingService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/servicebus/NoOpMessagingService.java
@@ -1,11 +1,12 @@
 package uk.gov.hmcts.reform.sscs.model.servicebus;
 
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.model.servicebus.SessionAwareRequest;
 
 public class NoOpMessagingService implements SessionAwareMessagingService {
 
     @Override
-    public boolean sendMessage(SessionAwareRequest message) {
+    public boolean sendMessage(SessionAwareRequest message, SscsCaseDetails caseDetails) {
         return true;
     }
 }
--- src/main/java/uk/gov/hmcts/reform/sscs/model/servicebus/SessionAwareMessagingService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/servicebus/SessionAwareMessagingService.java
@@ -1,9 +1,10 @@
 package uk.gov.hmcts.reform.sscs.model.servicebus;
 
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.model.servicebus.SessionAwareRequest;
 
 public interface SessionAwareMessagingService {
 
-    boolean sendMessage(SessionAwareRequest message);
+    boolean sendMessage(SessionAwareRequest message, SscsCaseDetails caseDetails);
 
 }
