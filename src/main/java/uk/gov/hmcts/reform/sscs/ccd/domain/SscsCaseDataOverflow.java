c9c932aae3982f337a8fb15fc31d3627d46663eb
nilay <nilay.patel@solirius.com>
2025-05-09 13:26:53 +0100

    adding casedata overflow

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsCaseDataOverflow.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
+public class SscsCaseDataOverflow {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private YesNo selectNextHmcHearingType;
+}
