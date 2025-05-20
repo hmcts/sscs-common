9314bb94696034684e34f6c1c34b53604ae136f5
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2025-05-20 15:39:33 +0100

    Added FQPM methods

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberComposition.java
@@ -1,16 +1,16 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.ArrayList;
 import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.util.ObjectUtils;
-
-import static java.util.Objects.isNull;
 
 @Data
 @Builder(toBuilder = true)
@@ -18,6 +18,8 @@ import static java.util.Objects.isNull;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PanelMemberComposition {
+    private static final String FQPM_REF = PanelMemberType.TRIBUNAL_MEMBER_FINANCIALLY_QUALIFIED.toRef();
+
     private String panelCompositionJudge;
     private String panelCompositionMemberMedical1;
     private String panelCompositionMemberMedical2;
@@ -29,5 +31,27 @@ public class PanelMemberComposition {
                 isNull(panelCompositionMemberMedical1) &&
                 isNull(panelCompositionMemberMedical2) &&
                 ObjectUtils.isEmpty(panelCompositionDisabilityAndFqMember);
+    }
+
+    @JsonIgnore
+    public boolean hasFqpm() {
+        return nonNull(panelCompositionDisabilityAndFqMember)
+            && panelCompositionDisabilityAndFqMember.contains(FQPM_REF);
+    }
+
+    @JsonIgnore
+    public void addFqpm() {
+        if (isNull(panelCompositionDisabilityAndFqMember)) {
+            panelCompositionDisabilityAndFqMember = new ArrayList<>(List.of(FQPM_REF));
+        } else if (!panelCompositionDisabilityAndFqMember.contains(FQPM_REF)) {
+            panelCompositionDisabilityAndFqMember.add(FQPM_REF);
+        }
+    }
+
+    @JsonIgnore
+    public void removeFqpm() {
+        if (nonNull(panelCompositionDisabilityAndFqMember) && panelCompositionDisabilityAndFqMember.contains(FQPM_REF)) {
+            panelCompositionDisabilityAndFqMember.remove(FQPM_REF);
+        }
     }
 }
---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelMemberCompositionTest.java
@@ -0,0 +1,93 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PanelMemberCompositionTest {
+
+    private static final String FQPM_REF = PanelMemberType.TRIBUNAL_MEMBER_FINANCIALLY_QUALIFIED.toRef();
+
+    static Stream<List<String>> listsWithoutFqpm() {
+        return Stream.of(
+            new ArrayList<>(List.of("58", "44")),
+            new ArrayList<>(),
+            null
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("listsWithoutFqpm")
+    void hasFqpm_shouldReturnFalse_whenFqpmNotPresent(List<String> disabilityAndFqpm) {
+        PanelMemberComposition panelComposition = PanelMemberComposition.builder()
+            .panelCompositionDisabilityAndFqMember(disabilityAndFqpm)
+            .build();
+
+        assertThat(panelComposition.hasFqpm()).isFalse();
+    }
+
+    @Test
+    void hasFqpm_shouldReturnTrue_whenListContainsFqpm() {
+        PanelMemberComposition panelComposition = PanelMemberComposition.builder()
+            .panelCompositionDisabilityAndFqMember(List.of("58", FQPM_REF, "44"))
+            .build();
+
+        assertThat(panelComposition.hasFqpm()).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("listsWithoutFqpm")
+    void addFqpm_shouldAddFqpm(List<String> disabilityAndFqpm) {
+        PanelMemberComposition panelComposition = PanelMemberComposition.builder()
+            .panelCompositionDisabilityAndFqMember(disabilityAndFqpm)
+            .build();
+
+        panelComposition.addFqpm();
+
+        assertThat(panelComposition.getPanelCompositionDisabilityAndFqMember())
+            .contains(FQPM_REF);
+    }
+
+    @Test
+    void addFqpm_shouldNotAddFqpmAgain_whenAlreadyPresent() {
+        PanelMemberComposition panelComposition = PanelMemberComposition.builder()
+            .panelCompositionDisabilityAndFqMember(new ArrayList<>(List.of("44", "58", FQPM_REF)))
+            .build();
+
+        panelComposition.addFqpm();
+
+        assertThat(panelComposition.getPanelCompositionDisabilityAndFqMember())
+            .containsOnlyOnce(FQPM_REF);
+    }
+
+    @ParameterizedTest
+    @MethodSource("listsWithoutFqpm")
+    void removeFqpm_shouldNotChangeList_whenFqpmNotPresent(List<String> disabilityAndFqpm) {
+        PanelMemberComposition panelComposition = PanelMemberComposition.builder()
+            .panelCompositionDisabilityAndFqMember(disabilityAndFqpm == null ? null : List.copyOf(disabilityAndFqpm))
+            .build();
+
+        panelComposition.removeFqpm();
+
+        assertThat(panelComposition.getPanelCompositionDisabilityAndFqMember())
+            .isEqualTo(disabilityAndFqpm);
+    }
+
+    @Test
+    void removeFqpm_shouldRemoveFqpm_whenPresent() {
+        PanelMemberComposition panelComposition = PanelMemberComposition.builder()
+            .panelCompositionDisabilityAndFqMember(new ArrayList<>(List.of("44", FQPM_REF)))
+            .build();
+
+        panelComposition.removeFqpm();
+
+        assertThat(panelComposition.getPanelCompositionDisabilityAndFqMember())
+            .doesNotContain(FQPM_REF);
+    }
+}
