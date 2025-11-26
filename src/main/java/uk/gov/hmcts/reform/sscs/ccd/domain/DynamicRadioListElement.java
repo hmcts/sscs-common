239772c705cb8c259c9d35093eebc1da4d0b26fd
Stuart Wilson <stuart.wilson@hmtcs.net>
2025-11-26 11:58:26 +0000

    SSCSFT-24 ESA 2008 & 2013 regulations - A new ESA Regulations Year field to hold years 2008 and 2013 (default)
    Updated with review comments to remove wild-cards for imports less than 10

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DynamicRadioListElement.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/DynamicRadioListElement.java
@@ -1,7 +1,11 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
