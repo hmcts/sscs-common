a34ac1f0bb339d91de41cb8859ebc6c9a1ad8653
stuart-hmcts <stuart.wilson@hmcts.net>
2025-12-03 11:42:58 +0000

    SSCSFT-24 ESA 2008 & 2013 regulations - A new ESA Regulations Year field to hold years 2008 and 2013 (default) (#1872)
    * SSCSFT-24 ESA 2008 & 2013 regulations - A new ESA Regulations Year field to hold years 2008 and 2013 (default)
    
    * SSCSFT-24 ESA 2008 & 2013 regulations - A new ESA Regulations Year field to hold years 2008 and 2013 (default)
    
    Update re. code review comments
    
    * SSCSFT-24 ESA 2008 & 2013 regulations - A new ESA Regulations Year field to hold years 2008 and 2013 (default)
    
    Update re. code review comments - introduce a DynamicRadioList
    
    * SSCSFT-24 ESA 2008 & 2013 regulations - A new ESA Regulations Year field to hold years 2008 and 2013 (default)
    
    Update re. code review comments - introduce a DynamicRadioList
    
    * SSCSFT-24 ESA 2008 & 2013 regulations - A new ESA Regulations Year field to hold years 2008 and 2013 (default)
    
    Updated with review comments to remove wild-cards for imports less than 10
    
    * SSCSFT-24 ESA 2008 & 2013 regulations - A new ESA Regulations Year field to hold years 2008 and 2013 (default)
    
    Updated with review comments to remove Dynamic Radio List
    
    ---------
    
    Co-authored-by: Stuart Wilson <stuart.wilson@hmtcs.net>

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/domain/SscsEsaCaseDataTest.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.reform.sscs.ccd.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SscsEsaCaseDataTest {
+
+    private final SscsEsaCaseData data = new SscsEsaCaseData();
+
+    @ParameterizedTest
+    @MethodSource("regulation35Selection")
+    void shouldReturnWhetherRegulation35SelectionApplies(String schedule3ActivitiesApply, YesNo expected) {
+        data.setDoesRegulation35Apply(YesNo.YES);
+        data.setEsaWriteFinalDecisionSchedule3ActivitiesApply(schedule3ActivitiesApply);
+
+        final YesNo regulation35Selection = data.getRegulation35Selection();
+
+        assertThat(regulation35Selection).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("schedule3Selection")
+    void shouldReturnWhetherSchedule3SelectionApplies(String schedule3ActivitiesApply, List<String> expected) {
+        data.setEsaWriteFinalDecisionSchedule3ActivitiesQuestion(List.of("test_data"));
+        data.setEsaWriteFinalDecisionSchedule3ActivitiesApply(schedule3ActivitiesApply);
+
+        final List<String> schedule3Selections = data.getSchedule3Selections();
+
+        assertThat(schedule3Selections).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> regulation35Selection() {
+        return Stream.of(
+            Arguments.of("Yes", null),
+            Arguments.of("No", YesNo.YES)
+        );
+    }
+
+    private static Stream<Arguments> schedule3Selection() {
+        return Stream.of(
+            Arguments.of("Yes", List.of("test_data")),
+            Arguments.of("No", List.of()),
+            Arguments.of(null, null)
+        );
+    }
+
+}
