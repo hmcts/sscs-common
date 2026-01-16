6a61b9b312d98c27c703c1aa6ff3f695f5c4fb17
Stuart Wilson <stuart.wilson@hmtcs.net>
2026-01-16 12:17:44 +0000

    SSCSFT-127 Create test users programmatically on the fly for FTs

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ExtendedSscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ExtendedSscsCaseData.java
@@ -1,5 +1,10 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
@@ -16,4 +21,27 @@ import lombok.NoArgsConstructor;
 public class ExtendedSscsCaseData {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private YesNo selectNextHmcHearingType;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private DynamicList features;
+    
+    public void enableFeature(String feature) {
+
+        if (features == null || features.getListItems() == null || features.getListItems().isEmpty()) {
+            // List.of is immutable - need to review this re. adding/removing items
+            features = new DynamicList(null, List.of(new DynamicListItem(feature, feature)));
+        } else {
+            features.getListItems().add(new DynamicListItem(feature, feature));
+        }
+    }
+    
+    public void disableFeature(String feature) {
+        if (features == null || features.getListItems() == null) {return;}
+        features.getListItems().removeIf(item -> item.getCode().equals(feature));
+    }
+    
+    public boolean isFeatureEnabled(String feature) {
+        if (features == null || features.getListItems() == null) {return false;}
+        return features.getListItems().stream().anyMatch(item -> item.getCode().equals(feature));
+    }
 }
