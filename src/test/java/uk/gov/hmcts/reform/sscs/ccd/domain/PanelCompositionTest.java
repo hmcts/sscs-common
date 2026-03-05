fd4601ca25e4c361b66bf1c28e870fb4507f5ed7
Nitin Prabhu <142304341+nitinprabhuhmcts@users.noreply.github.com>
2026-03-05 11:50:22 +0000

    Display correct Panel Composition for UC - Upload Response document and Cover email (#1893)

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelCompositionTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/domain/PanelCompositionTest.java
@@ -16,6 +16,7 @@ public class PanelCompositionTest {
         "JUDGE_DOCTOR_AND_DISABILITY_EXPERT_IF_APPLICABLE, judge\\, doctor and disability expert (if applicable), barnwr\\, meddyg ac arbenigwr anabledd (os yw’n berthnasol)",
         "JUDGE_AND_ONE_OR_TWO_DOCTORS, judge and 1 or 2 doctors, barnwr ac 1 neu 2 feddyg",
         "JUDGE_AND_FINANCIALLY_QUALIFIED_PANEL_MEMBER, judge and Financially Qualified Panel Member (if applicable), Barnwr ac Aelod Panel sydd â chymhwyster i ddelio gyda materion Ariannol (os yw’n berthnasol)",
+         "JUDGE_AND_DOCTOR_IF_APPLICABLE, judge and doctor (if applicable), barnwr a meddyg (os yw’n berthnasol)",
         "IBC_PANEL_COMPOSITION, judge and if applicable a medical member and/or a financially qualified tribunal member, barnwr ac os yw’n berthnasol aelod meddygol a/neu aelod o’r tribiwnlys sy’n gymwys mewn materion ariannol"}
     )
     public void assertAllValues(String row) {
