73127bbe4a03d717f875a49c67d84a0d99deb625
Gemma Talbot <gemma.talbot@solirius.com>
2026-03-16 11:10:47 +0000

    add writeFinalDecisionSevereYesNo field

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ExtendedSscsCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/ExtendedSscsCaseData.java
@@ -22,4 +22,7 @@ public class ExtendedSscsCaseData {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private YesNo awareOfAnyAdditionalOtherParties;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private YesNo writeFinalDecisionSevereYesNo;
 }
