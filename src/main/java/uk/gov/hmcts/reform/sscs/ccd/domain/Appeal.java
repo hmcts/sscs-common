acbaf959784f6f0223a949b3f0f39b77f244c262
alibase <alibase@gmail.com>
2026-05-26 14:55:08 +0100

    SSCSFT-2516 Add JsonInclude annotation

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Appeal.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/domain/Appeal.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.sscs.ccd.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @Builder(toBuilder = true)
 public class Appeal {
